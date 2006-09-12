4a65bbebdc031cd80ea9450edde9ae30899b2c86
mueller <mueller@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2006-09-12 09:34:51 +0000

    Test of Thread begun.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@59 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

---
+++ test/testthread.cpp
@@ -0,0 +1,23 @@
+/*
+ * testthread.cpp
+ *
+ * Copyright (C) 2006 by Universitaet Stuttgart (VIS). Alle Rechte vorbehalten.
+ */
+
+#include "testthread.h"
+#include "testhelper.h"
+
+#include "vislib/Runnable.h"
+#include "vislib/Thread.h"
+
+
+class MyRunnable : public vislib::sys::Runnable {
+
+};
+
+
+void TestThread(void) {
+    using namespace vislib::sys;
+
+
+}
---
+++ test/testthread.h
@@ -0,0 +1,15 @@
+/*
+ * testthread.h
+ *
+ * Copyright (C) 2006 by Universitaet Stuttgart (VIS). Alle Rechte vorbehalten.
+ */
+
+#ifndef VISLIBTEST_TESTTHREAD_H_INCLUDED
+#define VISLIBTEST_TESTTHREAD_H_INCLUDED
+#if (_MSC_VER > 1000)
+#pragma once
+#endif /* (_MSC_VER > 1000) */
+
+void TestThread(void);
+
+#endif /* VISLIBTEST_TESTTHREAD_H_INCLUDED */
