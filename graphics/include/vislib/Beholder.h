41473ea26f1df90f413a58a80eabf3ecef35ea38
grottel <grottel@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2006-09-20 09:54:42 +0000

    vislib::graphics added. (with empty beholder class) Project configurations slightly modified.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@83 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

---
+++ graphics/include/vislib/Beholder.h
@@ -0,0 +1,28 @@
+/*
+ * Beholder.h
+ *
+ * Copyright (C) 2006 by Universitaet Stuttgart (VIS). Alle Rechte vorbehalten.
+ * Copyright (C) 2005 by Christoph Mueller. Alle Rechte vorbehalten.
+ */
+
+#ifndef VISLIB_BEHOLDER_H_INCLUDED
+#define VISLIB_BEHOLDER_H_INCLUDED
+#if (_MSC_VER > 1000)
+#pragma once
+#endif /* (_MSC_VER > 1000) */
+
+
+
+namespace vislib {
+namespace graphics {
+
+    /**
+     *
+     */
+    class Beholder {
+    };
+
+} /* end namespace graphics */
+} /* end namespace vislib */
+
+#endif /* VISLIB_BEHOLDER_H_INCLUDED */
---
+++ graphics/src/Beholder.cpp
@@ -0,0 +1,12 @@
+/*
+ * Beholder.cpp  20.09.2006
+ *
+ * Copyright (C) 2006 by Universitaet Stuttgart (VIS). Alle Rechte vorbehalten.
+ */
+
+
+#include "vislib/Beholder.h"
+
+/*
+ * This file is intentionally empty.
+ */
