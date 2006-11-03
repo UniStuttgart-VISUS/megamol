5db17367c20b68edbe6e874781098b98823cd12b
mueller <mueller@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2006-11-03 11:31:52 +0000

    Started regular expression.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@139 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

---
+++ base/src/RegEx.cpp
@@ -0,0 +1,12 @@
+/*
+ * RegEx.cpp
+ *
+ * Copyright (C) 2006 by Universitaet Stuttgart (VIS). Alle Rechte vorbehalten.
+ */
+
+
+#include "vislib/RegEx.h"
+
+/*
+ * This file is intentionally empty.
+ */
