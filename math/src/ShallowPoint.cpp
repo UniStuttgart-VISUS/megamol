01febeab31f0e995fe5ad42dfb4960a43c6f6f06
mueller <mueller@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2007-06-20 15:17:00 +0000

    ShallowPoint added.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@347 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

---
+++ math/src/ShallowPoint.cpp
@@ -0,0 +1,12 @@
+/*
+ * ShallowPoint.cpp
+ *
+ * Copyright (C) 2006 - 2007 by Universitaet Stuttgart (VIS). 
+ * Alle Rechte vorbehalten.
+ */
+
+#include "vislib/ShallowPoint.h"
+
+/*
+ * This file is intentionally empty.
+ */
