5ba4e3df0f5d23a68470c32d774aa60056af63e3
mueller <mueller@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2007-09-01 13:18:27 +0000

    AssertNoException when creating process of known executable.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@408 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

--- test/testprocess.cpp
+++ test/testprocess.cpp
@@ -50,28 +50,12 @@ void TestProcess(void) {
         cout << pe.GetMsgA() << endl;
     }
 
-    Process::Environment envi1;
-
-    ::AssertTrue("New environment is empty", envi1.IsEmpty());
-    ::AssertTrue("EMPTY_ENVIRONMENT is empty", Process::EMPTY_ENVIRONMENT.IsEmpty());
-#ifdef _WIN32
-    ::AssertEqual("Empty environment is NULL", static_cast<const void *>(envi1), static_cast<const void *>(NULL));
-#else
-    ::AssertEqual("Empty environment is NULL", static_cast<char *const *>(envi1), static_cast<char *const *>(NULL));
-#endif 
-
-    Process::Environment envi2("test=hugo", NULL);
-    ::AssertFalse("Initialised environment is not empty", envi2.IsEmpty());
-
-    Process::Environment envi3("test=hugo", "hugo=horst", NULL);
-    ::AssertFalse("Initialised environment is not empty", envi3.IsEmpty());
-
     try {
         Process p1;
 #ifdef _WIN32
-        p1.Create("notepad.exe");
+        AssertNoException("Process::Create", p1.Create("notepad.exe"));
 #else
-        p1.Create("top");
+        AssertNoException("Process::Create", p1.Create("top"));
 #endif 
 
         //Process p2;
