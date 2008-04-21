d0f4996f251b2a5ea8578338bbb9e517f60646a1
grottel <grottel@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2008-04-21 07:17:37 +0000

    gl now builds glh_genext.h in makefile.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@578 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

--- gl/project.mk
+++ gl/project.mk
@@ -7,3 +7,14 @@
 TargetName := vislibgl
 
 IncludeDir += ../base/include ../math/include ../sys/include ../graphics/include
+
+# forward declaration to make it the default target
+all: $(TargetName)d $(TargetName)
+
+# add dependancy to glh
+$(TargetName)d: ./include/glh/glh_genext.h
+$(TargetName): ./include/glh/glh_genext.h
+
+# build target for glh
+./include/glh/glh_genext.h:
+	cd ./include/glh && make glh_genext.h
