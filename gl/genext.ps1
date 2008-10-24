c557c0189f1ee9808c7ea360f3ef5df9d0fecd99
mueller <mueller@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2008-10-24 08:42:35 +0000

    Fixed glh auto build on Windows ... again.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@709 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

--- gl/genext.ps1
+++ gl/genext.ps1
@@ -4,7 +4,23 @@
 # Alle Rechte vorbehalten.
 #
 
-#check arguments
+# Original Commands 32 bit
+# powershell -nologo -noprofile -noninteractive -command "%1""\include\glh\genextfile.ps1" "%1""\include\GL\glext.h" "%1""\include\glh\extfile.txt"
+# set INCLUDE="$(InputDir)\include\";%INCLUDE%
+# "$(VCInstallDir)\vcpackages\vcbuild.exe" "$(InputDir)\include\glh\glh.vcproj" $(ConfigurationName) /useenv /nologo
+# @echo Generating $(InputDir)\include\glh\glh_genext.h" from "$(InputDir)\include\glh\extfile.txt" ...
+# "$(InputDir)\include\glh\$(ConfigurationName)\glh.exe" "$(InputDir)\include\glh\glh_genext.h" "$(InputDir)\include\glh\extfile.txt"
+
+# Original Commands 32 bit
+# powershell -nologo -noprofile -noninteractive -command "$(InputDir)\include\glh\genextfile.ps1" "$(InputDir)\include\GL\glext.h" "$(InputDir)\include\glh\extfile.txt"
+# powershell -nologo -noprofile -noninteractive -command "[xml]$proj = gc '$(InputDir)\include\glh\glh.vcproj'; $proj.GetElementsByTagName('Tool') | where { $_.Name -eq 'VCCLCompilerTool' } | foreach { $_.SetAttribute('AdditionalIncludeDirectories', '$(InputDir)\include\;' + $_.AdditionalIncludeDirectories) }; $proj.Save('$(InputDir)\include\glh\glh64crowbar.vcproj')"
+# "$(VCInstallDir)\vcpackages\vcbuild.exe" "$(InputDir)\include\glh\glh64crowbar.vcproj" $(ConfigurationName) /nologo /platform:Win32
+# @echo Generating $(InputDir)\include\glh\glh_genext.h" from "$(InputDir)\include\glh\extfile.txt" ...
+# "$(InputDir)\include\glh\$(ConfigurationName)\glh.exe" "$(InputDir)\include\glh\glh_genext.h" "$(InputDir)\include\glh\extfile.txt"
+# del "$(InputDir)\include\glh\glh64crowbar.vcproj"
+
+
+# Check arguments
 if ($Args.Length -ne 4) {
     echo "Error: Argument list invalid."
     echo "Must be: InputDir Bits ConfigurationName VCInstallDir"
@@ -21,51 +37,36 @@ if (($Bits -ne "32") -and ($Bits -ne "64"))  {
     exit -2
 }
 
-# generate 'extfile.txt'
+
+# Generate 'extfile.txt'
 & "$InputDir\include\glh\genextfile.ps1" "$InputDir\include\GL\glext.h" "$InputDir\include\glh\extfile.txt"
 
+
+# Include the path to wglext.h in the environment INCLUDE variable.
+$incPath = "`"$InputDir\include\glh`";" 
+$incPath += gc Env:INCLUDE
+si -path Env:INCLUDE -value "$incPath"
+echo $Env:INCLUDE
+
+
 # compile 'glh.exe'
-if ($Bits -eq "32") {
+$projName = "$InputDir\" + "include\glh\glh$Bits" + "crowbar.vcproj"
 
-    # commands 32
-    # powershell -nologo -noprofile -noninteractive -command "%1""\include\glh\genextfile.ps1" "%1""\include\GL\glext.h" "%1""\include\glh\extfile.txt"
-    # set INCLUDE="$(InputDir)\include\";%INCLUDE%
-    # "$(VCInstallDir)\vcpackages\vcbuild.exe" "$(InputDir)\include\glh\glh.vcproj" $(ConfigurationName) /useenv /nologo
-    # @echo Generating $(InputDir)\include\glh\glh_genext.h" from "$(InputDir)\include\glh\extfile.txt" ...
-    # "$(InputDir)\include\glh\$(ConfigurationName)\glh.exe" "$(InputDir)\include\glh\glh_genext.h" "$(InputDir)\include\glh\extfile.txt"
-
-    # Create a copy to prevent in-place upgrade of svn-external project
-    [xml]$proj = gc "$InputDir\include\glh\glh.vcproj";
-    $proj.Save("$InputDir\include\glh\glh32crowbar.vcproj");
-    
-    & "$VCInstallDir\vcpackages\vcbuild.exe" "$InputDir\include\glh\glh32crowbar.vcproj" "$ConfigurationName" /useenv /nologo /upgrade /override:"$InputDir\glhSettings.vsprops"
-    
-    del "$InputDir\include\glh\glh32crowbar.vcproj"
-}
-
+[xml]$proj = gc "$InputDir\include\glh\glh.vcproj";
 if ($Bits -eq "64") {
-
-    # commands 64
-    # powershell -nologo -noprofile -noninteractive -command "$(InputDir)\include\glh\genextfile.ps1" "$(InputDir)\include\GL\glext.h" "$(InputDir)\include\glh\extfile.txt"
-    # powershell -nologo -noprofile -noninteractive -command "[xml]$proj = gc '$(InputDir)\include\glh\glh.vcproj'; $proj.GetElementsByTagName('Tool') | where { $_.Name -eq 'VCCLCompilerTool' } | foreach { $_.SetAttribute('AdditionalIncludeDirectories', '$(InputDir)\include\;' + $_.AdditionalIncludeDirectories) }; $proj.Save('$(InputDir)\include\glh\glh64crowbar.vcproj')"
-    # "$(VCInstallDir)\vcpackages\vcbuild.exe" "$(InputDir)\include\glh\glh64crowbar.vcproj" $(ConfigurationName) /nologo /platform:Win32
-    # @echo Generating $(InputDir)\include\glh\glh_genext.h" from "$(InputDir)\include\glh\extfile.txt" ...
-    # "$(InputDir)\include\glh\$(ConfigurationName)\glh.exe" "$(InputDir)\include\glh\glh_genext.h" "$(InputDir)\include\glh\extfile.txt"
-    # del "$(InputDir)\include\glh\glh64crowbar.vcproj"
-
-    [xml]$proj = gc "$InputDir\include\glh\glh.vcproj";
     $proj.GetElementsByTagName('Tool') | where { $_.Name -eq 'VCCLCompilerTool' } | foreach {
-        $_.SetAttribute('AdditionalIncludeDirectories', '$InputDir\include\;' + $_.AdditionalIncludeDirectories);
+        $_.SetAttribute('AdditionalIncludeDirectories', '$InputDir\include\;$InputDir\include\glh\;' + $_.AdditionalIncludeDirectories);
     };
-    $proj.Save("$InputDir\include\glh\glh64crowbar.vcproj");
-
-    & "$VCInstallDir\vcpackages\vcbuild.exe" "$InputDir\include\glh\glh64crowbar.vcproj" "$ConfigurationName" /nologo /upgrade /platform:Win32 /override:"$InputDir\glhSettings.vsprops"
-
-    del "$InputDir\include\glh\glh64crowbar.vcproj"
 }
+$proj.Save($projName);
 
-# call 'glh.exe' to generate 'glh_genext.h'
+& "$VCInstallDir\vcpackages\vcbuild.exe" $projName /upgrade
+& "$VCInstallDir\vcpackages\vcbuild.exe" $projName "$ConfigurationName" /useenv /nologo /platform:Win32
+
+del $projName
+
+
+# Call 'glh.exe' to generate 'glh_genext.h'
 & "$InputDir\include\glh\$ConfigurationName\glh.exe" "$InputDir\include\glh\glh_genext.h" "$InputDir\include\glh\extfile.txt"
 
-# finished
 exit 0
