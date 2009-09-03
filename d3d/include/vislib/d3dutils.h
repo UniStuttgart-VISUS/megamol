bf3722b8b0a0882dee667242990c1e81dc4852d1
mueller <mueller@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2009-09-03 14:59:21 +0000

    Added D3D9 mode enumeration utility functions.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@868 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

--- d3d/include/vislib/d3dutils.h
+++ d3d/include/vislib/d3dutils.h
@@ -15,6 +15,8 @@
 #pragma managed(push, off)
 #endif /* defined(_WIN32) && defined(_MANAGED) */
 
+#include <d3d9.h>
+
 
 #ifndef SAFE_RELEASE
 #define SAFE_RELEASE(obj) if (obj != NULL) { obj->Release(); obj = NULL; }
@@ -29,8 +31,100 @@ namespace d3d {
     typedef enum ApiVersion_t {
         D3DVERSION_UNKNOWN = 0,
         D3DVERSION_9,
-        D3DVERSION_10
+        D3DVERSION_10,
+        D3DVERSION_11
     } ApiVersion;
+
+    /** 
+     * This enumeration is used to judge wether a screen resolution should be
+     * considered larger than another one.
+     */
+    typedef enum FullscreenSizeCriterion_t {
+        CRITERION_AREA = 1, //< Maximise the screen area.
+        CRITERION_WIDTH,    //< Maximise the screen width.
+        CRITERION_HEIGHT    //< Maximise the screen height.
+    } FullscreenSizeCriterion;
+
+    /**
+     * Answer the dimensions of the backbuffer of the specified device.
+     *
+     * @param outWidth
+     * @param outHeight
+     * @param device
+     *
+     * @throws
+     */
+    void GetBackbufferSize(UINT& outWidth, UINT& outHeight, 
+        IDirect3DDevice9 *device);
+
+    /**
+     * Answer the maximum resolution the specified adapter supports.
+     *
+     * @param outWidth
+     * @param outHeight
+     * @param outRefreshRate
+     * @param d3d
+     * @param adapterID
+     * @param format
+     * @param criterion
+     *
+     * @throws
+     * @throws
+     */
+    void GetMaximumFullscreenResolution(UINT& outWidth, 
+        UINT& outHeight, 
+        UINT& outRefreshRate,
+        IDirect3D9 *d3d, 
+        const UINT adapterID, 
+        const D3DFORMAT& format,
+        const FullscreenSizeCriterion criterion = CRITERION_AREA);
+
+    /**
+     * Answer the maximum resolution that is supported by all adapters of the
+     * specified device.
+     *
+     * @param outWidth
+     * @param outHeight
+     * @param outRefreshRate
+     * @param d3d
+     * @param format
+     * @param criterion
+     *
+     * @return true if a mode was found that is supported by all adapters, 
+     *         false otherwise.
+     *
+     * @throws
+     * @throws
+     */
+    bool GetMaximumSharedFullscreenResolution(UINT& outWidth, 
+        UINT& outHeight, 
+        UINT& outRefreshRate,
+        IDirect3D9 *d3d, 
+        const D3DFORMAT& format,
+        const FullscreenSizeCriterion criterion = CRITERION_AREA);
+
+    /**
+     * Answer whether the specified fullscreen resolution is supported by the
+     * specified adapter. If requested, return the refresh rate for that mode.
+     * 
+     * @param d3d
+     * @param adapterID
+     * @param format
+     * @param width
+     * @param height
+     * @param outRefreshRate
+     *
+     * @return
+     *
+     * @throws
+     * @throws
+     */
+    bool IsFullscreenResolutionSupported(IDirect3D9 *d3d, 
+        const UINT adapterID, 
+        const D3DFORMAT& format,
+        const UINT width,
+        const UINT height,
+        UINT *outRefreshRate = NULL);
 
 } /* end namespace d3d */
 } /* end namespace graphics */
