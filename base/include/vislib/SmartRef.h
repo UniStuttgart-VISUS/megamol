11104036aab8ccfb82b2f77727547790999837d3
mueller <mueller@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2010-01-13 17:16:20 +0000

    Linux hotfix
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@890 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

--- base/include/vislib/SmartRef.h
+++ base/include/vislib/SmartRef.h
@@ -244,8 +244,13 @@ namespace vislib {
     template<class Tp> 
     SmartRef<Tp> const SmartRef<T>::DynamicCast(const bool addRef) const {
         return (this->IsNull()) 
+#ifdef _WIN32
             ? const SmartRef<Tp>(NULL, addRef)
             : const SmartRef<Tp>(dynamic_cast<Tp *>(this->obj), addRef);
+#else /* _WIN32 */
+            ? SmartRef<Tp>(NULL, addRef)
+            : SmartRef<Tp>(dynamic_cast<Tp *>(this->obj), addRef);
+#endif /* _WIN32 */
     }
 
 
