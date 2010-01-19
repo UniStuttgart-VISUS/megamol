b8b478a41c64bb5d08be3554ee333a12d72233c7
grottel <grottel@77d55b08-817d-0410-979b-1fec1beb8c2e>
2010-01-19 11:19:50 +0000

    Shader files copied from old repository
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/core/trunk@4 77d55b08-817d-0410-979b-1fec1beb8c2e

---
+++ Shaders/commondefines.glsl
@@ -0,0 +1,16 @@
+#define CLIP
+#define DEPTH
+#define SMALL_SPRITE_LIGHTING
+//#define CALC_CAM_SYS
+
+#ifdef DEBUG
+#undef CLIP
+#define RETICLE
+#define AXISHINTS
+#endif // DEBUG
+
+//#define BULLSHIT
+
+#ifndef FLACH
+#define FLACH
+#endif
