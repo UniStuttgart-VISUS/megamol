222f23f473db7187b6ad14751515c5c57f94871d
grottel <grottel@6c50cc09-817d-0410-90cf-7b22d5ef511c>
2010-01-20 08:37:52 +0000

    (Empty) solution and configuration scripts
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/frontends/console/trunk@2 6c50cc09-817d-0410-90cf-7b22d5ef511c

---
+++ configure.lin.pl
@@ -0,0 +1,54 @@
+#
+# configure.lin.pl
+# MegaMol Console
+#
+# Copyright (C) 2008-2010 by VISUS (Universitaet Stuttgart).
+# Alle Rechte vorbehalten.
+#
+push @INC, "configperl";
+require "configperl.inc";
+
+my @pps = ();
+my @fps = ();
+my @cfps = ();
+my @sps = ();
+
+$a = PathParameter->new();
+    $a->id("outbin");
+    $a->description("Path to the global \"bin\" output directory");
+    $a->placeholder("%outbin%");
+    $a->autoDetect(0);
+    push @pps, $a;
+
+$a = PathParameter->new();
+    $a->id("vislib");
+    $a->description("Path to the vislib directory");
+    $a->placeholder("%vislib%");
+    $a->markerFile("vislib.sln\$");
+    $a->relativeLocation("./");
+    $a->autoDetect(1);
+    push @pps, $a;
+$a = PathParameter->new();
+    $a->id("visglut");
+    $a->description("Path to the visglut directory");
+    $a->placeholder("%visglutPath%");
+    $a->markerFile("include/visglut.h\$");
+    $a->relativeLocation("../");
+    $a->autoDetect(1);
+    push @pps, $a;
+$a = PathParameter->new();
+    $a->id("mmcore");
+    $a->description("Path to the MegaMol core directory");
+    $a->placeholder("%mmcorePath%");
+    $a->markerFile("api/MegaMolCore.h\$");
+    $a->relativeLocation("../");
+    $a->autoDetect(1);
+    push @pps, $a;
+
+$c = ConfigFilePair->new();
+    $c->inFile("ExtLibs.mk.input");
+    $c->outFile("ExtLibs.mk");
+    push @cfps, $c;
+
+Configure("MegaMol(TM) Console Configuration for Linux", ".megamol.console.lin.cache", \@pps, \@fps, \@cfps, \@sps);
+
