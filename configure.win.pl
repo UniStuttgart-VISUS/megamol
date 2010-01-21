8dd7f00eda162b6579fb932672fe008a3d244369
grottel <grottel@cb50be13-817d-0410-ab5c-3777bfb244a9>
2010-01-21 09:10:55 +0000

    Configuration scripts added
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/mmstd_trisoup/trunk@3 cb50be13-817d-0410-ab5c-3777bfb244a9

---
+++ configure.win.pl
@@ -0,0 +1,52 @@
+#
+# configure.win.pl
+# MegaMol Plugin
+#
+# Copyright (C) 2008-2010 by VISUS (Universitaet Stuttgart).
+# Alle Rechte vorbehalten.
+#
+push @INC, "configperl";
+require "configperl.inc";
+
+my @pps = ();
+my @fps = ();
+my @cfps = ();
+my @sps = ();
+
+$a = PathParameter->new();
+    $a->id("outbin");
+    $a->description("Path to the global \"bin\" output directory");
+    $a->placeholder("%outbin%");
+    $a->autoDetect(0);
+    push @pps, $a;
+$a = PathParameter->new();
+    $a->id("outshader");
+    $a->description("Path to the global \"shaders\" output directory");
+    $a->placeholder("%outshader%");
+    $a->autoDetect(0);
+    push @pps, $a;
+
+$a = PathParameter->new();
+    $a->id("mmcore");
+    $a->description("Path to the MegaMol core directory");
+    $a->placeholder("%mmcorePath%");
+    $a->markerFile("api/MegaMolCore.h\$");
+    $a->relativeLocation("../");
+    $a->autoDetect(1);
+    push @pps, $a;
+$a = PathParameter->new();
+    $a->id("vislib");
+    $a->description("Path to the vislib directory");
+    $a->placeholder("%vislib%");
+    $a->markerFile("vislib.sln\$");
+    $a->relativeLocation("./");
+    $a->autoDetect(1);
+    push @pps, $a;
+
+$c = ConfigFilePair->new();
+    $c->inFile("ExtLibs.vsprops.input");
+    $c->outFile("ExtLibs.vsprops");
+    push @cfps, $c;
+
+Configure("MegaMol(TM) Plugin Configuration for Windows", ".megamol.plg.win.cache", \@pps, \@fps, \@cfps, \@sps);
+
