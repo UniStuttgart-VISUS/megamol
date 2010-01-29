f717f56e0250748f8babad43ccfe485cad5fd6a7
grottel <grottel@5e8c2511-817d-0410-85a1-fd227d6480d1>
2010-01-29 17:24:39 +0000

    SolPath Modules created (are all empty!)
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/protein/trunk@13 5e8c2511-817d-0410-85a1-fd227d6480d1

---
+++ SolPathDataSource.cpp
@@ -0,0 +1,43 @@
+/*
+ * SolPathRenderer.cpp
+ *
+ * Copyright (C) 2010 by VISUS (University of Stuttgart)
+ * Alle Rechte vorbehalten.
+ */
+#include "stdafx.h"
+#include "SolPathDataSource.h"
+
+using namespace megamol::protein;
+
+
+/*
+ * SolPathDataSource::SolPathDataSource
+ */
+SolPathDataSource::SolPathDataSource(void) : core::Module() {
+    // TODO: Implement
+}
+
+
+/*
+ * SolPathDataSource::~SolPathDataSource
+ */
+SolPathDataSource::~SolPathDataSource(void) {
+    // TODO: Implement
+}
+
+
+/*
+ * SolPathDataSource::create
+ */
+bool SolPathDataSource::create(void) {
+    // TODO: Implement
+    return false;
+}
+
+
+/*
+ * SolPathDataSource::release
+ */
+void SolPathDataSource::release(void) {
+    // TODO: Implement
+}
---
+++ SolPathRenderer.cpp
@@ -0,0 +1,72 @@
+/*
+ * SolPathRenderer.cpp
+ *
+ * Copyright (C) 2010 by VISUS (University of Stuttgart)
+ * Alle Rechte vorbehalten.
+ */
+#include "stdafx.h"
+#include "SolPathRenderer.h"
+
+using namespace megamol;
+using namespace megamol::protein;
+
+
+/*
+ * SolPathRenderer::SolPathRenderer
+ */
+SolPathRenderer::SolPathRenderer(void) : core::view::Renderer3DModule() {
+    // TODO: Implement
+}
+
+
+/*
+ * SolPathRenderer::~SolPathRenderer
+ */
+SolPathRenderer::~SolPathRenderer(void) {
+    this->Release();
+    // TODO: Implement
+}
+
+
+/*
+ * SolPathRenderer::create
+ */
+bool SolPathRenderer::create(void) {
+    // TODO: Implement
+    return false;
+}
+
+
+/*
+ * SolPathRenderer::GetCapabilities
+ */
+bool SolPathRenderer::GetCapabilities(core::Call& call) {
+    // TODO: Implement
+    return false;
+}
+
+
+/*
+ * SolPathRenderer::GetExtents
+ */
+bool SolPathRenderer::GetExtents(core::Call& call) {
+    // TODO: Implement
+    return false;
+}
+
+
+/*
+ * SolPathRenderer::release
+ */
+void SolPathRenderer::release(void) {
+    // TODO: Implement
+}
+
+
+/*
+ * SolPathRenderer::Render
+ */
+bool SolPathRenderer::Render(core::Call& call) {
+    // TODO: Implement
+    return false;
+}
