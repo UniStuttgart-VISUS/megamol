40854e5dfda8a689bc2fb33f98e08f2ccf4125ce
scharnkn <scharnkn@8215d0b1-3b2f-4a13-8d91-4b3e73067666>
2010-11-23 18:03:19 +0000

    Modified ProteinRenderer to use the color class
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/protein_cuda/trunk@191 8215d0b1-3b2f-4a13-8d91-4b3e73067666

--- Color.h
+++ Color.h
@@ -12,6 +12,7 @@
 #endif /* (defined(_MSC_VER) && (_MSC_VER > 1000)) */
 
 #include "MolecularDataCall.h"
+#include "CallProteinData.h"
 
 namespace megamol {
 namespace protein {
@@ -31,8 +32,20 @@ namespace protein {
             CHAIN       = 6,
             MOLECULE    = 7,
             RAINBOW     = 8,
-            CHAINBOW    = 9
+            CHAINBOW    = 9,
+            AMINOACID   = 10,
+            VALUE       = 11,
+            CHAIN_ID    = 12
         };
+
+        /**
+         * fill amino acid color table
+         *
+         * @param aminoAcidColorTable The amino acid color table.
+         */
+        static void FillAminoAcidColorTable(
+            vislib::Array<vislib::math::Vector<unsigned char, 3> >
+              &aminoAcidColorTable);
 
         /**
          * Make color table for all atoms acoording to the current coloring
@@ -41,7 +54,6 @@ namespace protein {
          * recomputation is forced by parameter.
          *
          * @param mol                 The data interface.
-         * @param forceRecompute      Force recomputation of the color table.
          * @param minGradColor        The minimum value for gradient coloring.
          * @param midGradColor        The middle value for gradient coloring.
          * @param maxGradColor        The maximum value for gradient coloring.
@@ -49,6 +61,7 @@ namespace protein {
          * @param atomColorTable      The atom color table.
          * @param colorLookupTable    The color lookup table.
          * @param rainbowColors       The rainbow color lookup table.
+         * @param forceRecompute      Force recomputation of the color table.
          */
         static void MakeColorTable(const MolecularDataCall *mol,
             vislib::TString minGradColor,
@@ -58,7 +71,28 @@ namespace protein {
             vislib::Array<vislib::math::Vector<float, 3> > &atomColorTable,
             vislib::Array<vislib::math::Vector<float, 3> > &colorLookupTable,
             vislib::Array<vislib::math::Vector<float, 3> > &rainbowColors,
-            bool forceRecompute=false);
+            bool forceRecompute = false);
+
+        /**
+         * Make color table for all atoms acoording to the current coloring
+         * mode.
+         * The color table is only computed if it is empty or if the
+         * recomputation is forced by parameter.
+         *
+         * @param prot                The data interface.
+         * @param currentColoringMode The current coloring mode.
+         * @param protAtomColorTable  The atom color table.
+         * @param aminoAcidColorTable The amino acid color table.
+         * @param rainbowColors       The rainbow color lookup table.
+         * @param forceRecompute      Force recomputation of the color table.
+         */
+        static void MakeColorTable( const CallProteinData *prot,
+            ColoringMode currentColoringMode,
+            vislib::Array<unsigned char> &protAtomColorTable,
+            vislib::Array<vislib::math::Vector<unsigned char, 3> >
+              &aminoAcidColorTable,
+            std::vector<vislib::math::Vector<float,3> > &rainbowColors,
+            bool forceRecompute = false);
 
          /**
          * Creates a rainbow color table with 'num' entries.
@@ -68,6 +102,15 @@ namespace protein {
          */
         static void MakeRainbowColorTable( unsigned int num,
             vislib::Array<vislib::math::Vector<float, 3> > &rainbowColors);
+
+        /**
+         * Creates a rainbow color table with 'num' entries.
+         *
+         * @param num            The number of color entries.
+         * @param rainbowColors  The rainbow color lookup table.
+         */
+        static void MakeRainbowColorTable( unsigned int num,
+            std::vector<vislib::math::Vector<float,3> > &rainbowColors);
 
         /**
          * Read color table from file.
