4cb35ab47e087bbbcf33e8fe77386761277c1cab
kroneml <kroneml@5e8c2511-817d-0410-85a1-fd227d6480d1>
2011-04-11 10:32:10 +0000

    SolventVolumeRenderer: threshold for hydrogen bonds / visual neighborhood debugging
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/protein/trunk@272 5e8c2511-817d-0410-85a1-fd227d6480d1

--- GridNeighbourFinder.h
+++ GridNeighbourFinder.h
@@ -6,6 +6,12 @@
  */
 
 
+#ifndef MEGAMOLPROTEIN_GRIDNEIGHBORFIND_H_INCLUDED
+#define MEGAMOLPROTEIN_GRIDNEIGHBORFIND_H_INCLUDED
+#if (defined(_MSC_VER) && (_MSC_VER > 1000))
+#pragma once
+#endif /* (defined(_MSC_VER) && (_MSC_VER > 1000)) */
+
 #include "stdafx.h"
 #include "vislib/ArrayAllocator.h"
 #include "vislib/Log.h"
@@ -171,3 +177,5 @@ namespace protein {
 	};
 }; //namespace megamol
 }; //namespace protein
+
+#endif
