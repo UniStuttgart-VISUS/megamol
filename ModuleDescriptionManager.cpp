ba1e66ede8bcd7bed59346889f359e62c5e12456
grottel <grottel@77d55b08-817d-0410-979b-1fec1beb8c2e>
2012-06-18 16:30:31 +0000

    DirPartFilter implemented
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/core/trunk@402 77d55b08-817d-0410-979b-1fec1beb8c2e

--- ModuleDescriptionManager.cpp
+++ ModuleDescriptionManager.cpp
@@ -50,6 +50,7 @@
 #include "moldyn/VIMDataSource.h"
 #include "moldyn/VisIttDataSource.h"
 #include "moldyn/DirPartColModulate.h"
+#include "moldyn/DirPartFilter.h"
 //#include "special/ColStereoDisplay.h"
 #include "view/ClipPlane.h"
 #include "view/LinearTransferFunction.h"
@@ -144,6 +145,7 @@ void ModuleDescriptionManager::registerObjects(ModuleDescriptionManager *instanc
         instance->registerAutoDescription<moldyn::VIMDataSource>();
         instance->registerAutoDescription<moldyn::VisIttDataSource>();
         instance->registerAutoDescription<moldyn::DirPartColModulate>();
+        instance->registerAutoDescription<moldyn::DirPartFilter>();
         //instance->registerAutoDescription<special::ColStereoDisplay>();
         instance->registerAutoDescription<view::ClipPlane>();
         instance->registerAutoDescription<view::LinearTransferFunction>();
