b0600b352d930e92ae787660d3c522d858766cc7
grottel <grottel@5e8c2511-817d-0410-85a1-fd227d6480d1>
2012-10-02 09:35:56 +0000

    MoleculeBallifier added
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/protein/trunk@681 5e8c2511-817d-0410-85a1-fd227d6480d1

---
+++ MoleculeBallifier.cpp
@@ -0,0 +1,142 @@
+/*
+ * MoleculeBallifier.cpp
+ *
+ * Copyright (C) 2012 by TU Dresden
+ * All rights reserved.
+ */
+
+#include "stdafx.h"
+#include "MoleculeBallifier.h"
+#include "MolecularDataCall.h"
+#include "moldyn\MultiParticleDataCall.h"
+
+
+using namespace megamol;
+using namespace megamol::protein;
+
+
+/*
+ * 
+ */
+MoleculeBallifier::MoleculeBallifier(void) : core::Module(), 
+        outDataSlot("outData", "Sends MultiParticleDataCall data out into the world"),
+        inDataSlot("inData", "Fetches MolecularDataCall data"),
+        inHash(0), outHash(0), data(), colMin(0.0f), colMax(1.0f) {
+
+    this->inDataSlot.SetCompatibleCall<MolecularDataCallDescription>();
+    this->MakeSlotAvailable(&this->inDataSlot);
+
+    this->outDataSlot.SetCallback(core::moldyn::MultiParticleDataCall::ClassName(),
+        core::moldyn::MultiParticleDataCall::FunctionName(0), &MoleculeBallifier::getData);
+    this->outDataSlot.SetCallback(core::moldyn::MultiParticleDataCall::ClassName(),
+        core::moldyn::MultiParticleDataCall::FunctionName(1), &MoleculeBallifier::getExt);
+    this->MakeSlotAvailable(&this->outDataSlot);
+
+}
+
+
+/*
+ * 
+ */
+MoleculeBallifier::~MoleculeBallifier(void) {
+    this->Release();
+}
+
+
+/*
+ * 
+ */
+bool MoleculeBallifier::create(void) {
+    // intentionally empty
+    return true;
+}
+
+
+/*
+ * 
+ */
+void MoleculeBallifier::release(void) {
+    // intentionally empty
+}
+
+
+/*
+ * 
+ */
+bool MoleculeBallifier::getData(core::Call& c) {
+    using core::moldyn::MultiParticleDataCall;
+    MultiParticleDataCall *ic = dynamic_cast<MultiParticleDataCall*>(&c);
+    if (ic == NULL) return false;
+
+    MolecularDataCall *oc = this->inDataSlot.CallAs<MolecularDataCall>();
+    if (oc == NULL) return false;
+
+    oc->SetFrameID(ic->FrameID());
+
+    if ((*oc)(0)) {
+        if (this->inHash != oc->DataHash()) {
+            this->inHash = oc->DataHash();
+            this->outHash++;
+
+/*
+da kannst Du die atom-position und den B-Faktor auslesen
+AtomCount, AtomPositions, AtomBFactors
+über den Atom-Type kannst Du auf das Type-Array zugreifen, das den Radius drin hat
+*/
+
+            unsigned int cnt = oc->AtomCount();
+            this->data.AssertSize(sizeof(float) * 5 * cnt);
+            float *fData = this->data.As<float>();
+
+            for (unsigned int i = 0; i < cnt; i++, fData += 5) {
+                fData[0] = oc->AtomPositions()[i * 3 + 0];
+                fData[1] = oc->AtomPositions()[i * 3 + 1];
+                fData[2] = oc->AtomPositions()[i * 3 + 2];
+
+                fData[3] = oc->AtomTypes()[oc->AtomTypeIndices()[i]].Radius();
+
+                fData[4] = oc->AtomBFactors()[i];
+                if ((i == 0) || (this->colMin > fData[4])) this->colMin = fData[4];
+                if ((i == 0) || (this->colMax < fData[4])) this->colMax = fData[4];
+            }
+
+        }
+
+        ic->SetDataHash(this->outHash);
+        ic->SetParticleListCount(1);
+        MultiParticleDataCall::Particles& p = ic->AccessParticles(0);
+        p.SetCount(this->data.GetSize() / (sizeof(float) * 5));
+        if (p.GetCount() > 0) {
+            p.SetVertexData(MultiParticleDataCall::Particles::VERTDATA_FLOAT_XYZR, this->data.At(0), sizeof(float) * 5);
+            p.SetColourData(MultiParticleDataCall::Particles::COLDATA_FLOAT_I, this->data.At(sizeof(float) * 4), sizeof(float) * 5);
+            p.SetColourMapIndexValues(this->colMin, this->colMax);
+        }
+
+    }
+
+    return true;
+}
+
+
+/*
+ * 
+ */
+bool MoleculeBallifier::getExt(core::Call& c) {
+    using core::moldyn::MultiParticleDataCall;
+    MultiParticleDataCall *ic = dynamic_cast<MultiParticleDataCall*>(&c);
+    if (ic == NULL) return false;
+
+    MolecularDataCall *oc = this->inDataSlot.CallAs<MolecularDataCall>();
+    if (oc == NULL) return false;
+
+    oc->SetFrameID(ic->FrameID());
+
+    if ((*oc)(1)) {
+        ic->SetFrameCount(oc->FrameCount());
+        ic->SetFrameID(oc->FrameID());
+        ic->AccessBoundingBoxes() = oc->AccessBoundingBoxes();
+        return true;
+    }
+
+    return false;
+}
---
+++ MoleculeBallifier.h
@@ -0,0 +1,52 @@
+/*
+ * MoleculeBallifier.h
+ *
+ * Copyright (C) 2012 by TU Dresden
+ * All rights reserved.
+ */
+
+#ifndef MMPROTEINPLUGIN_MOLECULEBALLIFIER_H_INCLUDED
+#define MMPROTEINPLUGIN_MOLECULEBALLIFIER_H_INCLUDED
+#if (defined(_MSC_VER) && (_MSC_VER > 1000))
+#pragma once
+#endif /* (defined(_MSC_VER) && (_MSC_VER > 1000)) */
+
+#include "Module.h"
+#include "CalleeSlot.h"
+#include "CallerSlot.h"
+#include "vislib/RawStorage.h"
+
+namespace megamol {
+namespace protein {
+
+    class MoleculeBallifier : public megamol::core::Module {
+    public:
+        static const char *ClassName(void) {
+            return "MoleculeBallifier";
+        }
+        static const char *Description(void) {
+            return "MoleculeBallifier";
+        }
+        static bool IsAvailable(void) {
+            return true;
+        }
+        MoleculeBallifier(void);
+        virtual ~MoleculeBallifier(void);
+    protected:
+        virtual bool create(void);
+        virtual void release(void);
+    private:
+        bool getData(core::Call& c);
+        bool getExt(core::Call& c);
+
+        core::CalleeSlot outDataSlot;
+        core::CallerSlot inDataSlot;
+        SIZE_T inHash, outHash;
+        vislib::RawStorage data;
+        float colMin, colMax;
+    };
+
+} /* end namespace protein */
+} /* end namespace megamol */
+
+#endif /* MMPROTEINPLUGIN_MOLECULEBALLIFIER_H_INCLUDED */
