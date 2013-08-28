8a8cfb4e161c899232c90d57a2879d1fcd458d5f
kaukerdl <kaukerdl@8215d0b1-3b2f-4a13-8d91-4b3e73067666>
2013-08-28 13:02:40 +0000

    residueselection + call
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/protein_cuda/trunk@1110 8215d0b1-3b2f-4a13-8d91-4b3e73067666

---
+++ ResidueSelection.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "ResidueSelection.h"
+#include "ResidueSelectionCall.h"
+
+using namespace megamol;
+using namespace megamol::protein;
+
+
+ResidueSelection::ResidueSelection(void) :
+    getSelectionSlot("getSelection", "Provides selection data to clients."),
+    selection() {
+
+    this->getSelectionSlot.SetCallback( ResidueSelectionCall::ClassName(), ResidueSelectionCall::FunctionName(ResidueSelectionCall::CallForGetSelection), &ResidueSelection::getSelectionCallback);
+    this->getSelectionSlot.SetCallback( ResidueSelectionCall::ClassName(), ResidueSelectionCall::FunctionName(ResidueSelectionCall::CallForSetSelection), &ResidueSelection::setSelectionCallback);
+    this->MakeSlotAvailable(&this->getSelectionSlot);
+}
+
+
+ResidueSelection::~ResidueSelection(void){
+    this->Release();
+}
+
+
+bool ResidueSelection::create(void) {
+    return true;
+}
+
+
+void ResidueSelection::release(void) {
+    // intentionally empty
+}
+
+
+bool ResidueSelection::getSelectionCallback(core::Call& caller) {
+    ResidueSelectionCall *sc = dynamic_cast<ResidueSelectionCall*>(&caller);
+    if (sc == NULL) return false;
+
+    sc->SetSelectionPointer(&this->selection);
+
+    return true;
+}
+
+
+bool ResidueSelection::setSelectionCallback(core::Call& caller) {
+    ResidueSelectionCall *sc = dynamic_cast<ResidueSelectionCall*>(&caller);
+    if (sc == NULL) return false;
+    
+    this->selection = *sc->GetSelectionPointer();
+
+    return true;
+}
