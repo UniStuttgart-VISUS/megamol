7d75af7928fb7c4c1c8b42506921ef7319ff8101
kroneml <kroneml@8215d0b1-3b2f-4a13-8d91-4b3e73067666>
2013-10-22 16:07:37 +0000

    Visual Studio compiles again!
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/protein_cuda/trunk@1215 8215d0b1-3b2f-4a13-8d91-4b3e73067666

--- Protein.cpp
+++ Protein.cpp
@@ -14,7 +14,6 @@
 #include "DataWriter.h"
 #include "PDBWriter.h"
 #include "VTIWriter.h"
-#include "SurfaceMappingTest.h"
 
 // views
 #include "View3DSpaceMouse.h"
@@ -190,19 +189,18 @@ PROTEIN_API void* mmplgModuleDescription(int idx) {
 #ifdef WITH_CUDA
         case HAPTICS_OFFSET +  0 : return new ModuleAutoDescription<protein::PotentialVolumeRaycaster>();
         case HAPTICS_OFFSET +  1 : return new ModuleAutoDescription<protein::StreamlineRenderer>();
-        case HAPTICS_OFFSET +  2 : return new ModuleAutoDescription<protein::SurfaceMappingTest>();
-        case HAPTICS_OFFSET +  3 : return new ModuleAutoDescription<protein::MoleculeCudaSESRenderer>();
-        case HAPTICS_OFFSET +  4 : return new ModuleAutoDescription<protein::MoleculeCBCudaRenderer>();
-        case HAPTICS_OFFSET +  5 : return new ModuleAutoDescription<protein::QuickSurfRenderer>();
-        case HAPTICS_OFFSET +  6 : return new ModuleAutoDescription<protein::QuickSurfRenderer2>();
-        case HAPTICS_OFFSET +  7 : return new ModuleAutoDescription<protein::QuickSurfMTRenderer>();
-        case HAPTICS_OFFSET +  8 : return new ModuleAutoDescription<protein::MoleculeVolumeCudaRenderer>();
-        case HAPTICS_OFFSET +  9 : return new ModuleAutoDescription<protein::VolumeMeshRenderer>();
-        case HAPTICS_OFFSET + 10 : return new ModuleAutoDescription<protein::DataWriter>();
-        case HAPTICS_OFFSET + 11 : return new ModuleAutoDescription<protein::CrystalStructureVolumeRenderer>();
-        case HAPTICS_OFFSET + 12 : return new ModuleAutoDescription<protein::ComparativeMolSurfaceRenderer>();
-        case HAPTICS_OFFSET + 13 : return new ModuleAutoDescription<protein::ComparativeFieldTopologyRenderer>();
-        #define CUDA_OFFSET 14
+        case HAPTICS_OFFSET +  2 : return new ModuleAutoDescription<protein::MoleculeCudaSESRenderer>();
+        case HAPTICS_OFFSET +  3 : return new ModuleAutoDescription<protein::MoleculeCBCudaRenderer>();
+        case HAPTICS_OFFSET +  4 : return new ModuleAutoDescription<protein::QuickSurfRenderer>();
+        case HAPTICS_OFFSET +  5 : return new ModuleAutoDescription<protein::QuickSurfRenderer2>();
+        case HAPTICS_OFFSET +  6 : return new ModuleAutoDescription<protein::QuickSurfMTRenderer>();
+        case HAPTICS_OFFSET +  7 : return new ModuleAutoDescription<protein::MoleculeVolumeCudaRenderer>();
+        case HAPTICS_OFFSET +  8 : return new ModuleAutoDescription<protein::VolumeMeshRenderer>();
+        case HAPTICS_OFFSET +  9 : return new ModuleAutoDescription<protein::DataWriter>();
+        case HAPTICS_OFFSET + 10 : return new ModuleAutoDescription<protein::CrystalStructureVolumeRenderer>();
+        case HAPTICS_OFFSET + 11 : return new ModuleAutoDescription<protein::ComparativeMolSurfaceRenderer>();
+        case HAPTICS_OFFSET + 12 : return new ModuleAutoDescription<protein::ComparativeFieldTopologyRenderer>();
+        #define CUDA_OFFSET 13
 #else
         #define CUDA_OFFSET 0
 #endif // WITH_CUDA
