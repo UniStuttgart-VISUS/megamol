f0b6bdc89ea45bd535ac0133e1ed6073a174e662
mueller <mueller@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2014-04-08 08:25:53 +0000

    Added dummy to force IB build.
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@1295 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

---
+++ ibcomm/src/IbDummy.cpp
@@ -0,0 +1,22 @@
+/*
+ * IbDummy.cpp
+ *
+ * Copyright (C) 2006 - 2014 by Visualisierungsinstitut Universitaet Stuttgart.
+ * Alle Rechte vorbehalten.
+ */
+
+#include "IbDummy.h"
+
+
+/*
+ * vislib::ibcomm::IbDummy::IbDummy
+ */
+vislib::ibcomm::IbDummy::IbDummy(void) {
+}
+
+
+/*
+ * vislib::ibcomm::IbDummy::~IbDummy
+ */
+vislib::ibcomm::IbDummy::~IbDummy(void) {
+}
