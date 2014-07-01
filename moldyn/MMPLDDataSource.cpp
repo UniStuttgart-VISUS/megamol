e0c0679f31b62816791b2f24b054f42f954567e1
grottel <grottel@77d55b08-817d-0410-979b-1fec1beb8c2e>
2014-07-01 07:08:39 +0000

    do not break me MegaMol
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/core/trunk@584 77d55b08-817d-0410-979b-1fec1beb8c2e

--- moldyn/MMPLDDataSource.cpp
+++ moldyn/MMPLDDataSource.cpp
@@ -50,7 +50,7 @@ moldyn::MMPLDDataSource::Frame::~Frame() {
  */
 bool moldyn::MMPLDDataSource::Frame::LoadFrame(vislib::sys::File *file, unsigned int idx, UINT64 size, unsigned int version) {
     this->frame = idx;
-	this->fileVersion = version;
+    this->fileVersion = version;
     this->dat.EnforceSize(static_cast<SIZE_T>(size));
     return (file->Read(this->dat, size) == size);
 }
@@ -129,19 +129,19 @@ void moldyn::MMPLDDataSource::Frame::SetData(MultiParticleDataCall& call) {
         pts.SetVertexData(vrtDatType, this->dat.At(p), stride);
         pts.SetColourData(colDatType, this->dat.At(p + vrtSize), stride);
 
-		if (this->fileVersion == 101)
-		{
-			p += stride * pts.GetCount();
+        if (this->fileVersion == 101)
+        {
+            p += stride * pts.GetCount();
 
-			// TODO: who deletes this?
-			SimpleSphericalParticles::ClusterInfos *ci = new SimpleSphericalParticles::ClusterInfos();
-			ci->numClusters = *this->dat.AsAt<unsigned int>(p); p += sizeof(unsigned int);
-			ci->sizeofPlainData = *this->dat.AsAt<size_t>(p); p += sizeof(size_t);
-			ci->plainData = (unsigned int*)malloc(ci->sizeofPlainData);
-			memcpy(ci->plainData, this->dat.At(p), ci->sizeofPlainData); p += ci->sizeofPlainData;
-			pts.SetClusterInfos(ci);
-		}
-	}
+            // TODO: who deletes this?
+            SimpleSphericalParticles::ClusterInfos *ci = new SimpleSphericalParticles::ClusterInfos();
+            ci->numClusters = *this->dat.AsAt<unsigned int>(p); p += sizeof(unsigned int);
+            ci->sizeofPlainData = *this->dat.AsAt<size_t>(p); p += sizeof(size_t);
+            ci->plainData = (unsigned int*)malloc(ci->sizeofPlainData);
+            memcpy(ci->plainData, this->dat.At(p), ci->sizeofPlainData); p += ci->sizeofPlainData;
+            pts.SetClusterInfos(ci);
+        }
+    }
 
 }
 
@@ -212,7 +212,7 @@ void moldyn::MMPLDDataSource::loadFrame(view::AnimDataModule::Frame *frame,
     //Log::DefaultLog.WriteMsg(Log::LEVEL_INFO, "Requesting frame %u of %u frames\n", idx, this->FrameCount());
     ASSERT(idx < this->FrameCount());
     this->file->Seek(this->frameIdx[idx]);
-	if (!f->LoadFrame(this->file, idx, this->frameIdx[idx + 1] - this->frameIdx[idx], this->fileVersion)) {
+    if (!f->LoadFrame(this->file, idx, this->frameIdx[idx + 1] - this->frameIdx[idx], this->fileVersion)) {
         // failed
         Log::DefaultLog.WriteMsg(Log::LEVEL_ERROR, "Unable to read frame %d from MMPLD file\n", idx);
     }
@@ -284,7 +284,7 @@ bool moldyn::MMPLDDataSource::filenameChanged(param::ParamSlot& slot) {
     if (ver != 100 && ver != 101) {
         _ERROR_OUT("MMPLD file header version wrong");
     }
-	this->fileVersion = ver;
+    this->fileVersion = ver;
 
     UINT32 frmCnt = 0;
     _ASSERT_READFILE(&frmCnt, 4);
