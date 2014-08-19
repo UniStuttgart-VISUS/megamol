4c8c94b1e2c27a3fd528bf112617e96fc338f12e
grottel <grottel@77d55b08-817d-0410-979b-1fec1beb8c2e>
2014-08-19 12:40:05 +0000

    Defines fixed
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/core/trunk@638 77d55b08-817d-0410-979b-1fec1beb8c2e

--- view/LinkedView3D.cpp
+++ view/LinkedView3D.cpp
@@ -47,8 +47,8 @@ view::LinkedView3D::~LinkedView3D(void) {
  */
 void view::LinkedView3D::Render(const mmcRenderViewContext& context) {
 
-#if (VISLIB_VERSION_MAJOR == 1) && (VISLIB_VERSION_MINOR == 0)
-	this->observableCamParams->CopyChangedParamsFrom(this->camParams);
+#if (VISLIB_VERSION_MAJOR >= 1) && (VISLIB_VERSION_MINOR >= 0)
+    this->observableCamParams->CopyChangedParamsFrom(this->camParams);
 #endif
 
     // Get camera parameters call
