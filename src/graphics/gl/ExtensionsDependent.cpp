daac8a75a22e2430a4e1395617be7712db5f35fd
grottel <grottel@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2015-01-24 11:03:20 +0000

    INCOMPLETE gl integrated
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@1359 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

---
+++ src/graphics/gl/ExtensionsDependent.cpp
@@ -0,0 +1,12 @@
+/*
+ * ExtensionsDependent.cpp
+ *
+ * Copyright (C) 2006 - 2007 by Universitaet Stuttgart (VIS). 
+ * Alle Rechte vorbehalten.
+ */
+
+#include "vislib/graphics/gl/ExtensionsDependent.h"
+
+/*
+ * This file is intentionally empty.
+ */
