6295bad17335fc028864eb9707381d71eca2c05e
staibjm <staibjm@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2015-01-29 14:11:50 +0000

    Updated cmake
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@1372 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

--- prep_cmake.sh
+++ prep_cmake.sh
@@ -6,7 +6,6 @@
 prefix=${1:-"/usr/"}
 
 rm -rf build.release
-
 mkdir build.release
 cd build.release
 cmake .. -DCMAKE_INSTALL_PREFIX=$prefix
@@ -16,7 +15,5 @@ rm -rf build.debug
 mkdir build.debug
 cd build.debug
 cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_BUILD_TYPE=Debug
-cd ..
-
 cd ..
 
