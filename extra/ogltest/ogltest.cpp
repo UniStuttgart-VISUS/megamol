c691dfbb22a5e7f5f39bb266a3b538e5c8aa17b8
grottel <grottel@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2015-01-30 11:48:53 +0000

    Minimalistic external extra test
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@1374 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

---
+++ extra/ogltest/ogltest.cpp
@@ -0,0 +1,10 @@
+// ogltest.cpp : Defines the entry point for the console application.
+//
+#include "vislib/types.h"
+#include "vislib/graphics/gl/IncludeAllGL.h"
+
+
+int main(int argc, char* argv[]) {
+	return 0;
+}
+
