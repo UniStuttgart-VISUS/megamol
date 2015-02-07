f39419e0942b0c5873f53ba7ae7cfb499354d669
grottel <grottel@3ec011a9-ccf5-4689-8f2f-5f4aa021d377>
2015-02-07 10:33:48 +0000

    macro_utils.h added for convenience macros
    git-svn-id: https://svn.vis.uni-stuttgart.de/utilities/vislib/trunk@1386 3ec011a9-ccf5-4689-8f2f-5f4aa021d377

---
+++ include/vislib/macro_utils.h
@@ -0,0 +1,33 @@
+/*
+ * macro_utils.h
+ * Copyright (C) 2015 by Sebastian Grottel
+ * All rights reserved. Alle Rechte vorbehalten.
+ */
+
+#ifndef VISLIB_MACRO_UTILS_H_INCLUDED
+#define VISLIB_MACRO_UTILS_H_INCLUDED
+#pragma once
+#if defined(_WIN32) && defined(_MANAGED)
+#pragma managed(push, off)
+#endif /* defined(_WIN32) && defined(_MANAGED) */
+
+/*
+ * Helper macro to suppress compiler warnings
+ *
+ * @example: Use a template member variable internlly in a class exported by
+ *           a dll.
+ *
+ *   VISLIB_MSVC_SUPPRESS_WARNING(4261)
+ *   std::vector<my_type> my_array;
+ *
+ */
+#ifdef _MSC_VER
+#define VISLIB_MSVC_SUPPRESS_WARNING(A) __pragma(warning(suppress:A)) 
+#else /* _MSC_VER */
+#define VISLIB_MSVC_SUPPRESS_WARNING(A)
+#endif /* _MSC_VER */
+
+#if defined(_WIN32) && defined(_MANAGED)
+#pragma managed(pop)
+#endif /* defined(_WIN32) && defined(_MANAGED) */
+#endif /* VISLIB_MACRO_UTILS_H_INCLUDED */
