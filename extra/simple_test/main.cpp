2086cb2ed286e78ba5f0094daf36b434c2b2da96
grottel <grottel@77d55b08-817d-0410-979b-1fec1beb8c2e>
2015-02-09 17:20:01 +0000

    Adjustments to the CMake files for less hacks and more correctness.
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/core/trunk@707 77d55b08-817d-0410-979b-1fec1beb8c2e

--- extra/simple_test/main.cpp
+++ extra/simple_test/main.cpp
@@ -11,5 +11,9 @@ int main(int argc, char **argv) {
     unsigned short ver[4];
     ::mmcGetVersionInfo(&ver[0], &ver[1], &ver[2], &ver[3], nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
     std::cout << "Using MegaMol Core (V.:" << ver[0] << "." << ver[1] << "." << ver[2] << "." << ver[3] << ")" << std::endl;
+
+    int mod_cnt = ::mmcModuleCount();
+    std::cout << "Core backdoor reported " << mod_cnt << " Module descriptions" << std::endl;
+
     return 0;
 }
