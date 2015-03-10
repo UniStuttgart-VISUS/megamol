87edd19c63bfaf5ef1fcf7d48b6b7e508756798d
grottel <grottel@6c50cc09-817d-0410-90cf-7b22d5ef511c>
2015-03-10 13:13:12 +0000

    Cmake files extended for installation
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/frontends/console/trunk@251 6c50cc09-817d-0410-90cf-7b22d5ef511c

---
+++ extra/megamol.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# MegaMol startup script
+# Copyright 2015, http://go.visus.uni-stuttgart.de/megamol
+#
+
+# Edit this if you want to use the debug version
+LD_LIBRARY_PATH=@cfg_LIB_PATH@ @cfg_MEGAMOLCON@ "$@"
+
