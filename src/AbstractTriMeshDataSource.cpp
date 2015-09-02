acf0fe0b9d4a86a9e5a0895e267f0d3969999795
grottel <grottel@cb50be13-817d-0410-ab5c-3777bfb244a9>
2015-09-02 11:32:37 +0000

    creating new trunk based on the release v1.1 preparations
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/mmstd_trisoup/trunk@170 cb50be13-817d-0410-ab5c-3777bfb244a9

---
+++ src/AbstractTriMeshDataSource.cpp
@@ -0,0 +1,87 @@
+/*
+ * AbstractTriMeshDataSource.h
+ *
+ * Copyright (C) 2010 by Sebastian Grottel
+ * Copyright (C) 2010 by VISUS (Universitaet Stuttgart)
+ * Alle Rechte vorbehalten.
+ */
+#include "stdafx.h"
+#include "AbstractTriMeshDataSource.h"
+#include "vislib/assert.h"
+#include "vislib/sys/Log.h"
+
+using namespace megamol;
+using namespace megamol::trisoup;
+
+
+/*
+ * AbstractTriMeshDataSource::AbstractTriMeshDataSource
+ */
+AbstractTriMeshDataSource::AbstractTriMeshDataSource(void) : core::Module(),
+        objs(), mats(), bbox(-1.0f, -1.0f, -1.0f, 1.0f, 1.0f, 1.0f), datahash(0),
+        getDataSlot("getdata", "The slot publishing the loaded data") {
+
+    this->getDataSlot.SetCallback(core::misc::CallTriMeshData::ClassName(), "GetData", &AbstractTriMeshDataSource::getDataCallback);
+    this->getDataSlot.SetCallback(core::misc::CallTriMeshData::ClassName(), "GetExtent", &AbstractTriMeshDataSource::getExtentCallback);
+    this->MakeSlotAvailable(&this->getDataSlot);
+
+}
+
+
+/*
+ * AbstractTriMeshDataSource::~AbstractTriMeshDataSource
+ */
+AbstractTriMeshDataSource::~AbstractTriMeshDataSource(void) {
+    this->Release();
+    ASSERT(this->objs.IsEmpty());
+    ASSERT(this->mats.IsEmpty());
+}
+
+
+/*
+ * AbstractTriMeshDataSource::create
+ */
+bool AbstractTriMeshDataSource::create(void) {
+    // intentionally empty
+    return true;
+}
+
+
+/*
+ * AbstractTriMeshDataSource::release
+ */
+void AbstractTriMeshDataSource::release(void) {
+    this->objs.Clear();
+    this->mats.Clear();
+}
+
+
+/*
+ * AbstractTriMeshDataSource::getDataCallback
+ */
+bool AbstractTriMeshDataSource::getDataCallback(core::Call& caller) {
+    core::misc::CallTriMeshData *ctmd = dynamic_cast<core::misc::CallTriMeshData *>(&caller);
+    if (ctmd == NULL) return false;
+    this->assertData();
+
+    ctmd->SetDataHash(this->datahash);
+    ctmd->SetObjects(static_cast<unsigned int>(this->objs.Count()), this->objs.PeekElements());
+    ctmd->SetUnlocker(NULL);
+
+    return true;
+}
+
+
+/*
+ * AbstractTriMeshDataSource::getExtentCallback
+ */
+bool AbstractTriMeshDataSource::getExtentCallback(core::Call& caller) {
+    core::misc::CallTriMeshData *ctmd = dynamic_cast<core::misc::CallTriMeshData *>(&caller);
+    if (ctmd == NULL) return false;
+    this->assertData();
+
+    ctmd->SetDataHash(this->datahash);
+    ctmd->SetExtent(1, this->bbox.Left(), this->bbox.Bottom(), this->bbox.Back(), this->bbox.Right(), this->bbox.Top(), this->bbox.Front());
+
+    return true;
+}
---
+++ src/TriSoup.cpp
@@ -0,0 +1,152 @@
+/*
+ * TriSoup.cpp
+ * Copyright (C) 2009-2015 by MegaMol Team
+ * Alle Rechte vorbehalten.
+ */
+
+#include "stdafx.h"
+#include "TriSoup/TriSoup.h"
+
+#include "mmcore/api/MegaMolCore.std.h"
+#include "mmcore/utility/plugins/Plugin200Instance.h"
+#include "mmcore/versioninfo.h"
+#include "vislib/vislibversion.h"
+
+#include "TriSoupRenderer.h"
+#include "TriSoupDataSource.h"
+#include "WavefrontObjDataSource.h"
+#include "BlockVolumeMesh.h"
+#include "volumetrics/VoluMetricJob.h"
+#include "OSCBFix.h"
+#include "CoordSysMarker.h"
+#include "TrackerRendererTransform.h"
+#include "volumetrics/IsoSurface.h"
+#include "CallBinaryVolumeData.h"
+#include "CallVolumetricData.h"
+
+
+/* anonymous namespace hides this type from any other object files */
+namespace {
+    /** Implementing the instance class of this plugin */
+    class plugin_instance : public ::megamol::core::utility::plugins::Plugin200Instance {
+    public:
+        /** ctor */
+        plugin_instance(void)
+            : ::megamol::core::utility::plugins::Plugin200Instance(
+
+                /* machine-readable plugin assembly name */
+                "TriSoup",
+
+                /* human-readable plugin description */
+                "Plugin for rendering TriSoup mesh data") {
+
+            // here we could perform addition initialization
+        };
+        /** Dtor */
+        virtual ~plugin_instance(void) {
+            // here we could perform addition de-initialization
+        }
+        /** Registers modules and calls */
+        virtual void registerClasses(void) {
+
+            // register modules here:
+            this->module_descriptions.RegisterAutoDescription<megamol::trisoup::TriSoupRenderer>();
+            this->module_descriptions.RegisterAutoDescription<megamol::trisoup::TriSoupDataSource>();
+            this->module_descriptions.RegisterAutoDescription<megamol::trisoup::WavefrontObjDataSource>();
+            this->module_descriptions.RegisterAutoDescription<megamol::trisoup::BlockVolumeMesh>();
+            this->module_descriptions.RegisterAutoDescription<megamol::trisoup::volumetrics::VoluMetricJob>();
+            this->module_descriptions.RegisterAutoDescription<megamol::quartz::OSCBFix>();
+            this->module_descriptions.RegisterAutoDescription<megamol::trisoup::CoordSysMarker>();
+            this->module_descriptions.RegisterAutoDescription<megamol::trisoup::TrackerRendererTransform>();
+            this->module_descriptions.RegisterAutoDescription<megamol::trisoup::volumetrics::IsoSurface>();
+
+            // register calls here:
+            this->call_descriptions.RegisterAutoDescription<megamol::trisoup::CallBinaryVolumeData>();
+            this->call_descriptions.RegisterAutoDescription<megamol::trisoup::CallVolumetricData>();
+
+        }
+        MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_plugininstance_connectStatics
+    };
+}
+
+
+/*
+ * mmplgPluginAPIVersion
+ */
+TRISOUP_API int mmplgPluginAPIVersion(void) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgPluginAPIVersion
+}
+
+
+/*
+ * mmplgGetPluginCompatibilityInfo
+ */
+TRISOUP_API
+::megamol::core::utility::plugins::PluginCompatibilityInfo *
+mmplgGetPluginCompatibilityInfo(
+        ::megamol::core::utility::plugins::ErrorCallback onError) {
+    // compatibility information with core and vislib
+    using ::megamol::core::utility::plugins::PluginCompatibilityInfo;
+    using ::megamol::core::utility::plugins::LibraryVersionInfo;
+
+    PluginCompatibilityInfo *ci = new PluginCompatibilityInfo;
+    ci->libs_cnt = 2;
+    ci->libs = new LibraryVersionInfo[2];
+
+    SetLibraryVersionInfo(ci->libs[0], "MegaMolCore",
+        MEGAMOL_CORE_MAJOR_VER, MEGAMOL_CORE_MINOR_VER, MEGAMOL_CORE_MAJOR_REV, MEGAMOL_CORE_MINOR_REV, 0
+#if defined(DEBUG) || defined(_DEBUG)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DEBUG_BUILD
+#endif
+#if defined(MEGAMOL_CORE_DIRTY) && (MEGAMOL_CORE_DIRTY != 0)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DIRTY_BUILD
+#endif
+        );
+
+    SetLibraryVersionInfo(ci->libs[1], "vislib",
+        VISLIB_VERSION_MAJOR, VISLIB_VERSION_MINOR, VISLIB_VERSION_REVISION, VISLIB_VERSION_BUILD, 0
+#if defined(DEBUG) || defined(_DEBUG)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DEBUG_BUILD
+#endif
+#if defined(VISLIB_DIRTY_BUILD) && (VISLIB_DIRTY_BUILD != 0)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DIRTY_BUILD
+#endif
+        );
+    //
+    // If you want to test additional compatibilties, add the corresponding versions here
+    //
+
+    return ci;
+}
+
+
+/*
+ * mmplgReleasePluginCompatibilityInfo
+ */
+TRISOUP_API
+void mmplgReleasePluginCompatibilityInfo(
+        ::megamol::core::utility::plugins::PluginCompatibilityInfo* ci) {
+    // release compatiblity data on the correct heap
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgReleasePluginCompatibilityInfo(ci)
+}
+
+
+/*
+ * mmplgGetPluginInstance
+ */
+TRISOUP_API
+::megamol::core::utility::plugins::AbstractPluginInstance*
+mmplgGetPluginInstance(
+        ::megamol::core::utility::plugins::ErrorCallback onError) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgGetPluginInstance(plugin_instance, onError)
+}
+
+
+/*
+ * mmplgReleasePluginInstance
+ */
+TRISOUP_API
+void mmplgReleasePluginInstance(
+        ::megamol::core::utility::plugins::AbstractPluginInstance* pi) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgReleasePluginInstance(pi)
+}
