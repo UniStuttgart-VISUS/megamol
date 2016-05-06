03ef4fc788a7a7a4bf87869fcd751cc75682ace3
schatzkn <schatzkn@7918b674-7eb5-4f1b-901b-2027da7c8861>
2016-05-06 11:42:49 +0000

    initial commit
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/protein_calls/trunk@1 7918b674-7eb5-4f1b-901b-2027da7c8861

---
+++ src/Protein_Calls.cpp
@@ -0,0 +1,145 @@
+/*
+ * Protein_Calls.cpp
+ * Copyright (C) 2009-2015 by MegaMol Team
+ * Alle Rechte vorbehalten.
+ */
+
+#include "stdafx.h"
+#include "Protein_Calls/Protein_Calls.h"
+
+#include "mmcore/api/MegaMolCore.std.h"
+#include "mmcore/utility/plugins/Plugin200Instance.h"
+#include "mmcore/versioninfo.h"
+#include "vislib/vislibversion.h"
+
+
+/* anonymous namespace hides this type from any other object files */
+namespace {
+    /** Implementing the instance class of this plugin */
+    class plugin_instance : public ::megamol::core::utility::plugins::Plugin200Instance {
+    public:
+        /** ctor */
+        plugin_instance(void)
+            : ::megamol::core::utility::plugins::Plugin200Instance(
+
+                /* machine-readable plugin assembly name */
+                "Protein_Calls", // TODO: Change this!
+
+                /* human-readable plugin description */
+                "Describing Protein_Calls (TODO: Change this!)") {
+
+            // here we could perform addition initialization
+        };
+        /** Dtor */
+        virtual ~plugin_instance(void) {
+            // here we could perform addition de-initialization
+        }
+        /** Registers modules and calls */
+        virtual void registerClasses(void) {
+
+            // register modules here:
+
+            //
+            // TODO: Register your plugin's modules here
+            // like:
+            //   this->module_descriptions.RegisterAutoDescription<megamol::Protein_Calls::MyModule1>();
+            //   this->module_descriptions.RegisterAutoDescription<megamol::Protein_Calls::MyModule2>();
+            //   ...
+            //
+
+            // register calls here:
+
+            //
+            // TODO: Register your plugin's calls here
+            // like:
+            //   this->call_descriptions.RegisterAutoDescription<megamol::Protein_Calls::MyCall1>();
+            //   this->call_descriptions.RegisterAutoDescription<megamol::Protein_Calls::MyCall2>();
+            //   ...
+            //
+
+        }
+        MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_plugininstance_connectStatics
+    };
+}
+
+
+/*
+ * mmplgPluginAPIVersion
+ */
+PROTEIN_CALLS_API int mmplgPluginAPIVersion(void) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgPluginAPIVersion
+}
+
+
+/*
+ * mmplgGetPluginCompatibilityInfo
+ */
+PROTEIN_CALLS_API
+::megamol::core::utility::plugins::PluginCompatibilityInfo *
+mmplgGetPluginCompatibilityInfo(
+        ::megamol::core::utility::plugins::ErrorCallback onError) {
+    // compatibility information with core and vislib
+    using ::megamol::core::utility::plugins::PluginCompatibilityInfo;
+    using ::megamol::core::utility::plugins::LibraryVersionInfo;
+
+    PluginCompatibilityInfo *ci = new PluginCompatibilityInfo;
+    ci->libs_cnt = 2;
+    ci->libs = new LibraryVersionInfo[2];
+
+    SetLibraryVersionInfo(ci->libs[0], "MegaMolCore",
+        MEGAMOL_CORE_MAJOR_VER, MEGAMOL_CORE_MINOR_VER, MEGAMOL_CORE_MAJOR_REV, MEGAMOL_CORE_MINOR_REV, 0
+#if defined(DEBUG) || defined(_DEBUG)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DEBUG_BUILD
+#endif
+#if defined(MEGAMOL_CORE_DIRTY) && (MEGAMOL_CORE_DIRTY != 0)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DIRTY_BUILD
+#endif
+        );
+
+    SetLibraryVersionInfo(ci->libs[1], "vislib",
+        VISLIB_VERSION_MAJOR, VISLIB_VERSION_MINOR, VISLIB_VERSION_REVISION, VISLIB_VERSION_BUILD, 0
+#if defined(DEBUG) || defined(_DEBUG)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DEBUG_BUILD
+#endif
+#if defined(VISLIB_DIRTY_BUILD) && (VISLIB_DIRTY_BUILD != 0)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DIRTY_BUILD
+#endif
+        );
+    //
+    // If you want to test additional compatibilties, add the corresponding versions here
+    //
+
+    return ci;
+}
+
+
+/*
+ * mmplgReleasePluginCompatibilityInfo
+ */
+PROTEIN_CALLS_API
+void mmplgReleasePluginCompatibilityInfo(
+        ::megamol::core::utility::plugins::PluginCompatibilityInfo* ci) {
+    // release compatiblity data on the correct heap
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgReleasePluginCompatibilityInfo(ci)
+}
+
+
+/*
+ * mmplgGetPluginInstance
+ */
+PROTEIN_CALLS_API
+::megamol::core::utility::plugins::AbstractPluginInstance*
+mmplgGetPluginInstance(
+        ::megamol::core::utility::plugins::ErrorCallback onError) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgGetPluginInstance(plugin_instance, onError)
+}
+
+
+/*
+ * mmplgReleasePluginInstance
+ */
+PROTEIN_CALLS_API
+void mmplgReleasePluginInstance(
+        ::megamol::core::utility::plugins::AbstractPluginInstance* pi) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgReleasePluginInstance(pi)
+}
