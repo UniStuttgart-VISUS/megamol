efff21495c4d1ac1a264fb6a64c198ef4cb852c2
grottel <grottel@f95d2973-9550-45a5-b795-f9f66c6b6f94>
2017-03-15 16:06:29 +0000

    Bug in MMGDDWriter fixed (last frame was written twice, corrupting the frame table)
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/mmstd_datatools/trunk@168 f95d2973-9550-45a5-b795-f9f66c6b6f94

--- src/io/MMGDDWriter.cpp
+++ src/io/MMGDDWriter.cpp
@@ -70,6 +70,8 @@ bool io::MMGDDWriter::run(void) {
         return false;
     }
 
+    Log::DefaultLog.WriteInfo("MMGDD Start Writing ...\n");
+
     file.Write("MMGDD", 6);
     uint16_t version = 100;
     file.Write(&version, 2);
@@ -80,19 +82,46 @@ bool io::MMGDDWriter::run(void) {
         file.Write(&frameCnt, 8);
     }
 
-    for (uint32_t i = 0; i <= frameCnt; i++) {
+    for (uint32_t i = 0; i < frameCnt; i++) {
         frameOffset = static_cast<UINT64>(file.Tell());
         file.Seek(12 + i * 8);
         file.Write(&frameOffset, 8);
         file.Seek(frameOffset);
 
         gdc->SetFrameID(i);
+        unsigned char flags = 0;
         if (!(*gdc)(GraphDataCall::GET_DATA)) {
             Log::DefaultLog.WriteMsg(Log::LEVEL_ERROR, "Unable to fetch data for frame %u. skipping.", i);
+            file.Write(&flags, 1);
             continue;
         }
+        if (gdc->FrameID() != i) {
+            Log::DefaultLog.WriteWarn("Wrong frame data answered: %u requested, %u received", i, gdc->FrameID());
+        }
 
-        unsigned char flags = gdc->IsDirected() ? 1 : 0;
+#define __WITH_VALUE_SUMMARY 0
+
+#if __WITH_VALUE_SUMMARY
+        unsigned int minVal = 1000000, maxVal = 0;
+        for (const auto& e : *gdc) {
+            if (minVal > e.i1) minVal = e.i1;
+            if (maxVal < e.i1) maxVal = e.i1;
+            if (minVal > e.i2) minVal = e.i2;
+            if (maxVal < e.i2) maxVal = e.i2;
+        }
+#endif
+        Log::DefaultLog.WriteInfo("MMGDD frame #%u: %u edges"
+#if __WITH_VALUE_SUMMARY
+            ", indices [%u %u]"
+#endif
+            "\n",
+            i, static_cast<unsigned int>(gdc->GetEdgeCount())
+#if __WITH_VALUE_SUMMARY
+            , minVal, maxVal
+#endif
+        );
+
+        flags = gdc->IsDirected() ? 1 : 0;
         file.Write(&flags, 1);
 
         file.Write(gdc->GetEdgeData(), gdc->GetEdgeCount() * sizeof(GraphDataCall::edge));
@@ -110,6 +139,8 @@ bool io::MMGDDWriter::run(void) {
     file.Write(&version, 1);
 
     file.Close();
+
+    Log::DefaultLog.WriteInfo("MMGDD Writing Completed...\n");
 
     return true;
 }
