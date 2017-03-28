a610b7cd32859814e9a690c10409cf25f15632f2
mueller <mueller@48bde7f2-199b-42c5-ada7-780c78a47d76>
2017-03-28 14:46:07 +0000

    Added project manipulation script.
    git-svn-id: https://svn.vis.uni-stuttgart.de/projects/megamol/plugins/cinematiccamera/branches/skybox@64 48bde7f2-199b-42c5-ada7-780c78a47d76

---
+++ New-DomeProject.ps1
@@ -0,0 +1,86 @@
+[CmdletBinding(SupportsShouldProcess = $true)]
+param(#[string] $Project = '.\cinematiccam02-2views-lasercross.mmprj',
+    [Parameter(Mandatory=$true)] [string] $Project,
+    [int] $From,
+    [int] $To,
+    [float] $TimeStep)
+
+
+# Gets a parameter tag or creates a new one with the specified value.
+function Get-Param($module, $name, $value) {
+    $retval = $module.SelectSingleNode("param[@name=`"$name`"]")
+
+    if (!$retval) {
+        Write-Verbose "Appending mandatory parameter `"$name`" ..."
+        $p = $module.OwnerDocument.CreateElement('param')
+        $module.AppendChild($p) | Out-Null
+
+        $a = $module.OwnerDocument.CreateAttribute('name')
+        $a.Value = $name
+        $p.Attributes.Append($a) | Out-Null
+
+        $a = $module.OwnerDocument.CreateAttribute('value')
+        $a.Value = $value
+        $p.Attributes.Append($a) | Out-Null
+
+        $retval = $p
+    }
+
+    return $retval
+}
+
+
+# Possible values for 'skyboxSide'.
+$sides = @('Top', 'Front', 'Left', 'Back', 'Right', 'Bottom')
+
+# Read the original project file.
+[xml] $xml = gc $Project
+
+# Get the view module we will edit and its containing view.
+$viewMod = $xml.SelectSingleNode('//module[@class="CinematicView"]')
+if (!$viewMod) {
+    throw 'The project must contain a CinematicView used for rendering the scene.'
+}
+
+$view = $viewMod.ParentNode
+
+# Get the screenshooter element to edit.
+$shootMod = $view.SelectSingleNode('module[@class="ScreenShooter"]')
+if (!$shootMod) {
+    throw 'The project must contain a ScreenShooter which saves the output to disk.'
+}
+
+# Get the parameters we need to modify.
+$paramSide = Get-Param $viewMod 'cinematicCam::skyboxSide' $sides[0]
+$paramFile = Get-Param $shootMod 'filename' ''
+$paramInst = Get-Param $shootMod 'view' 
+
+$baseName = Split-Path -Leaf $Project
+$ext = [System.IO.Path]::GetExtension($Project)
+for ($i = 0; $i -lt $sides.Length; ++$i) {
+    # Note: The identifier of the side in the project name is a numeric value,
+    # which allows us to create parametric sweep on the cluster.
+    $fileName = [System.IO.Path]::ChangeExtension($Project, "$i$ext")
+    # Determine side human-readable name of the side for easier handling of the
+    # output in post-processing of the video material.
+    $sideName = $sides[$i].ToLower()
+    
+    $paramSide.value = $sides[$i]
+    $paramFile.value = [System.IO.Path]::ChangeExtension($baseName, "$sideName.png")
+    if ($PSCmdlet.ShouldProcess($filePath, 'Save')) {
+        New-Item $fileName -Force -ItemType File
+        $xml.Save((Resolve-Path $fileName))
+    }
+}
+
+$hpcSweep = [System.IO.Path]::ChangeExtension($baseName, "*$ext")
+$hpcCmd = @"
+`$workDir = '\\vesta1\Entwicklung\sfbdome'
+`$head = 'vesta1'
+`$megaMol = 'bin\x64\Release\MegaMolCon.exe'
+`$job = New-HpcJob -Scheduler `$head -Name '$baseName'
+Add-HpcTask -Scheduler `$head -Job `$job -Type ParametricSweep -Start 0 -End $($sides.Length - 1) -WorkDir `$workDir -Command '`$megaMol -p $hpcSweep' -i $($view.name) $($paramInst.value)
+Submit-HpcJob -Scheduler `$head -Id `$job
+"@
+
+Write-Output $hpcCmd
