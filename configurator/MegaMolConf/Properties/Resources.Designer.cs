8d491ec7ea71187f9ffa381e1dd828590d562a43
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2017-09-25 18:16:34 +0200

    Fixed designer.cs problems and added live connection files to project

--- configurator/MegaMolConf/Properties/Resources.Designer.cs
+++ configurator/MegaMolConf/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MegaMolConf.MegaMolConf.Properties {
+namespace MegaMolConf.Properties {
     using System;
     
     
