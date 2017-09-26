75381eef2c6498c0befcfb0b655ac3a983a918c0
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2017-09-26 10:21:21 +0200

    ResourceManager works at runtime as well
    removed AssemblyInfo from version control
    fixed dependencies of configurator

--- configurator/MegaMolConf/Properties/Resources.Designer.cs
+++ configurator/MegaMolConf/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace MegaMolConf.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MegaMolConf.MegaMolConf.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MegaMolConf.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
