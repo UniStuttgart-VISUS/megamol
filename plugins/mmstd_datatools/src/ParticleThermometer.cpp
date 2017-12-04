0811bf73bcddd902a5193b9250f84fb2b3e18ae7
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2017-12-04 17:25:37 +0100

    ParticleThermometer alpha
    no idea what it currently does
    TODO: check against nieghborhood implementation
    TODO: compute actual differences

--- plugins/mmstd_datatools/src/ParticleThermometer.cpp
+++ plugins/mmstd_datatools/src/ParticleThermometer.cpp
@@ -7,7 +7,8 @@
 #include "stdafx.h"
 #include "ParticleThermometer.h"
 #include "mmcore/param/BoolParam.h"
-#include "nanoflann.hpp"
+#include "mmcore/param/FloatParam.h"
+#include "vislib/sys/Log.h"
 #include <cstdint>
 #include <algorithm>
 #include <cfloat>
@@ -16,15 +17,18 @@
 using namespace megamol;
 using namespace megamol::stdplugin;
 
-
 /*
  * datatools::ParticleThermometer::ParticleThermometer
  */
 datatools::ParticleThermometer::ParticleThermometer(void)
         : cyclXSlot("cyclX", "Considers cyclic boundary conditions in X direction"),
         cyclYSlot("cyclY", "Considers cyclic boundary conditions in Y direction"),
-        cyclZSlot("cyclZ", "Considdrs cyclic boundary conditions in Z direction"),
-        datahash(0), time(0), newColors(), minCol(0.0f), maxCol(1.0f) {
+        cyclZSlot("cyclZ", "Considers cyclic boundary conditions in Z direction"),
+        radiusSlot("radius", "the radius in which to look for neighbors"),
+        outDataSlot("outData", "Provides colors based on local particle temperature"),
+        inDataSlot("inData", "Takes the directional particle data"),
+        datahash(0), lastTime(-1), newColors(), minCol(0.0f), maxCol(1.0f), radius(-1.0),
+        allParts(), particleTree(nullptr), myPts(nullptr) {
 
     this->cyclXSlot.SetParameter(new core::param::BoolParam(true));
     this->MakeSlotAvailable(&this->cyclXSlot);
@@ -34,6 +38,18 @@ datatools::ParticleThermometer::ParticleThermometer(void)
 
     this->cyclZSlot.SetParameter(new core::param::BoolParam(true));
     this->MakeSlotAvailable(&this->cyclZSlot);
+
+    this->radiusSlot.SetParameter(new core::param::FloatParam(2.0));
+    this->MakeSlotAvailable(&this->radiusSlot);
+
+    this->outDataSlot.SetCallback(megamol::core::moldyn::DirectionalParticleDataCall::ClassName(), "GetData", &ParticleThermometer::getDataCallback);
+    this->outDataSlot.SetCallback(megamol::core::moldyn::DirectionalParticleDataCall::ClassName(), "GetExtent", &ParticleThermometer::getExtentCallback);
+    this->outDataSlot.SetCallback(megamol::core::moldyn::MultiParticleDataCall::ClassName(), "GetData", &ParticleThermometer::getDataCallback);
+    this->outDataSlot.SetCallback(megamol::core::moldyn::MultiParticleDataCall::ClassName(), "GetExtent", &ParticleThermometer::getExtentCallback);
+    this->MakeSlotAvailable(&this->outDataSlot);
+
+    this->inDataSlot.SetCompatibleCall<megamol::core::moldyn::DirectionalParticleDataCallDescription>();
+    this->MakeSlotAvailable(&this->inDataSlot);
 }
 
 
@@ -56,4 +72,288 @@ bool datatools::ParticleThermometer::create(void) {
 * datatools::ParticleThermometer::release
 */
 void datatools::ParticleThermometer::release(void) {
+}
+
+bool datatools::ParticleThermometer::assertData(core::moldyn::DirectionalParticleDataCall *in,
+    core::moldyn::MultiParticleDataCall *outMPDC, core::moldyn::DirectionalParticleDataCall *outDPDC) {
+
+    using megamol::core::moldyn::DirectionalParticleDataCall;
+    using megamol::core::moldyn::MultiParticleDataCall;
+
+    megamol::core::AbstractGetData3DCall *out;
+    if (outMPDC != nullptr) out = outMPDC;
+    if (outDPDC != nullptr) out = outDPDC;
+
+    unsigned int time = out->FrameID();
+
+    if (this->lastTime != time || this->datahash != in->DataHash()) {
+        // load previous Frame
+        in->SetFrameID(time, true);
+
+        if (!(*in)(0)) {
+            vislib::sys::Log::DefaultLog.WriteError("ParticleThermometer: could not get frame (%u)", time);
+            return false;
+        }
+
+        size_t totalParts = 0;
+        size_t plc = in->GetParticleListCount();
+        for (unsigned int i = 0; i < plc; i++) {
+            if (in->AccessParticles(i).GetVertexDataType() == DirectionalParticleDataCall::Particles::VertexDataType::VERTDATA_FLOAT_XYZ
+                || in->AccessParticles(i).GetVertexDataType() == DirectionalParticleDataCall::Particles::VertexDataType::VERTDATA_FLOAT_XYZR)
+                totalParts += in->AccessParticles(i).GetCount();
+        }
+
+        this->newColors.resize(totalParts);
+
+        allParts.reserve(totalParts);
+
+        // we could now filter particles according to something. but currently we need not.
+        size_t allpartcnt = 0;
+        for (unsigned int pli = 0; pli < plc; pli++) {
+            auto& pl = in->AccessParticles(pli);
+            if ((pl.GetVertexDataType() != DirectionalParticleDataCall::Particles::VERTDATA_FLOAT_XYZ)
+                && (pl.GetVertexDataType() != DirectionalParticleDataCall::Particles::VERTDATA_FLOAT_XYZR)) {
+                continue;
+            }
+
+            unsigned int vert_stride = 0;
+            if (pl.GetVertexDataType() == DirectionalParticleDataCall::Particles::VertexDataType::VERTDATA_FLOAT_XYZ) vert_stride = 12;
+            else if (pl.GetVertexDataType() == DirectionalParticleDataCall::Particles::VertexDataType::VERTDATA_FLOAT_XYZR) vert_stride = 16;
+            else continue;
+            vert_stride = std::max<unsigned int>(vert_stride, pl.GetVertexDataStride());
+            const unsigned char *vert = static_cast<const unsigned char*>(pl.GetVertexData());
+
+            int part_cnt = static_cast<int>(pl.GetCount());
+
+            for (int part_i = 0; part_i < part_cnt; ++part_i) {
+                const float *v = reinterpret_cast<const float *>(vert + (part_i * vert_stride));
+                allParts.push_back(allpartcnt + part_i);
+            }
+            allpartcnt += static_cast<size_t>(pl.GetCount());
+        }
+
+        // allocate nanoflann data structures for border
+        assert(allpartcnt == totalParts);
+        // TODO: can I really keep the Kd-Tree when cyclicness is changed? I'd say why not. But who knows the implementation!
+        this->myPts = std::make_shared<directionalPointcloud>(in, allParts,
+            this->cyclXSlot.Param<core::param::BoolParam>()->Value(), this->cyclYSlot.Param<core::param::BoolParam>()->Value(), this->cyclZSlot.Param<core::param::BoolParam>()->Value());
+
+        particleTree = std::make_shared<my_kd_tree_t>(3 /* dim */, *myPts, nanoflann::KDTreeSingleIndexAdaptorParams(10 /* max leaf */));
+        particleTree->buildIndex();
+    }
+    float theRadius = this->radiusSlot.Param<core::param::FloatParam>()->Value();
+    if (radius != theRadius) {
+        size_t allpartcnt = 0;
+        size_t cursor = 0;
+        float v[3];
+        float v2[3];
+        float vel[3];
+        float vel2[3];
+        size_t plc = in->GetParticleListCount();
+        std::vector<std::pair<size_t, float> > ret_matches;
+        nanoflann::SearchParams params;
+        params.sorted = false;
+        // TODO: can I really keep the Kd-Tree when cyclicness is changed? I'd say why not. But who knows the implementation! see above.
+        this->myPts->SetCyclicBoundary(this->cyclXSlot.Param<core::param::BoolParam>()->Value(),
+            this->cyclYSlot.Param<core::param::BoolParam>()->Value(), this->cyclZSlot.Param<core::param::BoolParam>()->Value());
+
+        for (unsigned int pli = 0; pli < plc; pli++) {
+            auto& pl = in->AccessParticles(pli);
+            if ((pl.GetVertexDataType() != DirectionalParticleDataCall::Particles::VERTDATA_FLOAT_XYZ)
+                && (pl.GetVertexDataType() != DirectionalParticleDataCall::Particles::VERTDATA_FLOAT_XYZR)) {
+                continue;
+            }
+            unsigned int vert_stride = 0;
+            if (pl.GetVertexDataType() == DirectionalParticleDataCall::Particles::VERTDATA_FLOAT_XYZ) vert_stride = 12;
+            else if (pl.GetVertexDataType() == DirectionalParticleDataCall::Particles::VERTDATA_FLOAT_XYZR) vert_stride = 16;
+            else continue;
+            vert_stride = std::max<unsigned int>(vert_stride, pl.GetVertexDataStride());
+            const unsigned char *vert = static_cast<const unsigned char*>(pl.GetVertexData());
+
+            size_t part_cnt = pl.GetCount();
+            for (size_t part_i = 0; part_i < part_cnt; ++part_i) {
+                // did we consider this particle?
+                while (allParts[cursor] < allpartcnt + part_i) cursor++;
+                if (allParts[cursor] == allpartcnt + part_i) {
+                    const float *v = reinterpret_cast<const float *>(vert + (part_i * vert_stride));
+                    const float *v2 = myPts->get_position(allpartcnt + part_i);
+
+                    particleTree->radiusSearch(v, theRadius, ret_matches, params);
+                    // does the query return the point itself? YES!
+                    for (int i = 0; i < ret_matches.size(); ++i) {
+                        if (ret_matches[i].first == allpartcnt + part_i) {
+                            // this is me
+                        } else {
+                            // these are others
+                            // add to the running average of velocities
+                        }
+                    }
+                } else {
+                    newColors[allpartcnt + part_i] = 0.0f;
+                }
+            }
+            allpartcnt += part_cnt;
+        }
+
+
+        radius = theRadius;
+    }
+
+    // now the colors are known, inject them
+    in->SetUnlocker(nullptr, false);
+    in->Unlock();
+
+#pragma region oldstuff
+        //// final computation
+        //bool cycl_x = this->cyclXSlot.Param<megamol::core::param::BoolParam>()->Value();
+        //bool cycl_y = this->cyclYSlot.Param<megamol::core::param::BoolParam>()->Value();
+        //bool cycl_z = this->cyclZSlot.Param<megamol::core::param::BoolParam>()->Value();
+        //auto bbox = dat.AccessBoundingBoxes().ObjectSpaceBBox();
+        //bbox.EnforcePositiveSize(); // paranoia
+        //auto bbox_cntr = bbox.CalcCenter();
+
+        //allpartcnt = 0;
+        //for (unsigned int pli = 0; pli < plc; pli++) {
+        //    auto& pl = dat.AccessParticles(pli);
+        //    if (pl.GetColourDataType() != SimpleSphericalParticles::COLDATA_FLOAT_I) continue;
+        //    if ((pl.GetVertexDataType() != SimpleSphericalParticles::VERTDATA_FLOAT_XYZ)
+        //        && (pl.GetVertexDataType() != SimpleSphericalParticles::VERTDATA_FLOAT_XYZR)) {
+        //        continue;
+        //    }
+
+        //    unsigned int vert_stride = 0;
+        //    if (pl.GetVertexDataType() == SimpleSphericalParticles::VERTDATA_FLOAT_XYZ) vert_stride = 12;
+        //    else if (pl.GetVertexDataType() == SimpleSphericalParticles::VERTDATA_FLOAT_XYZR) vert_stride = 16;
+        //    else continue;
+        //    vert_stride = std::max<unsigned int>(vert_stride, pl.GetVertexDataStride());
+        //    const unsigned char *vert = static_cast<const unsigned char*>(pl.GetVertexData());
+
+        //    int part_cnt = static_cast<int>(pl.GetCount());
+        //    const unsigned char *col = static_cast<const unsigned char*>(pl.GetColourData());
+        //    unsigned int col_stride = std::max<unsigned int>(pl.GetColourDataStride(), sizeof(float));
+
+        //    for (int part_i = 0; part_i < part_cnt; ++part_i) {
+        //        float c = *reinterpret_cast<const float *>(col + (part_i * col_stride));
+        //        const float *v = reinterpret_cast<const float *>(vert + (part_i * vert_stride));
+
+        //        if ((-border_epsilon < c) && (c < border_epsilon)) {
+        //            c = 0.0f;
+        //        } else {
+        //            float q[3];
+        //            float dist, distsq = static_cast<float>(DBL_MAX);
+        //            my_kd_tree_t& tree = (c < 0.0f) ? posTree : negTree;
+
+        //            for (int x_s = 0; x_s < (cycl_x ? 2 : 1); ++x_s) {
+        //                for (int y_s = 0; y_s < (cycl_y ? 2 : 1); ++y_s) {
+        //                    for (int z_s = 0; z_s < (cycl_z ? 2 : 1); ++z_s) {
+
+        //                        q[0] = v[0];
+        //                        q[1] = v[1];
+        //                        q[2] = v[2];
+        //                        if (x_s > 0) q[0] = v[0] + ((v[0] > bbox_cntr.X()) ? -bbox.Width() : bbox.Width());
+        //                        if (y_s > 0) q[1] = v[1] + ((v[1] > bbox_cntr.Y()) ? -bbox.Height() : bbox.Height());
+        //                        if (z_s > 0) q[2] = v[2] + ((v[2] > bbox_cntr.Z()) ? -bbox.Depth() : bbox.Depth());
+
+        //                        size_t n_idx;
+        //                        float n_distsq;
+        //                        tree.knnSearch(q, 1, &n_idx, &n_distsq);
+        //                        if (n_distsq < distsq) distsq = n_distsq;
+
+        //                    }
+        //                }
+        //            }
+
+        //            dist = sqrt(distsq);
+        //            if (c < 0.0f) dist = -dist;
+        //            c = static_cast<float>(dist);
+        //        }
+
+        //        if (c < this->minCol) this->minCol = c;
+        //        if (c > this->maxCol) this->maxCol = c;
+
+        //        this->newColors[allpartcnt + part_i] = c;
+        //    }
+
+        //    allpartcnt += static_cast<size_t>(part_cnt);
+        //}
+#pragma endregion
+
+    if (outMPDC != nullptr) {
+        outMPDC->SetParticleListCount(in->GetParticleListCount());
+        for (unsigned int i = 0; i < in->GetParticleListCount(); ++i) {
+            auto &pl = in->AccessParticles(i);
+            outMPDC->AccessParticles(i).SetCount(pl.GetCount());
+            outMPDC->AccessParticles(i).SetVertexData(pl.GetVertexDataType(), pl.GetVertexData(), pl.GetVertexDataStride());
+            outMPDC->AccessParticles(i).SetColourData(core::moldyn::MultiParticleDataCall::Particles::COLDATA_FLOAT_I, 
+                this->newColors.data(), 0);
+        }
+    } else if (outDPDC != nullptr) {
+        outDPDC->SetParticleListCount(in->GetParticleListCount());
+        for (unsigned int i = 0; i < in->GetParticleListCount(); ++i) {
+            auto &pl = in->AccessParticles(i);
+            outDPDC->AccessParticles(i).SetCount(pl.GetCount());
+            outDPDC->AccessParticles(i).SetVertexData(pl.GetVertexDataType(), pl.GetVertexData(), pl.GetVertexDataStride());
+            outDPDC->AccessParticles(i).SetColourData(core::moldyn::DirectionalParticleDataCall::Particles::COLDATA_FLOAT_I,
+                this->newColors.data(), 0);
+            outDPDC->AccessParticles(i).SetDirData(pl.GetDirDataType(), pl.GetDirData(), pl.GetDirDataStride());
+        }
+    }
+    this->datahash = in->DataHash();
+    this->lastTime = time;
+    out->SetUnlocker(in->GetUnlocker());
+    return true;
+}
+
+
+bool datatools::ParticleThermometer::getExtentCallback(megamol::core::Call& c) {
+    using megamol::core::moldyn::MultiParticleDataCall;
+    using megamol::core::moldyn::DirectionalParticleDataCall;
+
+    DirectionalParticleDataCall *outDpdc = dynamic_cast<DirectionalParticleDataCall*>(&c);
+    MultiParticleDataCall *outMpdc = dynamic_cast<MultiParticleDataCall*>(&c);
+    if (outMpdc == nullptr && outDpdc == nullptr) return false;
+
+    DirectionalParticleDataCall *inDpdc = this->inDataSlot.CallAs<DirectionalParticleDataCall>();
+    if (inDpdc == nullptr) return false;
+
+    megamol::core::AbstractGetData3DCall *out;
+    if (outMpdc != nullptr) out = outMpdc;
+    if (outDpdc != nullptr) out = outDpdc;
+
+    //if (!this->assertData(inMpdc, outDpdc)) return false;
+    inDpdc->SetFrameID(out->FrameID(), true);
+    if (!(*inDpdc)(1)) {
+        vislib::sys::Log::DefaultLog.WriteError("ParticleThermometer: could not get current frame extents (%u)", out->FrameID());
+        return false;
+    }
+    out->AccessBoundingBoxes().SetObjectSpaceBBox(inDpdc->GetBoundingBoxes().ObjectSpaceBBox());
+    out->AccessBoundingBoxes().SetObjectSpaceClipBox(inDpdc->GetBoundingBoxes().ObjectSpaceClipBox());
+    if (inDpdc->FrameCount() < 1) {
+        vislib::sys::Log::DefaultLog.WriteError("ParticleThermometer: no frame data!");
+        return false;
+    }
+    out->SetFrameCount(inDpdc->FrameCount());
+    // TODO: what am I actually doing here
+    inDpdc->SetUnlocker(nullptr, false);
+    inDpdc->Unlock();
+
+    return true;
+}
+
+bool datatools::ParticleThermometer::getDataCallback(megamol::core::Call& c) {
+    using megamol::core::moldyn::MultiParticleDataCall;
+    using megamol::core::moldyn::DirectionalParticleDataCall;
+
+    DirectionalParticleDataCall *outDpdc = dynamic_cast<DirectionalParticleDataCall*>(&c);
+    MultiParticleDataCall *outMpdc = dynamic_cast<MultiParticleDataCall*>(&c);
+    if (outMpdc == nullptr && outDpdc == nullptr) return false;
+
+    DirectionalParticleDataCall *inDpdc = this->inDataSlot.CallAs<DirectionalParticleDataCall>();
+    if (inDpdc == nullptr) return false;
+
+    if (!this->assertData(inDpdc, outMpdc, outDpdc)) return false;
+
+    //inMpdc->Unlock();
+
+    return true;
 }
--- plugins/mmstd_datatools/src/ParticleThermometer.h
+++ plugins/mmstd_datatools/src/ParticleThermometer.h
@@ -10,9 +10,12 @@
 #pragma once
 
 #include "mmcore/param/ParamSlot.h"
+#include "mmcore/CalleeSlot.h"
+#include "mmcore/CallerSlot.h"
 #include "mmcore/Module.h"
+#include "mmcore/moldyn/DirectionalParticleDataCall.h"
 #include <vector>
-
+#include "nanoflann.hpp"
 
 namespace megamol {
 namespace stdplugin {
@@ -45,6 +48,24 @@ namespace datatools {
         /** Dtor */
         virtual ~ParticleThermometer(void);
 
+        /**
+        * Called when the data is requested by this module
+        *
+        * @param c The incoming call
+        *
+        * @return True on success
+        */
+        bool getDataCallback(megamol::core::Call& c);
+
+        /**
+        * Called when the extend information is requested by this module
+        *
+        * @param c The incoming call
+        *
+        * @return True on success
+        */
+        bool getExtentCallback(megamol::core::Call& c);
+
     protected:
 
         /** Lazy initialization of the module */
@@ -55,13 +76,192 @@ namespace datatools {
 
     private:
 
+        class directionalPointcloud {
+        private:
+
+            megamol::core::moldyn::DirectionalParticleDataCall *dat;
+            std::vector<size_t> &indices;
+            bool cycleX, cycleY, cycleZ;
+
+        public:
+
+            typedef float coord_t;
+
+            directionalPointcloud(megamol::core::moldyn::DirectionalParticleDataCall *dat, std::vector<size_t> &indices, bool cycleX, bool cycleY, bool cycleZ) : dat(dat), indices(indices),
+                cycleX(cycleX), cycleY(cycleY), cycleZ(cycleZ) {
+                // intentionally empty
+            }
+            ~directionalPointcloud() {
+                // intentionally empty
+            }
+
+            void SetCyclicBoundary(bool cycleX, bool cycleY, bool cycleZ) {
+                this->cycleX = cycleX;
+                this->cycleY = cycleY;
+                this->cycleZ = cycleZ;
+            }
+
+            // Must return the number of data points
+            inline size_t kdtree_get_point_count() const {
+                return indices.size();
+            }
+
+            // Returns the distance between the vector "p1[0:size-1]" and the data point with index "idx_p2" stored in the class:
+            inline coord_t kdtree_distance(const coord_t *p1, const size_t idx_p2, size_t /*size*/) const {
+                float const *p2 = get_position(idx_p2);
+
+                //const coord_t d0 = p1[0] - p2[0];
+                //const coord_t d1 = p1[1] - p2[1];
+                //const coord_t d2 = p1[2] - p2[2];
+
+                coord_t dx, dy, dz;
+                const auto& bbox = dat->AccessBoundingBoxes().ObjectSpaceBBox();
+                coord_t width = bbox.Width();
+                coord_t height = bbox.Height();
+                coord_t depth = bbox.Depth();
+
+                // dr = np.remainder(r1 - r2 + L/2., L) - L/2.
+                // remainder = x1 - floor(x1 / x2) * x2
+                dx = p2[0] - p1[0];
+                if (this->cycleX && fabs(dx) > width / 2) {
+                    float x1 = p1[0] - p2[0] + width / 2;
+                    dx = x1 - floor(x1 / width) * width;
+                    dx -= width / 2;
+                }
+                dy = p2[1] - p1[1];
+                if (this->cycleY && fabs(dy) > height / 2) {
+                    float y1 = p1[1] - p2[1] + height / 2;
+                    dy = y1 - floor(y1 / height) * height;
+                    dy -= height / 2;
+                }
+                dz = p2[2] - p1[2];
+                if (this->cycleZ && fabs(dz) > depth / 2) {
+                    float z1 = p1[2] - p2[2] + depth / 2;
+                    dz = z1 - floor(z1 / depth) * depth;
+                    dz -= depth / 2;
+                }
+
+                //return d0 * d0 + d1 * d1 + d2 * d2;
+                return dx * dx + dy*dy + dz*dz;
+            }
+
+            // Returns the dim'th component of the idx'th point in the class:
+            // Since this is inlined and the "dim" argument is typically an immediate value, the
+            //  "if/else's" are actually solved at compile time.
+            inline coord_t kdtree_get_pt(const size_t idx, int dim) const {
+                assert((dim >= 0) && (dim < 3));
+                return get_position(idx)[dim];
+            }
+
+            // Optional bounding-box computation: return false to default to a standard bbox computation loop.
+            //   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it again.
+            //   Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
+            template <class BBOX>
+            bool kdtree_get_bbox(BBOX& bb) const {
+                //return false;
+
+                assert(bb.size() == 3);
+                const auto& cbox = dat->AccessBoundingBoxes().ObjectSpaceBBox();
+                bb[0].low = cbox.Left();
+                bb[0].high = cbox.Right();
+                bb[1].low = cbox.Bottom();
+                bb[1].high = cbox.Top();
+                bb[2].low = cbox.Back();
+                bb[2].high = cbox.Front();
+                return true;
+            }
+
+        //private:
+
+            // TODO wow this is inefficient. think of something to avoid deferring the decision...
+            inline const coord_t* get_position(size_t index) const {
+                using megamol::core::moldyn::DirectionalParticles;
+
+                unsigned int plc = dat->GetParticleListCount();
+                for (unsigned int pli = 0; pli < plc; pli++) {
+                    auto& pl = dat->AccessParticles(pli);
+                    if ((pl.GetVertexDataType() != DirectionalParticles::VERTDATA_FLOAT_XYZ)
+                        && (pl.GetVertexDataType() != DirectionalParticles::VERTDATA_FLOAT_XYZR)
+                        && (pl.GetDirDataType() != DirectionalParticles::DIRDATA_FLOAT_XYZ)) {
+                        continue;
+                    }
+
+                    if (index < pl.GetCount()) {
+                        unsigned int vert_stride = 0;
+                        if (pl.GetVertexDataType() == DirectionalParticles::VERTDATA_FLOAT_XYZ) vert_stride = 12;
+                        else if (pl.GetVertexDataType() == DirectionalParticles::VERTDATA_FLOAT_XYZR) vert_stride = 16;
+                        else continue;
+                        vert_stride = std::max<unsigned int>(vert_stride, pl.GetVertexDataStride());
+                        const unsigned char *vert = static_cast<const unsigned char*>(pl.GetVertexData());
+
+                        return reinterpret_cast<const float *>(vert + (index * vert_stride));
+                    }
+
+                    index -= static_cast<size_t>(pl.GetCount());
+                }
+
+                return nullptr;
+            }
+
+            // TODO wow this is inefficient. think of something to avoid deferring the decision...
+            inline const coord_t* get_velocity(size_t index) const {
+                using megamol::core::moldyn::DirectionalParticles;
+
+                unsigned int plc = dat->GetParticleListCount();
+                for (unsigned int pli = 0; pli < plc; pli++) {
+                    auto& pl = dat->AccessParticles(pli);
+                    if ((pl.GetVertexDataType() != DirectionalParticles::VERTDATA_FLOAT_XYZ)
+                        && (pl.GetVertexDataType() != DirectionalParticles::VERTDATA_FLOAT_XYZR)
+                        && (pl.GetDirDataType() != DirectionalParticles::DIRDATA_FLOAT_XYZ)) {
+                        continue;
+                    }
+
+                    if (index < pl.GetCount()) {
+                        unsigned int dir_stride = 0;
+                        if (pl.GetDirDataType() == DirectionalParticles::DIRDATA_FLOAT_XYZ) dir_stride = 12;
+                        else continue;
+                        dir_stride = std::max<unsigned int>(dir_stride, pl.GetDirDataStride());
+                        const unsigned char *dir = static_cast<const unsigned char*>(pl.GetDirData());
+
+                        return reinterpret_cast<const float *>(dir + (index * dir_stride));
+                    }
+
+                    index -= static_cast<size_t>(pl.GetCount());
+                }
+
+                return nullptr;
+            }
+
+        };
+
+        bool assertData(core::moldyn::DirectionalParticleDataCall *in,
+            core::moldyn::MultiParticleDataCall *outMPDC, core::moldyn::DirectionalParticleDataCall *outDPDC);
+
         core::param::ParamSlot cyclXSlot;
         core::param::ParamSlot cyclYSlot;
         core::param::ParamSlot cyclZSlot;
+        core::param::ParamSlot radiusSlot;
         size_t datahash;
-        unsigned int time;
+        int lastTime;
         std::vector<float> newColors;
-        float minCol, maxCol;
+        //std::vector<float*> 
+        float minCol, maxCol, radius;
+        std::vector<size_t> allParts;
+
+        typedef nanoflann::KDTreeSingleIndexAdaptor<
+            nanoflann::L2_Simple_Adaptor<float, directionalPointcloud>,
+            directionalPointcloud,
+            3 /* dim */
+        > my_kd_tree_t;
+
+        std::shared_ptr<my_kd_tree_t> particleTree;
+        std::shared_ptr<directionalPointcloud> myPts;
+
+        /** The slot providing access to the manipulated data */
+        megamol::core::CalleeSlot outDataSlot;
+
+        /** The slot accessing the original data */
+        megamol::core::CallerSlot inDataSlot;
 
     };
 
