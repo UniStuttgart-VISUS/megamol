f3dc05289c7e39db344147e221cfa0520b42bfd5
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2017-12-04 11:36:02 +0100

    fix for new cxxopts interface

--- core/remoteconsole/src/ConClient.cpp
+++ core/remoteconsole/src/ConClient.cpp
@@ -30,58 +30,60 @@ int main(int argc, char* argv[]) {
         ;
 
     try {
-        options.parse(argc, argv);
+
+        auto parseRes = options.parse(argc, argv);
+
+        if (parseRes.count("help")) {
+            std::cout << options.help({ "" }) << std::endl;
+            exit(0);
+        }
+
+        // greeting text
+        printGreeting();
+
+        if (parseRes.count("open")) host = parseRes["open"].as<std::string>();
+        if (parseRes.count("source")) file = parseRes["source"].as<std::string>();
+        if (parseRes.count("keep-open")) keepOpen = parseRes["keep-open"].as<bool>();
+
+
+        //  Prepare our context and socket
+        zmq::context_t context(1);
+        zmq::socket_t socket(context, ZMQ_REQ);
+        Connection conn(socket);
+
+        if (!host.empty()) {
+            cout << "Connecting \"" << host << "\" ... ";
+            try {
+                conn.Connect(host);
+                cout << endl
+                    << "\tConnected" << endl
+                    << endl;
+
+            } catch (std::exception& ex) {
+                cout << endl
+                    << "ERR Socket connection failed: " << ex.what() << endl
+                    << endl;
+            } catch (...) {
+                cout << endl
+                    << "ERR Socket connection failed: unknown exception" << endl
+                    << endl;
+            }
+        }
+
+        if (!file.empty()) {
+            runScript(conn, file);
+
+        } else {
+            keepOpen = true;
+        }
+
+        if (keepOpen) {
+            interactiveConsole(conn);
+        }
+
     } catch (...) {
         std::cout << options.help({ "" }) << std::endl;
         exit(0);
-    }
-
-    if (options.count("help")) {
-        std::cout << options.help({ "" }) << std::endl;
-        exit(0);
-    }
-
-    // greeting text
-    printGreeting();
-
-    host = options["open"].as<std::string>();
-    file = options["source"].as<std::string>();
-    keepOpen = options["keep-open"].as<bool>();
-
-
-    //  Prepare our context and socket
-    zmq::context_t context(1);
-    zmq::socket_t socket(context, ZMQ_REQ);
-    Connection conn(socket);
-
-    if (!host.empty()) {
-        cout << "Connecting \"" << host << "\" ... ";
-        try {
-            conn.Connect(host);
-            cout << endl
-                << "\tConnected" << endl
-                << endl;
-
-        } catch (std::exception& ex) {
-            cout << endl
-                << "ERR Socket connection failed: " << ex.what() << endl
-                << endl;
-        } catch (...) {
-            cout << endl
-                << "ERR Socket connection failed: unknown exception" << endl
-                << endl;
-        }
-    }
-
-    if (!file.empty()) { 
-        runScript(conn, file);
-
-    } else {
-        keepOpen = true;
-    }
-
-    if (keepOpen) {
-        interactiveConsole(conn);
     }
 
     return 0;
