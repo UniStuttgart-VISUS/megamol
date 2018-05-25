3e502620b905f4adb41fda950e6242741df6af52
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2018-05-25 15:56:28 +0200

    a little bit of PLYDataSource (does not work yet)

--- plugins/mmstd_datatools/src/io/PLYDataSource.cpp
+++ plugins/mmstd_datatools/src/io/PLYDataSource.cpp
@@ -10,18 +10,39 @@
 #include "mmcore/param/FilePathParam.h"
 #include "mmcore/param/FlexEnumParam.h"
 #include <fstream>
-#include "tinyply.h"
+#include <string>
+#include "mmcore/moldyn/MultiParticleDataCall.h"
 
 using namespace megamol;
 using namespace megamol::stdplugin::datatools;
 
+bool icompare_pred(unsigned char a, unsigned char b) { return std::tolower(a) == std::tolower(b); }
+
+bool icompare(std::string const& a, std::string const& b) {
+    if (a.length() == b.length()) {
+        return std::equal(b.begin(), b.end(), a.begin(), icompare_pred);
+    } else {
+        return false;
+    }
+}
+
+const char theUndef[] = "<undef>";
 
 io::PLYDataSource::PLYDataSource(void) : core::Module(),
         filename("filename", "The path to the PLY file to load."),
-        xElemSlot("x element", "which element to get the x position from"),
-        yElemSlot("y element", "which element to get the y position from"),
-        zElemSlot("z element", "which element to get the z position from"),
-        iElemSlot("i element", "which element to get the intensity from"),
+        vertElemSlot("vertex element", "which element to get the vertex info from"),
+        faceElemSlot("face element", "which element to get the face info from"),
+        xPropSlot("x property", "which property to get the x position from"),
+        yPropSlot("y property", "which property to get the y position from"),
+        zPropSlot("z property", "which property to get the z position from"),
+        nxPropSlot("nx property", "which property to get the normal x component from"),
+        nyPropSlot("ny property", "which property to get the normal y component from"),
+        nzPropSlot("nz property", "which property to get the normal z component from"),
+        rPropSlot("r property", "which property to get the red component from"),
+        gPropSlot("g property", "which property to get the green component from"),
+        bPropSlot("b property", "which property to get the blue component from"),
+        iPropSlot("i property", "which property to get the intensity from"),
+        indexPropSlot("index property", "which property to get the vertex indices from"),
         getData("getdata", "Slot to request data from this data source."),
         file(nullptr), data_hash(0) {
 
@@ -29,17 +50,49 @@ io::PLYDataSource::PLYDataSource(void) : core::Module(),
     this->filename.SetUpdateCallback(&PLYDataSource::filenameChanged);
     this->MakeSlotAvailable(&this->filename);
 
-    this->xElemSlot.SetParameter(new core::param::FlexEnumParam("undef"));
-    this->MakeSlotAvailable(&this->xElemSlot);
-    this->yElemSlot.SetParameter(new core::param::FlexEnumParam("undef"));
-    this->MakeSlotAvailable(&this->yElemSlot);
-    this->zElemSlot.SetParameter(new core::param::FlexEnumParam("undef"));
-    this->MakeSlotAvailable(&this->zElemSlot);
-    this->iElemSlot.SetParameter(new core::param::FlexEnumParam("undef"));
-    this->MakeSlotAvailable(&this->iElemSlot);
+    this->vertElemSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->vertElemSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->vertElemSlot);
+    this->faceElemSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->faceElemSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->faceElemSlot);
 
-    this->getData.SetCallback("GraphDataCall", "GetData", &PLYDataSource::getDataCallback);
-    this->getData.SetCallback("GraphDataCall", "GetExtent", &PLYDataSource::getExtentCallback);
+    this->xPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->xPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->xPropSlot);
+    this->yPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->yPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->yPropSlot);
+    this->zPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->zPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->zPropSlot);
+    this->nxPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->nxPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->nxPropSlot);
+    this->nyPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->nyPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->nyPropSlot);
+    this->nzPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->nzPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->nzPropSlot);
+    this->rPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->rPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->rPropSlot);
+    this->gPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->gPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->gPropSlot);
+    this->bPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->bPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->bPropSlot);
+    this->iPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->iPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->iPropSlot);
+    this->indexPropSlot.SetParameter(new core::param::FlexEnumParam(theUndef));
+    this->indexPropSlot.SetUpdateCallback(&PLYDataSource::anyEnumChanged);
+    this->MakeSlotAvailable(&this->indexPropSlot);
+
+    this->getData.SetCallback("MultiParticleDataCall", "GetData", &PLYDataSource::getDataCallback);
+    this->getData.SetCallback("MultiParticleDataCall", "GetExtent", &PLYDataSource::getExtentCallback);
     this->MakeSlotAvailable(&this->getData);
 
     //this->setFrameCount(1);
@@ -66,28 +119,183 @@ void io::PLYDataSource::release(void) {
     //frameIdx.clear();
 }
 
+bool io::PLYDataSource::assertData() {
+
+    if (!instream.is_open()) return false;
+    if (vertices != nullptr) return true; // there is a modicum of data present, we have read the file before
+
+    auto& vertElem = this->vertElemSlot.Param<core::param::FlexEnumParam>()->Value();
+
+    if (vertElem != theUndef) {
+        if (this->xPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef &&
+            this->yPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef &&
+            this->zPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef) {
+            try {
+                vertices = plf.request_properties_from_element(
+                    vertElem,
+                        {this->xPropSlot.Param<core::param::FlexEnumParam>()->Value(),
+                            this->yPropSlot.Param<core::param::FlexEnumParam>()->Value(),
+                            this->zPropSlot.Param<core::param::FlexEnumParam>()->Value()});
+            } catch (const std::exception& e) {
+                vislib::sys::Log::DefaultLog.WriteError("tinyply exception reading vertices: %s", e.what());
+            }
+        }
+        if (this->nxPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef &&
+            this->nyPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef &&
+            this->nzPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef) {
+            try {
+                normals = plf.request_properties_from_element(
+                    vertElem,
+                        {this->nxPropSlot.Param<core::param::FlexEnumParam>()->Value(),
+                            this->nyPropSlot.Param<core::param::FlexEnumParam>()->Value(),
+                            this->nzPropSlot.Param<core::param::FlexEnumParam>()->Value()});
+            } catch (const std::exception& e) {
+                vislib::sys::Log::DefaultLog.WriteError("tinyply exception reading normals: %s", e.what());
+            }
+        }
+        if (this->rPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef &&
+            this->gPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef &&
+            this->bPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef) {
+            try {
+                colors = plf.request_properties_from_element(
+                    vertElem,
+                        {this->rPropSlot.Param<core::param::FlexEnumParam>()->Value(),
+                            this->gPropSlot.Param<core::param::FlexEnumParam>()->Value(),
+                            this->bPropSlot.Param<core::param::FlexEnumParam>()->Value()});
+            } catch (const std::exception& e) {
+                vislib::sys::Log::DefaultLog.WriteError("tinyply exception reading colors: %s", e.what());
+            }
+        }
+    }
+    if (this->faceElemSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef) {
+        if (this->indexPropSlot.Param<core::param::FlexEnumParam>()->Value() != theUndef) {
+            try {
+                faces = plf.request_properties_from_element(this->faceElemSlot.Param<core::param::FlexEnumParam>()->Value(),
+                        {this->indexPropSlot.Param<core::param::FlexEnumParam>()->Value()});
+            } catch (const std::exception& e) {
+                vislib::sys::Log::DefaultLog.WriteError("tinyply exception reading faces: %s", e.what());
+            }
+        }
+    }
+    plf.read(instream);
+}
+
 bool io::PLYDataSource::filenameChanged(core::param::ParamSlot& slot) {
 
     using vislib::sys::Log;
 
-    std::ifstream instream(filename.Param<core::param::FilePathParam>()->Value(), std::ios::binary);
+    instream.open(filename.Param<core::param::FilePathParam>()->Value(), std::ios::binary);
     if (instream.fail()) {
         Log::DefaultLog.WriteMsg(Log::LEVEL_ERROR, "Unable to open PLY File \"%s\".", vislib::StringA(filename.Param<core::param::FilePathParam>()->Value()).PeekBuffer());
         // TODO ?
         return true;
     }
 
-    this->xElemSlot.Param<core::param::FlexEnumParam>()->ClearValues();
-    this->yElemSlot.Param<core::param::FlexEnumParam>()->ClearValues();
-    this->zElemSlot.Param<core::param::FlexEnumParam>()->ClearValues();
-    this->iElemSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->vertElemSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->faceElemSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->xPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->yPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->zPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->nxPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->nyPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->nzPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->rPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->gPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->bPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->iPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
+    this->indexPropSlot.Param<core::param::FlexEnumParam>()->ClearValues();
 
-    tinyply::PlyFile plf;
+    guessedVertices = "";
+    guessedFaces = "";
+    guessedIndices = "";
+    guessedPos.assign(3, "");
+    guessedNormal.assign(3, "");
+    guessedColor.assign(3, "");
+
+    vertices = normals = colors = faces = nullptr;
+
     plf.parse_header(instream);
+
     for (auto e: plf.get_elements()) {
+        this->vertElemSlot.Param<core::param::FlexEnumParam>()->AddValue(e.name);
+        this->faceElemSlot.Param<core::param::FlexEnumParam>()->AddValue(e.name);
+        if (icompare(e.name,"vertex")) {
+            guessedVertices = e.name;
+        }
+        if (icompare(e.name, "face")) {
+            guessedFaces = e.name;
+        }
+
         Log::DefaultLog.WriteMsg(Log::LEVEL_INFO, "element: %s, %u bytes", e.name.c_str(), e.size);
         for (auto p: e.properties) {
             Log::DefaultLog.WriteMsg(Log::LEVEL_INFO, "    property: %s %s", p.name.c_str(), tinyply::PropertyTable[p.propertyType].str.c_str());
+            this->xPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->yPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->zPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->nxPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->nyPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->nzPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->rPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->gPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->bPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->iPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+            this->indexPropSlot.Param<core::param::FlexEnumParam>()->AddValue(p.name);
+
+            if (icompare(p.name, "x")) {
+                guessedPos[0] = p.name;
+            }
+            if (icompare(p.name, "y")) {
+                guessedPos[1] = p.name;
+            }
+            if (icompare(p.name, "z")) {
+                guessedPos[2] = p.name;
+            }
+            if (icompare(p.name, "nx")) {
+                guessedNormal[0] = p.name;
+            }
+            if (icompare(p.name, "ny")) {
+                guessedNormal[1] = p.name;
+            }
+            if (icompare(p.name, "nz")) {
+                guessedNormal[2] = p.name;
+            }
+            if (icompare(p.name, "r")) {
+                guessedColor[0] = p.name;
+            }
+            if (icompare(p.name, "g")) {
+                guessedColor[1] = p.name;
+            }
+            if (icompare(p.name, "b")) {
+                guessedColor[2] = p.name;
+            }
+            if (icompare(p.name, "indices")){ // || icompare(p.name, "vertex_indices")) {
+                guessedIndices = p.name;
+            }
+        }
+    }
+
+    if (!guessedVertices.empty()) {
+        this->vertElemSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedVertices);
+        if (std::none_of(guessedPos.begin(), guessedPos.end(), [](std::string s) { return s.empty(); })) {
+            this->xPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedPos[0]);
+            this->yPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedPos[1]);
+            this->zPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedPos[2]);
+        }
+        if (std::none_of(guessedNormal.begin(), guessedNormal.end(), [](std::string s) { return s.empty(); })) {
+            this->nxPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedNormal[0]);
+            this->nyPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedNormal[1]);
+            this->nzPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedNormal[2]);
+        }
+        if (std::none_of(guessedColor.begin(), guessedColor.end(), [](std::string s) { return s.empty(); })) {
+            this->rPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedColor[0]);
+            this->gPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedColor[1]);
+            this->bPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedColor[2]);
+        }
+    }
+    if (!guessedFaces.empty()) {
+        this->faceElemSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedFaces);
+        if (!guessedIndices.empty()) {
+            this->indexPropSlot.Param<core::param::FlexEnumParam>()->SetValue(guessedIndices);
         }
     }
 
@@ -156,10 +364,15 @@ bool io::PLYDataSource::filenameChanged(core::param::ParamSlot& slot) {
 //    return true;
 }
 
-bool io::PLYDataSource::getDataCallback(core::Call& caller) {
-    //GraphDataCall *c2 = dynamic_cast<GraphDataCall*>(&caller);
-    //if (c2 == nullptr) return false;
+bool io::PLYDataSource::anyEnumChanged(core::param::ParamSlot& slot) {
+    this->vertices = nullptr;
+}
 
+bool io::PLYDataSource::getDataCallback(core::Call& caller) {
+    auto c2 = dynamic_cast<core::moldyn::MultiParticleDataCall*>(&caller);
+    if (c2 == nullptr) return false;
+
+    if (!assertData()) return false;
     //Frame *f = nullptr;
     //if (c2 != nullptr) {
     //    f = dynamic_cast<Frame *>(this->requestLockedFrame(c2->FrameID(), true));
@@ -170,17 +383,19 @@ bool io::PLYDataSource::getDataCallback(core::Call& caller) {
     //    f->SetData(*c2);
     //}
 
-    //return true;
+    return true;
 }
 
 bool io::PLYDataSource::getExtentCallback(core::Call& caller) {
-    //GraphDataCall *c2 = dynamic_cast<GraphDataCall*>(&caller);
+    auto c2 = dynamic_cast<core::moldyn::MultiParticleDataCall*>(&caller);
 
-    //if (c2 != nullptr) {
-    //    c2->SetFrameCount(this->FrameCount());
-    //    c2->SetDataHash(this->data_hash);
-    //    return true;
-    //}
+    if (!assertData()) return false;
 
-    //return false;
+    if (c2 != nullptr) {
+        c2->SetFrameCount(1);
+        c2->SetDataHash(this->data_hash);
+        return true;
+    }
+
+    return true;
 }
--- plugins/mmstd_datatools/src/io/PLYDataSource.h
+++ plugins/mmstd_datatools/src/io/PLYDataSource.h
@@ -14,8 +14,10 @@
 #include "mmcore/CalleeSlot.h"
 #include "mmstd_datatools/GraphDataCall.h"
 #include "vislib/sys/File.h"
+#include "tinyply.h"
 #include <cstdint>
 #include <vector>
+#include <fstream>
 
 
 namespace megamol {
@@ -43,6 +45,8 @@ namespace io {
         virtual bool create(void);
         virtual void release(void);
 
+        bool assertData(void);
+
         ///**
         // * Helper class to unlock frame data when 'CallSimpleSphereData' is
         // * used.
@@ -68,18 +72,42 @@ namespace io {
 
         bool filenameChanged(core::param::ParamSlot& slot);
 
+        bool anyEnumChanged(core::param::ParamSlot& slot);
+
         bool getDataCallback(core::Call& caller);
 
         bool getExtentCallback(core::Call& caller);
 
         core::param::ParamSlot filename;
 
-        core::param::ParamSlot xElemSlot;
-        core::param::ParamSlot yElemSlot;
-        core::param::ParamSlot zElemSlot;
-        core::param::ParamSlot iElemSlot;
+        core::param::ParamSlot vertElemSlot;
+        core::param::ParamSlot faceElemSlot;
+
+        core::param::ParamSlot xPropSlot;
+        core::param::ParamSlot yPropSlot;
+        core::param::ParamSlot zPropSlot;
+        core::param::ParamSlot nxPropSlot;
+        core::param::ParamSlot nyPropSlot;
+        core::param::ParamSlot nzPropSlot;
+        core::param::ParamSlot rPropSlot;
+        core::param::ParamSlot gPropSlot;
+        core::param::ParamSlot bPropSlot;
+        core::param::ParamSlot iPropSlot;
+        core::param::ParamSlot indexPropSlot;
+
+        std::vector<std::string> guessedPos, guessedNormal, guessedColor;
+        std::string guessedIndices;
+        std::string guessedVertices, guessedFaces;
+
+        /// this also guards data availability and consistency with element/property selection!
+        std::shared_ptr<tinyply::PlyData> vertices;
+
+        std::shared_ptr<tinyply::PlyData> normals, colors, faces;
 
         core::CalleeSlot getData;
+
+        std::ifstream instream;
+        tinyply::PlyFile plf;
 
         vislib::sys::File *file;
         size_t data_hash;
