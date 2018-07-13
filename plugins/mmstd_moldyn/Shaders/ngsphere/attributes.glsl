32d3a7b210084790ad8287c88f6d27d79d4f0de1
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2018-07-13 19:04:04 +0200

    forgot doubles in NGSphere shader

--- plugins/mmstd_moldyn/Shaders/ngsphere/attributes.glsl
+++ plugins/mmstd_moldyn/Shaders/ngsphere/attributes.glsl
@@ -1,5 +1,6 @@
 #extension GL_ARB_shader_storage_buffer_object : require
 #extension GL_ARB_gpu_shader5 : require
+#extension GL_ARB_gpu_shader_fp64 : enable
 uniform vec4 viewAttr;
 
 uniform float scaling;
