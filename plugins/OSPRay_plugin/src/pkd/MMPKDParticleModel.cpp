38bb1514625e5b71f23613ea454268f46c031122
Unknown <patrick.gralka@informatik.uni-stuttgart.de>
2018-07-24 17:52:05 +0200

    New particlemodel for MMPKD geometry.

---
+++ plugins/OSPRay_plugin/src/pkd/MMPKDParticleModel.cpp
@@ -0,0 +1,78 @@
+#include "stdafx.h"
+#include "pkd/MMPKDParticleModel.h"
+
+#include "vislib/sys/Log.h"
+
+using namespace megamol;
+
+void megamol::ospray::MMPKDParticleModel::fill(megamol::core::moldyn::SimpleSphericalParticles const& parts) {
+    auto const vtype = parts.GetVertexDataType();
+    auto const ctype = parts.GetColourDataType();
+
+    if (vtype == megamol::core::moldyn::MultiParticleDataCall::Particles::VERTDATA_FLOAT_XYZR ||
+        vtype == megamol::core::moldyn::MultiParticleDataCall::Particles::VERTDATA_SHORT_XYZ) {
+        throw std::runtime_error("MMPKDParticleModel does not support FLOAT_XYZR or SHORT_XYZ");
+    }
+
+    if (vtype == megamol::core::moldyn::MultiParticleDataCall::Particles::VERTDATA_FLOAT_XYZ) {
+        if (ctype != megamol::core::moldyn::MultiParticleDataCall::Particles::COLDATA_UINT8_RGBA) {
+            vislib::sys::Log::DefaultLog.WriteWarn("MMPKDParticleModel only supports UINT8_RGBA for FLOAT_XYZ, falling back to UINT8_RGBA");
+        }
+
+        // in this case use positionf
+        auto const pcount = parts.GetCount();
+        for (size_t pidx = 0; pidx < pcount; ++pidx) {
+            auto part = parts[pidx];
+
+            ospcommon::vec3f pos;
+            pos.x = part.vert.GetXf();
+            pos.y = part.vert.GetYf();
+            pos.z = part.vert.GetZf();
+
+            ospcommon::vec4uc col;
+            col.x = part.col.GetRu8();
+            col.y = part.col.GetGu8();
+            col.z = part.col.GetBu8();
+            col.w = part.col.GetAu8();
+
+            float color = 0.0f;
+            auto const ptr = reinterpret_cast<unsigned char*>(&color);
+            ptr[0] = col.x;
+            ptr[1] = col.y;
+            ptr[2] = col.z;
+            ptr[3] = col.w;
+
+            this->positionf.emplace_back(pos, color);
+        }
+    } else if (vtype == megamol::core::moldyn::MultiParticleDataCall::Particles::VERTDATA_DOUBLE_XYZ) {
+        if (ctype != megamol::core::moldyn::MultiParticleDataCall::Particles::COLDATA_SHORT_RGBA) {
+            vislib::sys::Log::DefaultLog.WriteWarn("MMPKDParticleModel only supports SHORT_RGBA for FLOAT_XYZ, falling back to SHORT_RGBA");
+        }
+
+        // in this case use positiond
+        auto const pcount = parts.GetCount();
+        for (size_t pidx = 0; pidx < pcount; ++pidx) {
+            auto part = parts[pidx];
+
+            ospcommon::vec3d pos;
+            pos.x = part.vert.GetXf();
+            pos.y = part.vert.GetYf();
+            pos.z = part.vert.GetZf();
+
+            ospcommon::vec4uc col;
+            col.x = part.col.GetRu8();
+            col.y = part.col.GetGu8();
+            col.z = part.col.GetBu8();
+            col.w = part.col.GetAu8();
+
+            double color = 0.0;
+            auto const ptr = reinterpret_cast<unsigned short*>(&color);
+            ptr[0] = static_cast<unsigned short>(col.x) * 257;
+            ptr[1] = static_cast<unsigned short>(col.y) * 257;
+            ptr[2] = static_cast<unsigned short>(col.z) * 257;
+            ptr[3] = static_cast<unsigned short>(col.w) * 257;
+
+            this->positiond.emplace_back(pos, color);
+        }
+    }
+}
---
+++ plugins/OSPRay_plugin/src/pkd/MMPKDParticleModel.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <limits>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "mmcore/moldyn/MultiParticleDataCall.h"
+
+#include "ospcommon/box.h"
+
+
+namespace megamol {
+namespace ospray {
+
+/*! complete input data for a particle model */
+struct MMPKDParticleModel {
+
+
+    MMPKDParticleModel() : radius(0) { }
+
+    union {
+        std::vector<ospcommon::vec4f> positionf; //!< particle position in float + color encoded in 'w'
+        std::vector<ospcommon::vec4d> positiond; //!< particle position in double + color encoded in 'w'
+    };
+
+    void fill(megamol::core::moldyn::SimpleSphericalParticles const& parts);
+
+    float radius; //!< radius to use (0 if not specified)
+};
+
+} /* end namespace ospray */
+} /* end namespace megamol */
