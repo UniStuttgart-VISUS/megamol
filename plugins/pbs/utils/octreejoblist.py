72d45e9282b10ab278fcfe2bd3d14e4b6d4826fe
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2018-07-24 17:56:04 +0200

    ignore not-raw file

--- plugins/pbs/utils/octreejoblist.py
+++ plugins/pbs/utils/octreejoblist.py
@@ -2,7 +2,7 @@ from os import listdir, stat
 from os.path import isfile, join, commonprefix, splitext
 
 thePath = "\\\\etude\\Archiv\\Daten\\Partikel\\FARO\\"
-theScript = "U:\\home\\reina\\src\\megamol-prj\\cpe2mmpld.lua"
+theScript = "U:\\home\\reina\\src\\megamol-dev\\plugins\\pbs\\utils\\cpe2mmpld.lua"
 
 allFiles = [f for f in listdir(thePath) if isfile(join(thePath, f))]
 filteredFiles = []
@@ -15,6 +15,9 @@ for f in allFiles:
         if pf == splitext(f)[0]:
             found = True
             break
+
+    if not splitext(f)[1] == ".raw":
+        found = True
     
     size = stat(thePath + f)
     if not found and size.st_size > 48:
