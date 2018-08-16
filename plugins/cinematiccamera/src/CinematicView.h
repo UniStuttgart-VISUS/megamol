bdefc607457342aa73e9c3d45dc2b12fd11e5b34
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2018-08-16 16:44:12 +0200

    back to color check

--- plugins/cinematiccamera/src/CinematicView.h
+++ plugins/cinematiccamera/src/CinematicView.h
@@ -157,6 +157,9 @@ namespace megamol {
             /** */
             bool setSimTime(float st);
 
+            /** */
+            bool checkFBO4ColorChanges(vislib::graphics::gl::FramebufferObject& fbo, const float* backCol);
+
             /**
             * My error handling function for png export
             *
