83227dd0c3569d04d5915e498229f0924811e270
Christoph Schulz <schulzcbs@gmail.com>
2018-08-23 14:07:50 +0200

    Make it work for core

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -3,20 +3,20 @@ include(ExternalProject)
 
 function(argument_default VARIABLE)
   if(args_${VARIABLE})
-    set(${VARIABLE} ${args_${VARIABLE}} PARENT_SCOPE)
+    set(${VARIABLE} "${args_${VARIABLE}}" PARENT_SCOPE)
   else()
-    set(${VARIABLE} ${ARGN} PARENT_SCOPE)
+    set(${VARIABLE} "${ARGN}" PARENT_SCOPE)
   endif()
 endfunction(argument_default)
 
 function(add_external_library TARGET)
   set(ARG_OPTIONS STATIC SHARED INTERFACE)
-  set(ARG_SINGLE LIBRARY_DEBUG LIBRARY_RELEASE INCLUDE_DIR COMPILE_DEFINITIONS)
+  set(ARG_SINGLE LIBRARY_DEBUG LIBRARY_RELEASE IMPORT_LIBRARY_DEBUG IMPORT_LIBRARY_RELEASE INCLUDE_DIR COMPILE_DEFINITIONS)
   set(ARG_SINGLE_EXT GIT_TAG)
   cmake_parse_arguments(args "${ARG_OPTIONS}" "${ARG_SINGLE};${ARG_SINGLE_EXT}" "" ${ARGN})
 
-  # Filter arguments for ExternalProject_Add.
-  set(ARGN_EXT ${ARGN})
+  # Map arguments for ExternalProject_Add.
+  set(ARGN_EXT "${ARGN}")
   list(REMOVE_ITEM ARGN_EXT ${ARG_OPTIONS})
   foreach(arg ${ARG_SINGLE})
     list(FIND ARGN_EXT ${arg} index)
@@ -26,12 +26,11 @@ function(add_external_library TARGET)
       list(REMOVE_AT ARGN_EXT ${index})
     endif()
   endforeach()
+  list(APPEND ARGN_EXT CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>)
 
   # Add external project.
-  set(INSTALL_PREFIX_EXT="${CMAKE_BINARY_DIR}/external")
   set(TARGET_EXT "${TARGET}_ext")
-  ExternalProject_Add(${TARGET_EXT} ${ARGN_EXT}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${INSTALL_PREFIX_EXT})
+  ExternalProject_Add(${TARGET_EXT} "${ARGN_EXT}")
 
   if(args_GIT_TAG)
     message(STATUS "${TARGET} tag: ${args_GIT_TAG}")
@@ -39,8 +38,10 @@ function(add_external_library TARGET)
 
   # Guess target properties, unless set.
   ExternalProject_Get_Property(${TARGET_EXT} INSTALL_DIR)
-  argument_default(LIBRARY_DEBUG ${CMAKE_STATIC_LIBRARY_PREFIX}${TARGET}${CMAKE_STATIC_LIBRARY_SUFFIX})
+  argument_default(LIBRARY_DEBUG ${TARGET})
   argument_default(LIBRARY_RELEASE ${LIBRARY_DEBUG})
+  argument_default(IMPORT_LIBRARY_DEBUG ${TARGET})
+  argument_default(IMPORT_LIBRARY_RELEASE ${IMPORT_LIBRARY_DEBUG})
   argument_default(INCLUDE_DIR include)
   argument_default(COMPILE_DEFINITIONS "")
 
@@ -53,18 +54,33 @@ function(add_external_library TARGET)
     set(LINKAGE INTERFACE)
   endif()
 
-  # Add import library.
-  add_library(${TARGET} ${LINKAGE} IMPORTED)
+  # Create include directory as required by INTERFACE_INCLUDE_DIRECTORIES.
+  file(MAKE_DIRECTORY "${INSTALL_DIR}/${INCLUDE_DIR}")
+
+  # Add as imported library.
+  add_library(${TARGET} ${LINKAGE} IMPORTED GLOBAL)
   add_dependencies(${TARGET} ${TARGET_EXT})
   set_target_properties(${TARGET} PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${INSTALL_DIR}/${INCLUDE_DIR}"
-    INTERFACE_COMPILE_DEFINITIONS "${COMPILE_DEFINITIONS}")
+    INTERFACE_COMPILE_DEFINITIONS "${COMPILE_DEFINITIONS}"
+    INTERFACE_INCLUDE_DIRECTORIES "${INSTALL_DIR}/${INCLUDE_DIR}")
   if(LINKAGE STREQUAL "STATIC" OR LINKAGE STREQUAL "SHARED")
     set_target_properties(${TARGET} PROPERTIES
       IMPORTED_CONFIGURATIONS "Debug;Release"
       IMPORTED_LOCATION_DEBUG "${INSTALL_DIR}/${LIBRARY_DEBUG}"
       IMPORTED_LOCATION_RELEASE "${INSTALL_DIR}/${LIBRARY_RELEASE}")
-      #IMPORTED_NO_SONAME_DEBUG
-	  #IMPORTED_NO_SONAME_RELEASE
   endif()
+  if(LINKAGE STREQUAL "STATIC" OR LINKAGE STREQUAL "SHARED")
+    set_target_properties(${TARGET} PROPERTIES
+      IMPORTED_IMPLIB_DEBUG "${INSTALL_DIR}/${IMPORT_LIBRARY_DEBUG}"
+      IMPORTED_IMPLIB_RELEASE "${INSTALL_DIR}/${IMPORT_LIBRARY_RELEASE}")
+  endif()
+
+  #message(STATUS
+  #  "${TARGET} settings:\n"
+  #  "  Definitions:\t${COMPILE_DEFINITIONS}\n"
+  #  "  Include:\t\t${INSTALL_DIR}/${INCLUDE_DIR}\n"
+  #  "  Library (debug):\t${INSTALL_DIR}/${LIBRARY_DEBUG}\n"
+  #  "  Library (release):\t${INSTALL_DIR}/${LIBRARY_RELEASE}\n"
+  #  "  Import (debug):\t${INSTALL_DIR}/${IMPORT_LIBRARY_DEBUG}\n"
+  #  "  Import (release):\t${INSTALL_DIR}/${IMPORT_LIBRARY_RELEASE}")
 endfunction(add_external_library)
