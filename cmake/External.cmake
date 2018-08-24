9a26db21f9ade8309b8e1e8f81cb4e9ba4d51f3d
Christoph Schulz <schulzcbs@gmail.com>
2018-08-24 17:29:31 +0200

    Rework all the things because of IceT

--- cmake/External.cmake
+++ cmake/External.cmake
@@ -1,70 +1,74 @@
 include(CMakeParseArguments)
 include(ExternalProject)
 
-function(argument_default VARIABLE)
+function(_argument_default VARIABLE)
   if(args_${VARIABLE})
     set(${VARIABLE} "${args_${VARIABLE}}" PARENT_SCOPE)
   else()
     set(${VARIABLE} "${ARGN}" PARENT_SCOPE)
   endif()
-endfunction(argument_default)
+endfunction(_argument_default)
 
-function(add_external_library TARGET)
-  set(ARG_OPTIONS STATIC SHARED INTERFACE)
-  set(ARG_SINGLE LIBRARY_DEBUG LIBRARY_RELEASE IMPORT_LIBRARY_DEBUG IMPORT_LIBRARY_RELEASE INCLUDE_DIR COMPILE_DEFINITIONS)
-  set(ARG_SINGLE_EXT GIT_TAG)
-  cmake_parse_arguments(args "${ARG_OPTIONS}" "${ARG_SINGLE};${ARG_SINGLE_EXT}" "" ${ARGN})
-
-  # Map arguments for ExternalProject_Add.
-  set(ARGN_EXT "${ARGN}")
-  list(REMOVE_ITEM ARGN_EXT ${ARG_OPTIONS})
-  foreach(arg ${ARG_SINGLE})
-    list(FIND ARGN_EXT ${arg} index)
-    if(NOT index EQUAL -1)
-      # Remove key and succeding value.
-      list(REMOVE_AT ARGN_EXT ${index})
-      list(REMOVE_AT ARGN_EXT ${index})
-    endif()
-  endforeach()
-  list(APPEND ARGN_EXT CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>)
-
-  # Add external project.
-  set(TARGET_EXT "${TARGET}_ext")
-  ExternalProject_Add(${TARGET_EXT} "${ARGN_EXT}")
+#
+# Adds an external project.
+#
+# See ExternalProject_Add(...) for usage.
+#
+function(add_external_project TARGET)
+  set(ARGS_ONE_VALUE GIT_TAG)
+  cmake_parse_arguments(args "" "${ARGS_ONE_VALUE}" "" ${ARGN})
 
   if(args_GIT_TAG)
     message(STATUS "${TARGET} tag: ${args_GIT_TAG}")
   endif()
+  
+  # Compile arguments for ExternalProject_Add.
+  set(ARGN_EXT "${ARGN}")
+  list(APPEND ARGN_EXT CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>)
+  
+  # Add external project.
+  ExternalProject_Add(${TARGET} "${ARGN_EXT}")
+endfunction(add_external_project)
 
-  # Guess target properties, unless set.
-  ExternalProject_Get_Property(${TARGET_EXT} INSTALL_DIR)
-  argument_default(LIBRARY_DEBUG ${TARGET})
-  argument_default(LIBRARY_RELEASE ${LIBRARY_DEBUG})
-  argument_default(IMPORT_LIBRARY_DEBUG ${TARGET})
-  argument_default(IMPORT_LIBRARY_RELEASE ${IMPORT_LIBRARY_DEBUG})
-  argument_default(INCLUDE_DIR include)
-  argument_default(COMPILE_DEFINITIONS "")
-
-  # Set linkage.
-  if(args_STATIC)
-    set(LINKAGE STATIC)
-  elseif(args_SHARED)
-    set(LINKAGE SHARED)
-  elseif(args_INTERFACE)
-    set(LINKAGE INTERFACE)
-  else()
-    message(FATAL_ERROR "Missing library linkage (STATIC, SHARED, INTERFACE)")
-  endif()
+#
+# Adds an external library, depending on an external project.
+#
+# add_external_library(<target> (STATIC|SHARED|INTERFACE)
+#     DEPENDS <external_project>
+#     LIBRARY_DEBUG "Debug.dll"
+#     LIBRARY_RELEASE "Release.dll"
+#     IMPORT_LIBRARY_DEBUG "Debug.lib"
+#     IMPORT_LIBRARY_RELEASE "Release.lib"
+#     INTERFACE_LIBRARIES <external_library>*")
+#
+function(add_external_library TARGET LINKAGE)
+  set(ARGS_ONE_VALUE DEPENDS COMPILE_DEFINITIONS INCLUDE_DIR LIBRARY_DEBUG LIBRARY_RELEASE IMPORT_LIBRARY_DEBUG IMPORT_LIBRARY_RELEASE INTERFACE_LIBRARIES)
+  cmake_parse_arguments(args "" "${ARGS_ONE_VALUE}" "" ${ARGN})
+ 
+   if(NOT args_DEPENDS)
+     message(FATAL_ERROR "Missing external project for ${TARGET} to depend on")
+   endif()
+  _argument_default(DEPENDS "")
+ 
+  # Guess library properties, unless set.
+  ExternalProject_Get_Property(${DEPENDS} INSTALL_DIR)
+  _argument_default(COMPILE_DEFINITIONS "")
+  _argument_default(INCLUDE_DIR include)
+  _argument_default(LIBRARY_DEBUG "NOTFOUND")
+  _argument_default(LIBRARY_RELEASE "NOTFOUND")
+  _argument_default(IMPORT_LIBRARY_DEBUG "NOTFOUND")
+  _argument_default(IMPORT_LIBRARY_RELEASE "NOTFOUND")
 
   # Create include directory as required by INTERFACE_INCLUDE_DIRECTORIES.
   file(MAKE_DIRECTORY "${INSTALL_DIR}/${INCLUDE_DIR}")
 
-  # Add as imported library.
+  # Add an imported library.
   add_library(${TARGET} ${LINKAGE} IMPORTED GLOBAL)
-  add_dependencies(${TARGET} ${TARGET_EXT})
+  add_dependencies(${TARGET} ${DEPENDS})
   set_target_properties(${TARGET} PROPERTIES
     INTERFACE_COMPILE_DEFINITIONS "${COMPILE_DEFINITIONS}"
-    INTERFACE_INCLUDE_DIRECTORIES "${INSTALL_DIR}/${INCLUDE_DIR}")
+    INTERFACE_INCLUDE_DIRECTORIES "${INSTALL_DIR}/${INCLUDE_DIR}"
+    INTERFACE_LINK_LIBRARIES "${INTERFACE_LIBRARIES}")
   if(LINKAGE STREQUAL "STATIC" OR LINKAGE STREQUAL "SHARED")
     set_target_properties(${TARGET} PROPERTIES
       IMPORTED_CONFIGURATIONS "Debug;Release"
@@ -78,11 +82,12 @@ function(add_external_library TARGET)
   endif()
 
   #message(STATUS
-  #  "${TARGET} settings:\n"
+  #  "${DEPENDS} / ${TARGET} settings:\n"
   #  "  Definitions:\t${COMPILE_DEFINITIONS}\n"
   #  "  Include:\t\t${INSTALL_DIR}/${INCLUDE_DIR}\n"
   #  "  Library (debug):\t${INSTALL_DIR}/${LIBRARY_DEBUG}\n"
   #  "  Library (release):\t${INSTALL_DIR}/${LIBRARY_RELEASE}\n"
   #  "  Import (debug):\t${INSTALL_DIR}/${IMPORT_LIBRARY_DEBUG}\n"
-  #  "  Import (release):\t${INSTALL_DIR}/${IMPORT_LIBRARY_RELEASE}")
+  #  "  Import (release):\t${INSTALL_DIR}/${IMPORT_LIBRARY_RELEASE}\n"
+  #  "  Interface:\t${INTERFACE_LIBRARIES}")
 endfunction(add_external_library)
