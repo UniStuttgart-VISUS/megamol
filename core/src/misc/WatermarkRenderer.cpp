8629c96942bcd512122f2174da176e23ca6e85a2
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2018-10-25 18:24:51 +0200

    getcapabilities ...

--- core/src/misc/WatermarkRenderer.cpp
+++ core/src/misc/WatermarkRenderer.cpp
@@ -127,17 +127,16 @@ bool WatermarkRenderer::create(void) {
 */
 bool WatermarkRenderer::GetCapabilities(megamol::core::view::CallRender3D& call) {
 
-    //*** DEPRECATED ***
-    //view::CallRender3D *cr3d_in = dynamic_cast<view::CallRender3D*>(&call);
-    //if (cr3d_in == nullptr) return false;
+    view::CallRender3D *cr3d_in = dynamic_cast<view::CallRender3D*>(&call);
+    if (cr3d_in == nullptr) return false;
 
-    //// Propagate changes made in GetCapabilities() from outgoing CallRender3D (cr3d_out) to incoming CallRender3D (cr3d_in).
-    //view::CallRender3D *cr3d_out = this->rendererCallerSlot.CallAs<view::CallRender3D>();
-    //if ((cr3d_out != nullptr) && (*cr3d_out)(core::view::AbstractCallRender::FnGetCapabilities)) {
-    //    cr3d_in->AddCapability(cr3d_out->GetCapabilities());
-    //}
+    // Propagate changes made in GetCapabilities() from outgoing CallRender3D (cr3d_out) to incoming CallRender3D (cr3d_in).
+    view::CallRender3D *cr3d_out = this->rendererCallerSlot.CallAs<view::CallRender3D>();
+    if ((cr3d_out != nullptr) && (*cr3d_out)(core::view::CallRender3D::FnGetCapabilities)) {
+        cr3d_in->AddCapability(cr3d_out->GetCapabilities());
+    }
 
-    //cr3d_in->AddCapability(view::CallRender3D::CAP_RENDER);
+    cr3d_in->AddCapability(view::CallRender3D::CAP_RENDER);
 
     return true;
 }
