92d26958ccd1f05c8f64e8112987405c5d7c748e
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2018-10-25 18:24:13 +0200

    getcapabilities ...

--- plugins/cinematiccamera/src/CinematicRenderer.cpp
+++ plugins/cinematiccamera/src/CinematicRenderer.cpp
@@ -139,19 +139,16 @@ void CinematicRenderer::release(void) {
 */
 bool CinematicRenderer::GetCapabilities(megamol::core::view::CallRender3D& call) {
 
-    /* DEPRECATED */
-    /*
     view::CallRender3D *cr3d_in = dynamic_cast<view::CallRender3D*>(&call);
     if (cr3d_in == nullptr) return false;
 
     // Propagate changes made in GetCapabilities() from outgoing CallRender3D (cr3d_out) to incoming CallRender3D (cr3d_in).
     view::CallRender3D *cr3d_out = this->rendererCallerSlot.CallAs<view::CallRender3D>();
-    if ((cr3d_out != nullptr) && (*cr3d_out)(core::view::AbstractCallRender::FnGetCapabilities)) {
+    if ((cr3d_out != nullptr) && (*cr3d_out)(core::view::CallRender3D::FnGetCapabilities)) {
         cr3d_in->AddCapability(cr3d_out->GetCapabilities());
     }
 
     cr3d_in->AddCapability(view::CallRender3D::CAP_RENDER);
-    */
 
 	return true;
 }
