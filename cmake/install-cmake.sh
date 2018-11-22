7a588e2c94a5b465db3a9f62e204f150129b3d9d
Christoph Schulz <schulzcbs@gmail.com>
2018-11-22 12:23:01 +0100

    Fix cmake-install

--- cmake/install-cmake.sh
+++ cmake/install-cmake.sh
@@ -1,19 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -z "$TRAVIS_BUILD_DIR" ]; then
-    echo "Need to set TRAVIS_BUILD_DIR"
-    exit 1
+  echo "Need to set TRAVIS_BUILD_DIR"
+  exit 1
 fi
 
 CMAKE_FILE="cmake-3.12.4-Linux-x86_64"
 CMAKE_URL="https://cmake.org/files/v3.12/$CMAKE_FILE.tar.gz"
 
 # Download and install.
-DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
-mkdir ${DEPS_DIR} && cd ${DEPS_DIR}
-travis_retry wget --no-check-certificate ${CMAKE_URL}
-tar -xf ${CMAKE_FILE}.tar.gz -C cmake-install
+DEPS_DIR="$TRAVIS_BUILD_DIR/deps"
+mkdir -p $DEPS_DIR/cmake-install && cd $DEPS_DIR
+travis_retry wget --no-check-certificate $CMAKE_URL
+tar xf $CMAKE_FILE.tar.gz 
+echo $CMAKE_FILE installed to $DEPS_DIR/$CMAKE_FILE
 
 # Add to path and go back
-PATH=${DEPS_DIR}/cmake-install:${DEPS_DIR}/cmake-install/bin:$PATH
-cd ${TRAVIS_BUILD_DIR}
+export PATH=$DEPS_DIR/$CMAKE_FILE:$DEPS_DIR/$CMAKE_FILE/bin:$PATH
+cd $TRAVIS_BUILD_DIR
