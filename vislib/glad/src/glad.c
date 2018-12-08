4e96ab4e6a38944678faf59f4b6ab35b63478f14
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2018-12-08 11:47:37 +0100

    Disable Wpedantic for glad.c for less verbose compile log

--- vislib/glad/src/glad.c
+++ vislib/glad/src/glad.c
@@ -593,6 +593,9 @@
         Too many extensions
 */
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9800,3 +9803,4 @@ int gladLoadGLLoader(GLADloadproc load) {
 	return GLVersion.major != 0 || GLVersion.minor != 0;
 }
 
+#pragma GCC diagnostic pop
