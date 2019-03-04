764a6020976321eafccbe807289ad51de8e63fd3
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2019-03-04 18:15:29 +0100

    Added option to normalize data before clustering

--- plugins/mmstd_datatools/include/mmstd_datatools/DBSCAN.h
+++ plugins/mmstd_datatools/include/mmstd_datatools/DBSCAN.h
@@ -10,12 +10,43 @@ namespace megamol {
 namespace stdplugin {
 namespace datatools {
 
-template <typename T, bool NOISE, int DIM = -1> class DBSCAN {
+template <typename T, bool NOISE, int DIM = -1, bool NORMAL = false> class DBSCAN {
 public:
     using cluster_set_t = std::vector<std::vector<T>>;
 
-    DBSCAN(size_t const numPts, size_t const stride, std::vector<T>& pts, vislib::math::Cuboid<float> const& bbox, size_t const minPts, float const sigma)
-        : minpts_(minPts), sigma_(sigma), numPts_(numPts), stride_(stride), ptsC_(numPts, stride, pts, bbox) {
+    DBSCAN(size_t const numPts, size_t const stride, std::vector<T>& pts, vislib::math::Cuboid<float> const& bbox,
+        size_t const minPts, float const sigma)
+        : minpts_(minPts), sigma_(sigma), numPts_(numPts), stride_(stride) /*, ptsC_(numPts, stride, pts, bbox)*/ {
+        if (NORMAL) {
+            n_pts.resize(pts.size());
+            mins.resize(stride, std::numeric_limits<T>::max());
+            maxs.resize(stride, std::numeric_limits<T>::lowest());
+            for (size_t idx = 0; idx < numPts; ++idx) {
+                for (size_t s = 0; s < DIM; ++s) {
+                    mins[s] = std::min(mins[s], pts[idx * stride + s]);
+                    maxs[s] = std::max(maxs[s], pts[idx * stride + s]);
+                }
+            }
+            for (size_t d = DIM; d < stride; ++d) {
+                mins[d] = 0.0f;
+                maxs[d] = 1.0f;
+            }
+            for (size_t idx = 0; idx < numPts; ++idx) {
+                for (size_t s = 0; s < DIM; ++s) {
+                    n_pts[idx * stride + s] = (pts[idx * stride + s] - mins[s]) / (maxs[s] - mins[s]);
+                }
+                for (size_t d = DIM; d < stride; ++d) {
+                    n_pts[idx * stride + d] = pts[idx * stride + d];
+                }
+            }
+            ptsC_ = genericPointCloud<T>(
+                numPts, stride, n_pts, vislib::math::Cuboid<float>(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f));
+            auto min_el = std::min_element(mins.data(), mins.data()+DIM);
+            auto max_el = std::max_element(maxs.data(), maxs.data()+DIM);
+            sigma_ /= ((*max_el) - (*min_el));
+        } else {
+            ptsC_ = genericPointCloud<T>(numPts, stride, pts, bbox);
+        }
         kdTree_ = std::make_shared<kd_tree_t>(DIM, ptsC_, nanoflann::KDTreeSingleIndexAdaptorParams());
         kdTree_->buildIndex();
     }
@@ -29,6 +60,8 @@ private:
     void expandCluster(size_t const qidx, std::vector<std::pair<size_t, T>>& matches, std::vector<T>& cluster,
         std::vector<bool>& marked, std::vector<bool>& noCluster, std::vector<size_t>& noise);
 
+    void fillCluster(std::vector<T>& cluster, size_t idx);
+
     std::shared_ptr<kd_tree_t> kdTree_;
 
     size_t minpts_;
@@ -40,17 +73,24 @@ private:
     size_t stride_;
 
     genericPointCloud<T> ptsC_;
+
+    std::vector<T> mins;
+
+    std::vector<T> maxs;
+
+    std::vector<T> n_pts;
 }; // end class DBSCAN
 
 
-template <typename T, bool NOISE, int DIM> typename DBSCAN<T, NOISE, DIM>::cluster_set_t DBSCAN<T, NOISE, DIM>::Scan() {
+template <typename T, bool NOISE, int DIM, bool NORMAL>
+typename DBSCAN<T, NOISE, DIM, NORMAL>::cluster_set_t DBSCAN<T, NOISE, DIM, NORMAL>::Scan() {
     std::vector<bool> marked(numPts_, false);
     std::vector<bool> noCluster(numPts_, true);
     std::vector<size_t> noise;
-    noise.reserve(numPts_/1000);
+    noise.reserve(numPts_ / 1000);
 
     std::array<T, DIM> qp;
-    //qp[DIM];
+    // qp[DIM];
 
     T sr = sigma_ * sigma_;
 
@@ -89,9 +129,10 @@ template <typename T, bool NOISE, int DIM> typename DBSCAN<T, NOISE, DIM>::clust
         for (auto const& el : noise) {
             ret.emplace_back();
             auto& cluster = ret.back();
-            for (size_t d = 0; d < stride_; ++d) {
+            fillCluster(cluster, el);
+            /*for (size_t d = 0; d < stride_; ++d) {
                 cluster.push_back(ptsC_.kdtree_get_pt(el, d));
-            }
+            }*/
         }
     }
 
@@ -99,11 +140,11 @@ template <typename T, bool NOISE, int DIM> typename DBSCAN<T, NOISE, DIM>::clust
 }
 
 
-template <typename T, bool NOISE, int DIM>
-void DBSCAN<T, NOISE, DIM>::expandCluster(size_t const qidx, std::vector<std::pair<size_t, T>>& matches,
+template <typename T, bool NOISE, int DIM, bool NORMAL>
+void DBSCAN<T, NOISE, DIM, NORMAL>::expandCluster(size_t const qidx, std::vector<std::pair<size_t, T>>& matches,
     std::vector<T>& cluster, std::vector<bool>& marked, std::vector<bool>& noCluster, std::vector<size_t>& noise) {
     std::array<T, DIM> qp;
-    //T qp[DIM];
+    // T qp[DIM];
 
     T sr = sigma_ * sigma_;
 
@@ -114,9 +155,10 @@ void DBSCAN<T, NOISE, DIM>::expandCluster(size_t const qidx, std::vector<std::pa
     sp.sorted = false;
 
     cluster.reserve(100 * stride_);
-    for (size_t d = 0; d < stride_; ++d) {
+    fillCluster(cluster, qidx);
+    /*for (size_t d = 0; d < stride_; ++d) {
         cluster.push_back(ptsC_.kdtree_get_pt(qidx, d));
-    }
+    }*/
     for (size_t midx = 0; midx < matches.size(); ++midx) {
         auto idx = matches[midx].first;
         if (!marked[idx]) {
@@ -140,9 +182,10 @@ void DBSCAN<T, NOISE, DIM>::expandCluster(size_t const qidx, std::vector<std::pa
         }
         if (noCluster[idx]) {
             noCluster[idx] = false;
-            for (size_t d = 0; d < stride_; ++d) {
+            fillCluster(cluster, idx);
+            /*for (size_t d = 0; d < stride_; ++d) {
                 cluster.push_back(ptsC_.kdtree_get_pt(idx, d));
-            }
+            }*/
             auto it = std::find(noise.begin(), noise.end(), idx);
             if (it != noise.end()) {
                 noise.erase(it);
@@ -150,6 +193,23 @@ void DBSCAN<T, NOISE, DIM>::expandCluster(size_t const qidx, std::vector<std::pa
         }
     }
 }
+
+
+template <typename T, bool NOISE, int DIM, bool NORMAL>
+void DBSCAN<T, NOISE, DIM, NORMAL>::fillCluster(std::vector<T>& cluster, size_t idx) {
+    if (NORMAL) {
+        for (size_t d = 0; d < stride_; ++d) {
+            auto val = ptsC_.kdtree_get_pt(idx, d);
+            val = (val * (maxs[d] - mins[d])) + mins[d];
+            cluster.push_back(val);
+        }
+    } else {
+        for (size_t d = 0; d < stride_; ++d) {
+            cluster.push_back(ptsC_.kdtree_get_pt(idx, d));
+        }
+    }
+}
+
 
 } // end namespace datatools
 } // end namespace stdplugin
