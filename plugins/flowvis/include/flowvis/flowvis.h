ca8b4fd9da562c1229ba761025c90dc2cee0a435
Alexander Straub <alexander.straub@visus.uni-stuttgart.de>
2019-03-21 18:14:15 +0100

    Skeleton for FlowVis plugin

---
+++ plugins/flowvis/include/flowvis/flowvis.h
@@ -0,0 +1,99 @@
+/*
+ * flowvis.h
+ * Copyright (C) 2009-2019 by MegaMol Team
+ * Alle Rechte vorbehalten.
+ */
+#ifndef FLOWVIS_H_INCLUDED
+#define FLOWVIS_H_INCLUDED
+#if (defined(_MSC_VER) && (_MSC_VER > 1000))
+#pragma once
+#endif /* (defined(_MSC_VER) && (_MSC_VER > 1000)) */
+
+
+#ifdef _WIN32
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the FLOWVIS_EXPORTS
+// symbol defined on the command line. this symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// FLOWVIS_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef FLOWVIS_EXPORTS
+#define FLOWVIS_API __declspec(dllexport)
+#else
+#define FLOWVIS_API __declspec(dllimport)
+#endif
+#else /* _WIN32 */
+#define FLOWVIS_API
+#endif /* _WIN32 */
+
+#include "mmcore/utility/plugins/Plugin200Instance.h"
+
+#ifdef FLOWVIS_EXPORTS
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Returns the version of the MegaMol™ plugin api used by this plugin.
+ *
+ * @return 200 -- (ver.: 2.00)
+ */
+FLOWVIS_API int mmplgPluginAPIVersion(void);
+
+/**
+ * Provides compatibility information
+ *
+ * @param onError Callback function pointer used when an error occures
+ *
+ * @return The compatibility information struct, or nullptr in case of an
+ *         error.
+ *
+ * @remarks Always use 'mmplgReleasePluginCompatibilityInfo' to release the
+ *          memory of the returned struct.
+ */
+FLOWVIS_API
+::megamol::core::utility::plugins::PluginCompatibilityInfo *
+mmplgGetPluginCompatibilityInfo(
+    ::megamol::core::utility::plugins::ErrorCallback onError);
+
+/**
+ * Releases the memory of a compatibility information struct previously
+ * returned by 'mmplgGetPluginCompatibilityInfo'
+ *
+ * @param ci The compatibility information struct to be released
+ */
+FLOWVIS_API void mmplgReleasePluginCompatibilityInfo(
+    ::megamol::core::utility::plugins::PluginCompatibilityInfo* ci);
+
+/**
+ * Creates a new instance of this plugin
+ *
+ * @param onError Callback function pointer used when an error occures
+ *
+ * @return A new instance of this plugin, or nullptr in case of an error
+ *
+ * @remarks Always use 'mmplgReleasePluginInstance' to release the memory of
+ *          the returned object.
+ */
+FLOWVIS_API
+::megamol::core::utility::plugins::AbstractPluginInstance*
+mmplgGetPluginInstance
+    (::megamol::core::utility::plugins::ErrorCallback onError);
+
+/**
+ * Releases the memory of the plugin instance previously returned by
+ * 'mmplgGetPluginInstance'
+ *
+ * @param pi The plugin instance to be released
+ */
+FLOWVIS_API void mmplgReleasePluginInstance(
+    ::megamol::core::utility::plugins::AbstractPluginInstance* pi);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif FLOWVIS_EXPORTS
+
+#endif /* FLOWVIS_H_INCLUDED */
---
+++ plugins/flowvis/src/flowvis.cpp
@@ -0,0 +1,129 @@
+/*
+ * flowvis.cpp
+ * Copyright (C) 2009-2019 by MegaMol Team
+ * Alle Rechte vorbehalten.
+ */
+
+#include "stdafx.h"
+#include "flowvis/flowvis.h"
+
+#include "mmcore/api/MegaMolCore.std.h"
+#include "mmcore/utility/plugins/Plugin200Instance.h"
+#include "mmcore/versioninfo.h"
+#include "vislib/vislibversion.h"
+
+/* anonymous namespace hides this type from any other object files */
+namespace {
+    /** Implementing the instance class of this plugin */
+    class plugin_instance : public ::megamol::core::utility::plugins::Plugin200Instance {
+    public:
+        /** ctor */
+        plugin_instance(void)
+            : ::megamol::core::utility::plugins::Plugin200Instance(
+
+                /* machine-readable plugin assembly name */
+                "flowvis",
+
+                /* human-readable plugin description */
+                "Plugin for flow visualization") {
+
+            // here we could perform addition initialization
+        };
+        /** Dtor */
+        virtual ~plugin_instance(void) {
+            // here we could perform addition de-initialization
+        }
+        /** Registers modules and calls */
+        virtual void registerClasses(void) {
+
+            // register modules here:
+            
+
+            // register calls here:
+
+        }
+        MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_plugininstance_connectStatics
+    };
+}
+
+
+/*
+ * mmplgPluginAPIVersion
+ */
+FLOWVIS_API int mmplgPluginAPIVersion(void) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgPluginAPIVersion
+}
+
+
+/*
+ * mmplgGetPluginCompatibilityInfo
+ */
+FLOWVIS_API
+::megamol::core::utility::plugins::PluginCompatibilityInfo *
+mmplgGetPluginCompatibilityInfo(
+        ::megamol::core::utility::plugins::ErrorCallback onError) {
+    // compatibility information with core and vislib
+    using ::megamol::core::utility::plugins::PluginCompatibilityInfo;
+    using ::megamol::core::utility::plugins::LibraryVersionInfo;
+
+    PluginCompatibilityInfo *ci = new PluginCompatibilityInfo;
+    ci->libs_cnt = 2;
+    ci->libs = new LibraryVersionInfo[2];
+
+    SetLibraryVersionInfo(ci->libs[0], "MegaMolCore",
+        MEGAMOL_CORE_MAJOR_VER, MEGAMOL_CORE_MINOR_VER, MEGAMOL_CORE_COMP_REV, 0
+#if defined(DEBUG) || defined(_DEBUG)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DEBUG_BUILD
+#endif
+#if defined(MEGAMOL_CORE_DIRTY) && (MEGAMOL_CORE_DIRTY != 0)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DIRTY_BUILD
+#endif
+        );
+
+    SetLibraryVersionInfo(ci->libs[1], "vislib",
+        vislib::VISLIB_VERSION_MAJOR, vislib::VISLIB_VERSION_MINOR, vislib::VISLIB_VERSION_REVISION, 0
+#if defined(DEBUG) || defined(_DEBUG)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DEBUG_BUILD
+#endif
+#if defined(VISLIB_DIRTY_BUILD) && (VISLIB_DIRTY_BUILD != 0)
+        | MEGAMOLCORE_PLUGIN200UTIL_FLAGS_DIRTY_BUILD
+#endif
+        );
+    //
+    // If you want to test additional compatibilties, add the corresponding versions here
+    //
+
+    return ci;
+}
+
+
+/*
+ * mmplgReleasePluginCompatibilityInfo
+ */
+FLOWVIS_API
+void mmplgReleasePluginCompatibilityInfo(
+        ::megamol::core::utility::plugins::PluginCompatibilityInfo* ci) {
+    // release compatiblity data on the correct heap
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgReleasePluginCompatibilityInfo(ci)
+}
+
+
+/*
+ * mmplgGetPluginInstance
+ */
+FLOWVIS_API
+::megamol::core::utility::plugins::AbstractPluginInstance*
+mmplgGetPluginInstance(
+        ::megamol::core::utility::plugins::ErrorCallback onError) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgGetPluginInstance(plugin_instance, onError)
+}
+
+
+/*
+ * mmplgReleasePluginInstance
+ */
+FLOWVIS_API
+void mmplgReleasePluginInstance(
+        ::megamol::core::utility::plugins::AbstractPluginInstance* pi) {
+    MEGAMOLCORE_PLUGIN200UTIL_IMPLEMENT_mmplgReleasePluginInstance(pi)
+}
