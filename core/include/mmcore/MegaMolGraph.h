1bafb53f171320b84ada6b7dfc4413f717795707
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2019-03-22 11:35:57 +0100

    that is what the graph should look like, I think
    missing: instance stuff, lock

---
+++ core/include/mmcore/MegaMolGraph.h
@@ -0,0 +1,108 @@
+#ifndef MEGAMOLCORE_MEGAMOLGRAPH_H_INCLUDED
+#define MEGAMOLCORE_MEGAMOLGRAPH_H_INCLUDED
+#if (defined(_MSC_VER) && (_MSC_VER > 1000))
+#    pragma once
+#endif /* (defined(_MSC_VER) && (_MSC_VER > 1000)) */
+
+#include <string>
+#include <functional>
+#include <vector>
+#include "mmcore/param/AbstractParam.h"
+#include "mmcore/param/ParamSlot.h"
+#include "mmcore/param/ParamUpdateListener.h"
+#include "mmcore/ModuleNamespace.h"
+#include "mmcore/Module.h"
+#include "mmcore/Call.h"
+#include "mmcore/api/MegaMolCore.h"
+#include "vislib/SmartPtr.h"
+
+namespace megamol {
+namespace core {
+
+class MegaMolGraph {
+
+    // todo: where do the descriptionmanagers go?
+    // todo: what about the view / job descriptions?
+public:
+    // todo: constructor(s)? see serialization
+
+    // todo: the lock!
+
+    bool QueueModuleDeletion(const std::string id);
+    bool QueueCallDeletion(const std::string from, const std::string to);
+    bool QueueModuleInstantiation(const std::string className, const std::string id);
+    bool QueueCallInstantiation(const std::string className, const std::string from, const std::string to);
+    bool QueueChainCallInstantiation(const std::string className, const std::string chainStart, const std::string to);
+    bool QueueParamValueChange(const std::string id, const std::string value);
+    /// @return group id 0 is invalid and means failure
+    uint32_t CreateParamGroup(const std::string name, int size);
+    bool QueueParamGroupValue(const std::string groupName, const std::string id, const std::string value);
+    bool QueueParamGroupValue(uint32_t groupId, const std::string id, const std::string value);
+    bool QueueUpdateFlush();
+    void PerformGraphUpdates();
+    bool AnythingQueued();
+
+    // todo: for everything below, RO version AND RW version? or would we just omit the const and imply the user needs to lock?
+    ////////////////////////////
+
+    vislib::SmartPtr<param::AbstractParam> FindParameter(const std::string name, bool quiet = false) const;
+    
+    // todo: optionally ask for the parameters of a specific module (name OR module pointer?)
+    inline void EnumerateParameters(std::function<void(const Module&, param::ParamSlot&)> cb) const;
+
+    // todo: can we get rid of this? where would we do the data unpacking?
+    inline void EnumerateParameters(mmcEnumStringAFunction func, void* data) const;
+
+    // todo: optionally pass Module instead of name
+    template <class A, class C>
+    typename std::enable_if<std::is_convertible<A*, Module*>::value && std::is_convertible<C*, Call*>::value,
+        bool>::type
+    EnumerateCallerSlots(std::string module_name, std::function<void(C&)> cb) const;
+
+    template <class A>
+    typename std::enable_if<std::is_convertible<A*, Module*>::value, bool>::type FindModule(
+        std::string module_name, std::function<void(A&)> cb) const;
+
+    // WHY??? this is just EnumerateParamters(FindModule()...) GET RID OF IT!
+    template <class A>
+    typename std::enable_if<std::is_convertible<A*, Module*>::value, bool>::type EnumerateParameterSlots(
+        std::string module_name, std::function<void(param::ParamSlot&)> cb) const;
+
+    size_t GetGlobalParameterHash(void) const;
+
+    // todo: why do we even need this? Like, honestly?
+    std::string FindParameterName(const vislib::SmartPtr<param::AbstractParam>& param) const;
+
+    // probably just throws everything away?
+    void Cleanup(void);
+
+    // serialize into... JSON?
+    std::string SerializeGraph(void) const;
+
+    // replace the whole graph with whatever is in serialization
+    void Deserialize(std::string othergraph);
+
+    // construct from serialization
+    MegaMolGraph(std::string othergraph);
+
+    void NotifyParameterValueChange(param::ParamSlot& slot) const;
+
+    void RegisterParamUpdateListener(param::ParamUpdateListener* pul);
+    void UnregisterParamUpdateListener(param::ParamUpdateListener* pul);
+
+private:
+    // todo: signature is weird, data structure might be as well
+    void computeGlobalParameterHash(ModuleNamespace::const_ptr_type path, ParamHashMap_t& map) const;
+    static void compareParameterHash(ParamHashMap_t& one, ParamHashMap_t& other) const;
+
+    void updateFlushIdxList(size_t const processedCount, std::vector<size_t>& list);
+    bool checkForFlushEvent(size_t const eventIdx, std::vector<size_t>& list) const;
+    void shortenFlushIdxList(size_t const eventCount, std::vector<size_t>& list);
+
+
+};
+
+} /* namespace core */
+} /* namespace megamol */
+
+#endif /* MEGAMOLCORE_MEGAMOLGRAPH_H_INCLUDED */
