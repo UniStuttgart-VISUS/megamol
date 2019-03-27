d97dbb3567a19284fd932e8e33c701e8a4a2919e
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2019-03-27 14:56:22 +0100

    Box filter for particle streams

---
+++ plugins/mmstd_datatools/src/DirParticleBoxFilter.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "AbstractParticleBoxFilter.h"
+
+namespace megamol {
+namespace stdplugin {
+namespace datatools {
+
+using DirParticleBoxFilter = AbstractParticleBoxFilter<core::moldyn::DirectionalParticleDataCall>;
+
+} // end namespace datatools
+} // end namespace stdplugin
+} // end namespace megamol
