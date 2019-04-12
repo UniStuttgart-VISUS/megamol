1682c7affc2310178703b9433df4eb38a3cb6096
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2019-04-12 18:48:48 +0200

    added empty settings class ... now

---
+++ plugins/gui/src/GUISettings.cpp
@@ -0,0 +1,34 @@
+/*
+ * GUISettings.cpp
+ *
+ * Copyright (C) 2019 by Universitaet Stuttgart (VIS).
+ * Alle Rechte vorbehalten.
+ */
+
+#include "stdafx.h"
+#include "GUISettings.h"
+
+
+using namespace megamol::gui;
+
+
+/**
+ * Ctor
+ */
+megamol::gui::GUISettings::GUISettings() {
+
+    this->file = "mmgui.ini";
+}
+
+
+/**
+ * Dtor
+ */
+megamol::gui::GUISettings::~GUISettings(void) {
+
+    // nothing to do here ...
+}
+
+
+
+
