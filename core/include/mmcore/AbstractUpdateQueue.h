6ab4357604d4742e19770f2105a81f32f50cee99
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2019-04-15 21:12:52 +0200

    Wrapper for a lockable queue

---
+++ core/include/mmcore/AbstractUpdateQueue.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <mutex>
+#include <queue>
+
+namespace megamol {
+namespace core {
+
+template <typename T> class AbstractUpdateQueue {
+public:
+    std::unique_lock<std::mutex> AcquireLock() { return std::unique_lock<std::mutex>(update_lock_); }
+
+    void Push(T&& el) { update_queue_.push(el); }
+
+    void Push(T const& el) { update_queue_.push(el); }
+
+    typename std::queue<T>::value_type Get() {
+        T el = update_queue_.front();
+        update_queue_.pop();
+        return el;
+    }
+
+    template <typename... Args> void Emplace(Args&&... vals) { update_queue_.emplace(std::forward<Args>(vals)...); }
+
+    bool Empty() { return update_queue_.empty(); }
+
+    typename std::queue<T>::size_type Size() { return update_queue_.size(); }
+
+private:
+    std::mutex update_lock_;
+
+    std::queue<T> update_queue_;
+}; // end class AbstractUpdateQueue
+
+} // end namespace core
+} // end namespace megamol
