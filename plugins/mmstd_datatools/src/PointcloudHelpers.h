0438614f83523d5f2c9e5ff6e6b010f5f1cb8986
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2019-05-09 13:31:56 +0200

    ICol pointcloud wrapper

--- plugins/mmstd_datatools/src/PointcloudHelpers.h
+++ plugins/mmstd_datatools/src/PointcloudHelpers.h
@@ -9,6 +9,7 @@
 #define MMSTD_DATATOOLS_POINTCLOUDHELPERS_H_INCLUDED
 #pragma once
 
+#include <array>
 #include <algorithm>
 #include <vector>
 #include "mmcore/moldyn/DirectionalParticleDataCall.h"
@@ -111,6 +112,79 @@ public:
         return nullptr;
     }
 };
+
+
+/**
+ * Class that implements the interface nanoflann needs for simple spherical particles with icol.
+ * The index vector addresses all of the particles across all lists, so its
+ * range is (0,Sum(Allof(particleLists).Count)).
+ */
+class simpleIColPointcloud {
+private:
+    megamol::core::moldyn::SimpleSphericalParticles* dat;
+    bool cycleX, cycleY, cycleZ;
+
+public:
+    typedef float coord_t;
+
+    simpleIColPointcloud(megamol::core::moldyn::SimpleSphericalParticles* dat)
+        : dat(dat) {
+        // intentionally empty
+    }
+    ~simpleIColPointcloud() {
+        // intentionally empty
+    }
+
+    // Must return the number of data points
+    inline size_t kdtree_get_point_count() const { return dat->GetCount(); }
+
+    // Returns the distance between the vector "p1[0:size-1]" and the data point with index "idx_p2" stored in the
+    // class:
+    inline coord_t kdtree_distance(const coord_t* p1, const size_t idx_p2, size_t /*size*/) const {
+        auto const p2 = get_position(idx_p2);
+
+        const coord_t d0 = p1[0] - p2[0];
+        const coord_t d1 = p1[1] - p2[1];
+        const coord_t d2 = p1[2] - p2[2];
+        const coord_t d3 = p1[3] - p2[3];
+
+        return d0 * d0 + d1 * d1 + d2 * d2 + d3 * d3;
+    }
+
+    // Returns the dim'th component of the idx'th point in the class:
+    // Since this is inlined and the "dim" argument is typically an immediate value, the
+    //  "if/else's" are actually solved at compile time.
+    inline coord_t kdtree_get_pt(const size_t idx, int dim) const {
+        assert((dim >= 0) && (dim < 4));
+        return get_position(idx)[dim];
+    }
+
+    // Optional bounding-box computation: return false to default to a standard bbox computation loop.
+    //   Return true if the BBOX was already computed by the class and returned in "bb" so it can be avoided to redo it
+    //   again. Look at bb.size() to find out the expected dimensionality (e.g. 2 or 3 for point clouds)
+    template <class BBOX> bool kdtree_get_bbox(BBOX& bb) const {
+        // return false;
+
+        assert(bb.size() == 3);
+        const auto& cbox = dat->AccessBoundingBoxes().ObjectSpaceBBox();
+        bb[0].low = cbox.Left();
+        bb[0].high = cbox.Right();
+        bb[1].low = cbox.Bottom();
+        bb[1].high = cbox.Top();
+        bb[2].low = cbox.Back();
+        bb[2].high = cbox.Front();
+        return true;
+    }
+
+    inline std::array<coord_t, 4> get_position(size_t index) const {
+        std::array<coord_t, 4> const ret{dat->GetParticleStore().GetXAcc()->Get_f(index),
+            dat->GetParticleStore().GetYAcc()->Get_f(index), dat->GetParticleStore().GetZAcc()->Get_f(index),
+            dat->GetParticleStore().GetCRAcc()->Get_f(index)};
+
+        return ret;
+    }
+};
+
 
 /**
  * Class that implements the interface nanoflann needs for directional particles.
