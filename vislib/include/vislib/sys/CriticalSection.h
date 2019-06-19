c5ffb1afe11a7aefb3f9dcadc2ec0c53ec4d2002
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2019-06-19 22:04:41 +0200

    Replaced CriticalSection by Mutex

--- vislib/include/vislib/sys/CriticalSection.h
+++ vislib/include/vislib/sys/CriticalSection.h
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2006 by Universitaet Stuttgart (VIS). Alle Rechte vorbehalten.
  * Copyright (C) 2005 by Christoph Mueller (christoph.mueller@vis.uni-stuttgart.de). Alle Rechte vorbehalten.
+ * Copyright 2019 MegaMol Dev Team
  */
 
 #ifndef VISLIB_CRITICALSECTION_H_INCLUDED
@@ -14,117 +15,13 @@
 #    pragma managed(push, off)
 #endif /* defined(_WIN32) && defined(_MANAGED) */
 
-
-#ifdef _WIN32
-#    include <Windows.h>
-#else /* _WIN32 */
-#    include "Mutex.h"
-#endif /* _WIN32 */
-
+#include "Mutex.h"
 #include "Lockable.h"
-#include "SyncObject.h"
-
 
 namespace vislib {
 namespace sys {
 
-/**
- * Implements a critical section.
- *
- * Implementation notes: On Windows, the implementation uses a critical
- * section, which cannot be used for inter-process synchronisation. Only
- * threads of a single process can be synchronised using this class. Use
- * Mutex or Semaphore, if you need inter-process synchronisation or a
- * TryLock() method on systems less than Windows NT 4. Note,
- * that critical sections are faster than Mutexes or Semaphores.
- *
- * You must compile your program with _WIN32_WINNT defined as 0x0400 or
- * later to use TryLock on the critical section. TryLock() will always fail
- * otherwise.
- *
- * On Linux systems, the critical section is emulated using a system mutex.
- *
- * @author Christoph Mueller
- */
-class CriticalSection : public SyncObject {
-
-public:
-    /**
-     * Create a new critical section, which is initially not locked.
-     */
-    CriticalSection(void);
-
-    /** Dtor. */
-    virtual ~CriticalSection(void);
-
-    /**
-     * Enter the crititcal section for the calling thread. The method blocks
-     * until the lock is acquired.
-     *
-     * @throws SystemException If the lock could not be acquired.
-     */
-    virtual void Lock(void);
-
-    /**
-     * Try to enter the critical section. If another thread is already in
-     * the critical section, the method will return immediately and the r
-     * eturn value is false. The method is therefore non-blocking.
-     *
-     * NOTE: This method will always return fail on Windows systems prior
-     * to Windows NT 4. Only programs that are compiled with _WIN32_WINNT
-     * defined as 0x0400 support this method.
-     *
-     * @return true, if the lock was acquired, false, if not.
-     *
-     * @throws UnsupportedOperationException On system prior to Windows
-     *                                       NT 4.
-     * @throws SystemException If an error occurred when trying to acquire
-     *                         the lock.
-     *
-     */
-    bool TryLock(void);
-
-    /**
-     * Leave the critical section.
-     *
-     * @throw SystemException If the lock could not be released.
-     */
-    virtual void Unlock(void);
-
-private:
-    /**
-     * Forbidden copy ctor.
-     *
-     * @param rhs The object to be cloned.
-     *
-     * @throws UnsupportedOperationException Unconditionally.
-     */
-    CriticalSection(const CriticalSection& rhs);
-
-    /**
-     * Forbidden assignment.
-     *
-     * @param rhs The right hand side operand.
-     *
-     * @return *this.
-     *
-     * @throws IllegalParamException If (this != &rhs).
-     */
-    CriticalSection& operator=(const CriticalSection& rhs);
-
-#ifdef _WIN32
-
-    /** The OS critical section. */
-    CRITICAL_SECTION critSect;
-
-#else /* _WIN32 */
-
-    /** The mutex used for protecting the critical section. */
-    Mutex mutex;
-
-#endif /* _WIN32 */
-};
-
+using CriticalSection = Mutex;
 
 /** name typedef for Lockable with this SyncObject */
 typedef Lockable<CriticalSection> CriticalSectionLockable;
