40ae9f650337dd3caab6b7b24de9c387f22c7974
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2019-06-19 22:04:51 +0200

    Copyright

--- vislib/src/sys/Mutex.cpp
+++ vislib/src/sys/Mutex.cpp
@@ -3,6 +3,7 @@
  *
  * Copyright (C) 2006 by Universitaet Stuttgart (VIS). Alle Rechte vorbehalten.
  * Copyright (C) 2005 by Christoph Mueller (christoph.mueller@vis.uni-stuttgart.de). Alle Rechte vorbehalten.
+ * Copyright 2019 MegaMol Dev Team
  */
 
 #include <chrono>
