1c484f2dd3d3f5efe84721c2dcd58914bba6805a
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2019-06-19 22:36:44 +0200

    Replaced ReaderWriterLock with vislib::sys::Mutex (std::recursive_timed_mutex)

---
+++ vislib/include/vislib/sys/ReaderWriterMutexWrapper.h
@@ -0,0 +1,37 @@
+/*
+ * ReaderWriterMutexWrapper.h
+ *
+ * Copyright 2019 MegaMol Dev Team
+ */
+#pragma once
+
+#include "AbstractReaderWriterLock.h"
+#include "Mutex.h"
+
+namespace vislib {
+namespace sys {
+
+class ReaderWriterMutexWrapper : public AbstractReaderWriterLock {
+public:
+    ReaderWriterMutexWrapper() = default;
+
+    ~ReaderWriterMutexWrapper() override {}
+
+    void Lock() override { mutex.Lock(); }
+
+    void LockExclusive() override { Lock(); };
+
+    void LockShared() override { Lock(); };
+
+    void Unlock() override { mutex.Unlock(); }
+
+    void UnlockExclusive() override { Unlock(); };
+
+    void UnlockShared() override { Unlock(); };
+
+private:
+    Mutex mutex;
+}; // end class ReaderWriterMutexWrapper
+
+} // end namespace sys
+} // end namespace vislib
