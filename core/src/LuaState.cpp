5cbaaa4c03c4b22082cdd3477d548492897a3f49
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2019-06-21 21:15:27 +0200

    Less permissive msvc options

--- core/src/LuaState.cpp
+++ core/src/LuaState.cpp
@@ -248,7 +248,7 @@ int dispatch(lua_State * L) {
 #define CHECK_LUA(call) __luaErr = call;\
     consumeError(__luaErr, __FILE__, __LINE__);
 
-void megamol::core::LuaState::consumeError(int error, char *file, int line) {
+void megamol::core::LuaState::consumeError(int error, char const* file, int line) const {
     if (error != LUA_OK) {
         const char *err = lua_tostring(L, -1); // get error from top of stack...
         vislib::sys::Log::DefaultLog.WriteError("Lua Error: %s at %s:%i\n", err, file, line);
