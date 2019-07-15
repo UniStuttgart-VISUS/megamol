86822b9abc302107a20f9c97e21099b3106d1cb2
Alexander Straub <alexander.straub@visus.uni-stuttgart.de>
2019-07-15 11:27:32 +0200

    Modified travis for CUDA

--- install-cuda-xenial.sh
+++ install-cuda-xenial.sh
@@ -63,12 +63,7 @@ if [ ${CUDA_INSTALL_EXTRA_LIBS:-1} -ne 0 ]; then
   if [ ${CUDA_VER_MAJOR} -ge 7 ]; then
     CUDA_PKGS+=" cuda-cusolver-dev-${CUDA_APT} "
   fi
-  if [ ${CUDA_VER_MAJOR} -eq 10 -a ${CUDA_VER_MINOR} -eq 1 ]; then
-    CUDA_PKGS+="cuda-cublas-dev-10-0 "
-    LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:${LD_LIBRARY_PATH}
-  else
-    CUDA_PKGS+="cuda-cublas-dev-${CUDA_APT} "
-  fi
+  CUDA_PKGS+="cuda-cublas-dev-${CUDA_APT} "
 fi
 
 travis_retry sudo apt-get install -y ${CUDA_PKGS}
