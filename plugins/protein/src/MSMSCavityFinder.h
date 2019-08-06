95db40d1833c5b8cca8fe40ab0582e6d6ad0365f
Karsten Schatz <karsten.schatz@visus.uni-stuttgart.de>
2019-08-06 18:36:32 +0200

    Added helper file for protein data

--- plugins/protein/src/MSMSCavityFinder.h
+++ plugins/protein/src/MSMSCavityFinder.h
@@ -9,7 +9,7 @@
 #    pragma once
 #endif /* (defined(_MSC_VER) && (_MSC_VER > 1000)) */
 
-//#include "SombreroHelpers.h"
+#include "ProteinHelpers.h"
 
 #include "mmcore/Call.h"
 #include "mmcore/CalleeSlot.h"
