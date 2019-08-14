210557b71e1cfbb0c648ca5bd0db94f2d4f9dbd3
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2019-08-14 14:14:14 +0200

    removed commented code

--- core/Shaders/sphere/simple_vert_color.glsl
+++ core/Shaders/sphere/simple_vert_color.glsl
@@ -5,19 +5,5 @@
         vertColor = globalCol;
     }
     if (bool(useTf)) {
-
         vertColor = tflookup(inColIdx);
-
-        // float cid = MAX_COLV - MIN_COLV;    
-        // if (cid < 0.000001) {
-        //     vertColor = texture(tfTexture, 0.5 / COLTAB_SIZE);
-        // } else {
-        //     cid = (inColIdx - MIN_COLV) / cid;
-        //     cid = clamp(cid, 0.0, 1.0);
-        
-        //     cid *= (1.0 - 1.0 / COLTAB_SIZE);
-        //     cid += 0.5 / COLTAB_SIZE;
-        
-        //     vertColor = texture(tfTexture, cid);
-        // }
     }
