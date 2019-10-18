ccf9697960d0624b7fe2d3b823f7aa5c8067e2b9
invor <michael.becher.1990@googlemail.com>
2019-10-18 16:50:51 +0200

    Adding (empty) modules for gpu probe billboard glyph rendering

---
+++ plugins/probe_gl/src/ProbeBillboardGlyphMaterial.h
@@ -0,0 +1,59 @@
+/*
+ * ProbeBillboardGlyphMaterial.h
+ *
+ * Copyright (C) 2019 by Universitaet Stuttgart (VISUS).
+ * All rights reserved.
+ */
+
+#ifndef PROBE_BILLBOARD_GLYPH_MATERIAL_H_INCLUDED
+#define PROBE_BILLBOARD_GLYPH_MATERIAL_H_INCLUDED
+
+#include "mesh/AbstractGPUMaterialDataSource.h"
+
+namespace megamol {
+namespace probe_gl {
+
+class ProbeBillboardGlyphMaterial : public mesh::AbstractGPUMaterialDataSource {
+public:
+    /**
+     * Answer the name of this module.
+     *
+     * @return The name of this module.
+     */
+    static const char* ClassName(void) { return "ProbeBillboardGlyphMaterial"; }
+
+    /**
+     * Answer a human readable description of this module.
+     *
+     * @return A human readable description of this module.
+     */
+    static const char* Description(void) { return "..."; }
+
+    /**
+     * Answers whether this module is available on the current system.
+     *
+     * @return 'true' if the module is available, 'false' otherwise.
+     */
+    static bool IsAvailable(void) { return true; }
+
+    ProbeBillboardGlyphMaterial();
+    ~ProbeBillboardGlyphMaterial();
+
+protected:
+    bool create();
+
+    bool getDataCallback(core::Call& caller);
+
+    bool getMetaDataCallback(core::Call& caller);
+
+private:
+    core::CallerSlot m_probes_slot;
+    size_t m_probes_cached_hash;
+};
+
+
+} // probe_gl
+} // namespace megamol
+
+
+#endif // !PROBE_BILLBOARD_GLYPH_MATERIAL_H_INCLUDED
---
+++ plugins/probe_gl/src/ProbeBillboardGlyphRenderTasks.h
@@ -0,0 +1,57 @@
+/*
+ * ProbeBillboardGlyphRenderTasks.h
+ *
+ * Copyright (C) 2019 by Universitaet Stuttgart (VISUS).
+ * All rights reserved.
+ */
+
+#ifndef PROBE_BILLBOARD_GLYPH_RENDER_TASK_H_INCLUDED
+#define PROBE_BILLBOARD_GLYPH_RENDER_TASK_H_INCLUDED
+
+#include "mesh/AbstractGPURenderTaskDataSource.h"
+
+namespace megamol {
+namespace probe_gl {
+
+class ProbeBillboardGlyphRenderTasks : public mesh::AbstractGPURenderTaskDataSource {
+public:
+    /**
+     * Answer the name of this module.
+     *
+     * @return The name of this module.
+     */
+    static const char* ClassName(void) { return "ProbeBillboardGlyphRenderTasks"; }
+
+    /**
+     * Answer a human readable description of this module.
+     *
+     * @return A human readable description of this module.
+     */
+    static const char* Description(void) { return "..."; }
+
+    /**
+     * Answers whether this module is available on the current system.
+     *
+     * @return 'true' if the module is available, 'false' otherwise.
+     */
+    static bool IsAvailable(void) { return true; }
+
+    ProbeBillboardGlyphRenderTasks();
+    ~ProbeBillboardGlyphRenderTasks();
+
+protected:
+    bool getDataCallback(core::Call& caller);
+
+    bool getMetaDataCallback(core::Call& caller);
+
+private:
+    core::CallerSlot m_probes_slot;
+    size_t m_probes_cached_hash;
+
+};
+
+} // namespace probe_gl
+} // namespace megamol
+
+
+#endif // !PROBE_BILLBOARD_GLYPH_RENDER_TASK_H_INCLUDED
