60dc105e0ce175f4aa59797a40184f5d12ee79d0
rauts <tobias.rau@visus.uni-stuttgart.de>
2019-10-31 20:50:09 +0100

    added generate glyphs module

---
+++ plugins/probe/src/GenerateGlyphs.cpp
@@ -0,0 +1,283 @@
+/*
+ * GenerateGlyphs.cpp
+ * Copyright (C) 2019 by MegaMol Team
+ * Alle Rechte vorbehalten.
+ */
+
+#include "GenerateGlyphs.h"
+#include "mesh/MeshCalls.h"
+#include "ProbeCalls.h"
+#include "DrawTextureUtility.h"
+
+namespace megamol {
+namespace probe {
+
+
+GenerateGlyphs::GenerateGlyphs() : 
+    Module()
+    , _deploy_texture("deployTexture", "") 
+    , _deploy_mesh("deployMesh", "") 
+    , _get_probes("getProbes", "")
+{
+
+    this->_deploy_mesh.SetCallback(mesh::CallMesh::ClassName(), mesh::CallMesh::FunctionName(0), &GenerateGlyphs::getMesh);
+    this->_deploy_mesh.SetCallback(
+        mesh::CallMesh::ClassName(), mesh::CallMesh::FunctionName(1), &GenerateGlyphs::getMetaData);
+    this->MakeSlotAvailable(&this->_deploy_mesh);
+
+    this->_deploy_texture.SetCallback(
+        mesh::CallImage::ClassName(), mesh::CallImage::FunctionName(0), &GenerateGlyphs::getTexture);
+    this->_deploy_texture.SetCallback(
+        mesh::CallImage::ClassName(), mesh::CallImage::FunctionName(1), &GenerateGlyphs::getTextureMetaData);
+    this->MakeSlotAvailable(&this->_deploy_texture);
+
+    this->_get_probes.SetCompatibleCall<CallProbesDescription>();
+    this->MakeSlotAvailable(&this->_get_probes);
+
+}
+
+GenerateGlyphs::~GenerateGlyphs() {
+    this->Release();
+}
+
+
+void GenerateGlyphs::doGlyphGeneration() {
+
+    this->_mesh_data = std::make_shared<mesh::MeshDataAccessCollection>();
+    this->_tex_data = std::make_shared<mesh::ImageDataAccessCollection>();
+
+    const float scale = 0.2f;
+
+    _dtu.resize(this->_probe_data->getProbeCount());
+    _generated_mesh.resize(this->_probe_data->getProbeCount() * 4);
+
+    this->_generated_mesh_indices = {0, 1, 3, 0, 3, 2};
+    this->_generated_texture_coordinates[3] = {0.0f, 0.0f};
+    this->_generated_texture_coordinates[1] = {0.0f, 1.0f};
+    this->_generated_texture_coordinates[2] = {1.0f, 0.0f};
+    this->_generated_texture_coordinates[0] = {1.0f, 1.0f};
+
+    for (int i = 0; i < this->_probe_data->getProbeCount(); i++) {
+
+        // calc vertices
+        auto probe = this->_probe_data->getProbe<FloatProbe>(i);
+        auto dir = probe.m_direction;
+        auto smallest_normal_index = std::distance(dir.begin(), std::min_element(dir.begin(), dir.end()));
+        dir[smallest_normal_index] = 1.0f;
+        auto second_smallest_normal_index = std::distance(dir.begin(), std::min_element(dir.begin(), dir.end()));
+        auto largest_normal_index = std::distance(
+            probe.m_direction.begin(), std::max_element(probe.m_direction.begin(), probe.m_direction.end()));
+
+        //int flip_axis = 0;
+        //if (largest_normal_index == 0) {
+        //    if (std::abs(probe.m_direction[0]) - std::abs(probe.m_direction[1]) < 0.3 ) {
+        //        flip_axis = 
+        //                    
+        //    }
+
+        //    }
+
+        std::array<float,3> axis0 = {0.0f, 0.0f, 0.0f};
+        if (smallest_normal_index == 1) smallest_normal_index = second_smallest_normal_index;
+        axis0[smallest_normal_index] = 1.0f;
+        std::array<float,3> plane_vec_1;
+        plane_vec_1[0] = probe.m_direction[1] * axis0[2] - probe.m_direction[2] * axis0[1];
+        plane_vec_1[1] = probe.m_direction[2] * axis0[0] - probe.m_direction[0] * axis0[2];
+        plane_vec_1[2] = probe.m_direction[0] * axis0[1] - probe.m_direction[1] * axis0[0];
+
+        std::array<float, 3> plane_vec_2;
+        plane_vec_2[0] = probe.m_direction[1] * plane_vec_1[2] - probe.m_direction[2] * plane_vec_1[1];
+        plane_vec_2[1] = probe.m_direction[2] * plane_vec_1[0] - probe.m_direction[0] * plane_vec_1[2];
+        plane_vec_2[2] = probe.m_direction[0] * plane_vec_1[1] - probe.m_direction[1] * plane_vec_1[0];
+
+        float plane_vec_1_length = std::sqrt(
+        plane_vec_1[0] * plane_vec_1[0] + plane_vec_1[1] * plane_vec_1[1] + plane_vec_1[2] * plane_vec_1[2]);
+        plane_vec_1[0] /= plane_vec_1_length;
+        plane_vec_1[1] /= plane_vec_1_length;
+        plane_vec_1[2] /= plane_vec_1_length;
+
+        float plane_vec_2_length = std::sqrt(
+        plane_vec_2[0] * plane_vec_2[0] + plane_vec_2[1] * plane_vec_2[1] + plane_vec_2[2] * plane_vec_2[2]);
+        plane_vec_2[0] /= plane_vec_2_length;
+        plane_vec_2[1] /= plane_vec_2_length;
+        plane_vec_2[2] /= plane_vec_2_length;
+
+        std::array<float,3> middle;
+        middle[0] = probe.m_position[0] + probe.m_direction[0] * probe.m_begin;
+        middle[1] = probe.m_position[1] + probe.m_direction[1] * probe.m_begin;
+        middle[2] = probe.m_position[2] + probe.m_direction[2] * probe.m_begin;
+
+        std::array<float, 3> vertex1;
+        vertex1[0] = middle[0] + scale / 2 * plane_vec_1[0] + scale / 2 * plane_vec_2[0];
+        vertex1[1] = middle[1] + scale / 2 * plane_vec_1[1] + scale / 2 * plane_vec_2[1];
+        vertex1[2] = middle[2] + scale / 2 * plane_vec_1[2] + scale / 2 * plane_vec_2[2];
+
+        std::array<float, 3> vertex2;
+        vertex2[0] = middle[0] - scale / 2 * plane_vec_1[0] + scale / 2 * plane_vec_2[0];
+        vertex2[1] = middle[1] - scale / 2 * plane_vec_1[1] + scale / 2 * plane_vec_2[1];
+        vertex2[2] = middle[2] - scale / 2 * plane_vec_1[2] + scale / 2 * plane_vec_2[2];
+
+        std::array<float, 3> vertex3;
+        vertex3[0] = middle[0] + scale / 2 * plane_vec_1[0] - scale / 2 * plane_vec_2[0];
+        vertex3[1] = middle[1] + scale / 2 * plane_vec_1[1] - scale / 2 * plane_vec_2[1];
+        vertex3[2] = middle[2] + scale / 2 * plane_vec_1[2] - scale / 2 * plane_vec_2[2];
+
+        std::array<float, 3> vertex4;
+        vertex4[0] = middle[0] - scale / 2 * plane_vec_1[0] - scale / 2 * plane_vec_2[0];
+        vertex4[1] = middle[1] - scale / 2 * plane_vec_1[1] - scale / 2 * plane_vec_2[1];
+        vertex4[2] = middle[2] - scale / 2 * plane_vec_1[2] - scale / 2 * plane_vec_2[2];
+
+        this->_generated_mesh[i * 4 + 0] = vertex1; 
+        this->_generated_mesh[i * 4 + 1] = vertex2;    
+        this->_generated_mesh[i * 4 + 2] = vertex3;
+        this->_generated_mesh[i * 4 + 3] = vertex4;
+
+        std::vector<mesh::MeshDataAccessCollection::VertexAttribute> vertex_attributes(2);
+        mesh::MeshDataAccessCollection::VertexAttribute pos_attrib;
+        pos_attrib.data = reinterpret_cast<uint8_t*>(&this->_generated_mesh[i * 4 + 0]);
+        pos_attrib.stride = sizeof(std::array<float, 3>);
+        pos_attrib.byte_size = pos_attrib.stride * 4;
+        pos_attrib.component_cnt = 3;
+        pos_attrib.component_type = mesh::MeshDataAccessCollection::FLOAT;
+        pos_attrib.offset = 0;
+        pos_attrib.semantic = mesh::MeshDataAccessCollection::POSITION;
+        vertex_attributes[0] = pos_attrib;
+
+        mesh::MeshDataAccessCollection::VertexAttribute texco_attrib;
+        texco_attrib.data = reinterpret_cast<uint8_t*>(this->_generated_texture_coordinates.data());
+        texco_attrib.stride = sizeof(float) * 2;
+        texco_attrib.byte_size = texco_attrib.stride * 4;
+        texco_attrib.component_cnt = 2;
+        texco_attrib.component_type = mesh::MeshDataAccessCollection::FLOAT;
+        texco_attrib.offset = 0;
+        texco_attrib.semantic = mesh::MeshDataAccessCollection::TEXCOORD;
+        vertex_attributes[1] = texco_attrib;
+
+        mesh::MeshDataAccessCollection::IndexData index_data;
+        index_data.data = reinterpret_cast<uint8_t*>(this->_generated_mesh_indices.data());
+        index_data.byte_size = sizeof(this->_generated_mesh_indices);
+        index_data.type = mesh::MeshDataAccessCollection::UNSIGNED_INT;
+
+        this->_mesh_data->addMesh(vertex_attributes, index_data);
+
+        // read sampled probe
+        auto samples = probe.getSamplingResult();
+
+        _dtu[i].setResolution(200, 200); // should be changeable
+        _dtu[i].setGraphType(DrawTextureUtility::PLOT); // should be changeable
+
+        auto tex_ptr = _dtu[i].draw(samples->samples, samples->min_value, samples->max_value);
+        this->_tex_data->addImage(mesh::ImageDataAccessCollection::RGBA8, _dtu[i].getPixelWidth(),
+            _dtu[i].getPixelHeight(), tex_ptr, 4 * _dtu[i].getPixelWidth() *
+            _dtu[i].getPixelHeight());
+
+    } // end for probe count
+}
+
+
+bool GenerateGlyphs::getMesh(core::Call& call) {
+
+    auto cprobes = this->_get_probes.CallAs<CallProbes>();
+    auto cm = dynamic_cast<mesh::CallMesh*>(&call);
+
+    if (cprobes == nullptr) return false;
+    if (cm == nullptr) return false;
+
+    auto probe_meta_data = cprobes->getMetaData();
+
+    if (probe_meta_data.m_data_hash != this->_data_hash)
+        if (!(*cprobes)(0)) return false;
+
+    probe_meta_data = cprobes->getMetaData();
+    auto mesh_meta_data = cm->getMetaData();
+
+    mesh_meta_data.m_bboxs = probe_meta_data.m_bboxs;
+
+    this->_probe_data = cprobes->getData();
+
+    if (probe_meta_data.m_data_hash != this->_data_hash)
+        doGlyphGeneration();
+
+    cm->setData(this->_mesh_data);
+
+    this->_data_hash = probe_meta_data.m_data_hash;
+
+    return true;
+}
+
+bool GenerateGlyphs::getMetaData(core::Call& call) {
+
+    auto cprobes = this->_get_probes.CallAs<CallProbes>();
+    auto cm = dynamic_cast<mesh::CallMesh*>(&call);
+
+    if (cprobes == nullptr) return false;
+    if (cm == nullptr) return false;
+
+    auto probe_meta_data = cprobes->getMetaData();
+    auto mesh_meta_data = cm->getMetaData();
+
+    probe_meta_data.m_frame_ID = mesh_meta_data.m_frame_ID;
+    cprobes->setMetaData(probe_meta_data);
+
+    if (!(*cprobes)(1)) return false;
+
+    probe_meta_data = cprobes->getMetaData();
+
+    if (this->_data_hash != probe_meta_data.m_data_hash) {
+        mesh_meta_data.m_data_hash++;
+        mesh_meta_data.m_frame_cnt = probe_meta_data.m_frame_cnt;
+        mesh_meta_data.m_bboxs = probe_meta_data.m_bboxs;
+        cm->setMetaData(mesh_meta_data);
+    }
+
+    return true;
+}
+
+bool GenerateGlyphs::getTexture(core::Call& call) {
+
+    auto cprobes = this->_get_probes.CallAs<CallProbes>();
+    auto ctex = dynamic_cast<mesh::CallImage*>(&call);
+
+    if (cprobes == nullptr) return false;
+    if (ctex == nullptr) return false;
+
+    auto probe_meta_data = cprobes->getMetaData();
+
+    if (probe_meta_data.m_data_hash != this->_data_hash)
+        if (!(*cprobes)(0)) return false;
+
+    probe_meta_data = cprobes->getMetaData();
+
+    this->_probe_data = cprobes->getData();
+
+    if (probe_meta_data.m_data_hash != this->_data_hash)
+        doGlyphGeneration();
+
+    ctex->setData(this->_tex_data);
+
+    this->_data_hash = probe_meta_data.m_data_hash;
+
+    return true;
+}
+
+bool GenerateGlyphs::getTextureMetaData(core::Call& call) {
+    auto cprobes = this->_get_probes.CallAs<CallProbes>();
+    auto ctex = dynamic_cast<mesh::CallImage*>(&call);
+
+    if (cprobes == nullptr) return false;
+    if (ctex == nullptr) return false;
+
+    if (!(*cprobes)(1)) return false;
+
+    auto probe_meta_data = cprobes->getMetaData();
+    auto tex_meta_data = ctex->getMetaData();
+
+    if (this->_data_hash != probe_meta_data.m_data_hash) {
+        tex_meta_data.m_data_hash++;
+        ctex->setMetaData(tex_meta_data);
+    }
+
+    return true;
+}
+} // namespace probe
+} // namespace megamol
---
+++ plugins/probe/src/GenerateGlyphs.h
@@ -0,0 +1,81 @@
+/*
+ * GenerateGlyphs.h
+ * Copyright (C) 2019 by MegaMol Team
+ * Alle Rechte vorbehalten.
+ */
+
+#pragma once
+
+#include "ProbeCollection.h"
+#include "mesh/MeshDataAccessCollection.h"
+#include "mmcore/Call.h"
+#include "mmcore/CalleeSlot.h"
+#include "mmcore/CallerSlot.h"
+#include "mmcore/Module.h"
+#include "mesh/ImageDataAccessCollection.h"
+#include "DrawTextureUtility.h"
+
+namespace megamol {
+namespace probe {
+
+class GenerateGlyphs : public core::Module {
+public:
+    /**
+     * Answer the name of this module.
+     *
+     * @return The name of this module.
+     */
+    static const char* ClassName(void) { return "GenerateGlyphs"; }
+
+    /**
+     * Answer a human readable description of this module.
+     *
+     * @return A human readable description of this module.
+     */
+    static const char* Description(void) { return "Creator for GenerateGlyphs."; }
+
+    /**
+     * Answers whether this module is available on the current system.
+     *
+     * @return 'true' if the module is available, 'false' otherwise.
+     */
+    static bool IsAvailable(void) { return true; }
+
+    /** Dtor. */
+    virtual ~GenerateGlyphs(void);
+
+    /** Ctor. */
+    GenerateGlyphs(void);
+
+    core::CalleeSlot _deploy_texture;
+    core::CalleeSlot _deploy_mesh;
+    core::CallerSlot _get_probes;
+
+protected:
+
+    bool create() override {return true;};
+    void release() override {};
+
+private:
+    bool getMesh(core::Call& call);
+    bool getMetaData(core::Call& call);
+
+    bool getTexture(core::Call& call);
+    bool getTextureMetaData(core::Call& call);
+
+    void doGlyphGeneration();
+
+    uint32_t _data_hash;
+    std::shared_ptr<ProbeCollection> _probe_data;
+    std::shared_ptr<mesh::MeshDataAccessCollection> _mesh_data;
+    std::shared_ptr<mesh::ImageDataAccessCollection> _tex_data;
+
+    std::vector<std::array<float,3>> _generated_mesh;
+    std::array<uint32_t,6> _generated_mesh_indices;
+    std::array<std::array<float,2>,4> _generated_texture_coordinates;
+
+    std::vector<DrawTextureUtility> _dtu;
+};
+
+} // namespace probe
+} // namespace megamol
