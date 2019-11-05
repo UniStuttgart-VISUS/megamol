c4c812d768960d75d37ed7951fa3b25dd7ad522b
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2019-11-05 14:26:57 +0100

    main3000: update CMake and main

--- main3000/src/main.cpp
+++ main3000/src/main.cpp
@@ -1,11 +1,42 @@
-#include "mmcore\MegaMolGraph.h"
+#include "mmcore/CoreInstance.h"
+#include "mmcore/MegaMolGraph.h"
+
+#include "AbstractRenderAPI.hpp"
+#include "OpenGL_GLFW_RAPI.hpp"
 
 int main() {
+    megamol::core::CoreInstance core;
+    core.Initialise();
 
-	factories::ModuleDescriptionManager moduleProvider;
-	factories::CallDescriptionManager callProvider;
+    const megamol::core::factories::ModuleDescriptionManager& moduleProvider = core.GetModuleDescriptionManager();
+    const megamol::core::factories::CallDescriptionManager& callProvider = core.GetCallDescriptionManager();
 
-	megamol::core::MegaMolGraph graph(moduleProvider, callProvider);
+    std::unique_ptr<megamol::render_api::AbstractRenderAPI> gl_api =
+        std::make_unique<megamol::render_api::OpenGL_GLFW_RAPI>();
+    std::string gl_api_name = "opengl";
 
-	return 0;
+	megamol::render_api::OpenGL_GLFW_RAPI::Config openglConfig;
+    openglConfig.windowTitlePrefix = openglConfig.windowTitlePrefix + " ~ Main3000";
+    gl_api->initAPI(&openglConfig);
+
+    megamol::core::MegaMolGraph graph(core, moduleProvider, callProvider, std::move(gl_api), gl_api_name);
+
+	// TODO: verify valid input IDs/names in graph instantiation methods - dont defer validation until executing the changes
+	graph.QueueModuleInstantiation("View3D", gl_api_name + "::view");
+	graph.QueueModuleInstantiation("SphereOutlineRenderer", gl_api_name + "::spheres");
+	graph.QueueModuleInstantiation("TestSpheresDataSource", gl_api_name + "::datasource");
+	graph.QueueCallInstantiation("CallRender3D", gl_api_name + "::view::rendering", gl_api_name + "::spheres::rendering");
+	graph.QueueCallInstantiation("MultiParticleDataCall", gl_api_name + "::spheres::getdata", gl_api_name + "::datasource::getData");
+
+	graph.ExecuteGraphUpdates();
+
+	while (true) {
+        graph.RenderNextFrame();
+	}
+
+	// clean up modules, calls, graph
+
+	// TODO: implement graph destructor
+
+    return 0;
 }
