91968d948d4ee1e0fc2cfe55618c72ba93b1f98a
Alexander Straub <alexander.straub@visus.uni-stuttgart.de>
2019-12-06 11:57:32 +0100

    Separation of commands for better error handling

--- cmake/External_build.cmake
+++ cmake/External_build.cmake
@@ -5,25 +5,50 @@ if(NOT EXISTS EXTERNAL_BUILT_${CONFIG})
   message(STATUS "INSTALL_COMMANDS: ${INSTALL_COMMANDS}")
   message(STATUS "COMMANDS: ${COMMANDS}")
 
+  # Build external project
   execute_process(
     COMMAND ${CMAKE_COMMAND} --build . --verbose --parallel --config ${CONFIG}
-    COMMAND ${CMAKE_COMMAND} --build . --target install --config ${CONFIG}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMAND_ECHO STDOUT
     RESULT_VARIABLE BUILD_RESULT)
 
   if(NOT "${BUILD_RESULT}" STREQUAL "0")
     message(FATAL_ERROR "Fatal error while building external project ${TARGET}")
   endif()
 
+  # Install external project
   execute_process(
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${INSTALL_DIR}
-    ${INSTALL_COMMANDS}
-    ${COMMANDS}
-    COMMAND ${CMAKE_COMMAND} -E touch EXTERNAL_BUILT_${CONFIG}
+    COMMAND ${CMAKE_COMMAND} --build . --target install --config ${CONFIG}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMAND_ECHO STDOUT
     RESULT_VARIABLE INSTALL_RESULT)
 
   if(NOT "${INSTALL_RESULT}" STREQUAL "0")
     message(FATAL_ERROR "Fatal error while installing external project ${TARGET}")
+  endif()
+
+  # Execute commands (mostly for copying files)
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${INSTALL_DIR}
+    ${INSTALL_COMMANDS}
+    ${COMMANDS}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMAND_ECHO STDOUT
+    RESULTS_VARIABLE COMMANDS_RESULTS)
+
+  foreach(COMMAND_RESULT IN LISTS COMMANDS_RESULTS)
+    if(NOT "${COMMAND_RESULT}" STREQUAL "0")
+      message(FATAL_ERROR "Fatal error while executing install commands for external project ${TARGET}")
+    endif()
+  endforeach()
+
+  # Execute commands (mostly for copying files)
+  execute_process(
+    COMMAND ${CMAKE_COMMAND} -E touch EXTERNAL_BUILT_${CONFIG}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    RESULT_VARIABLE TOUCH_RESULT)
+
+  if(NOT "${TOUCH_RESULT}" STREQUAL "0")
+    message(WARNING "Could not create EXTERNAL_BUILT file for external project ${TARGET}")
   endif()
 endif()
