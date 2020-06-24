c66d100b15744c43a9d0cfc1a445cea44e2e5400
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2020-06-24 13:43:52 +0200

    added empty param widget group class ...

---
+++ plugins/gui/src/ParamWidgetGroups.cpp
@@ -0,0 +1,18 @@
+/*
+ * ParamWidgetGroups.cpp
+ *
+ * Copyright (C) 2020 by Universitaet Stuttgart (VIS).
+ * Alle Rechte vorbehalten.
+ */
+
+#include "stdafx.h"
+#include "ParamWidgetGroups.h"
+
+
+using namespace megamol;
+using namespace megamol::gui;
+using namespace megamol::gui::configurator;
+
+megamol::gui::configurator::ParamWidgetGroups::ParamWidgetGroups(void) {}
+
+megamol::gui::configurator::ParamWidgetGroups::~ParamWidgetGroups(void) {}
