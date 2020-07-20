bb0a81a79a28f70360b9d181cf866433aae9ff90
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2020-07-20 12:58:42 +0200

    pasted commented code for picking

--- plugins/gui/src/widgets/WidgetPicking_gl.h
+++ plugins/gui/src/widgets/WidgetPicking_gl.h
@@ -11,6 +11,8 @@
 
 #include "GUIUtils.h"
 
+#include "glowl/FramebufferObject.hpp"
+
 #include "mmcore/view/Input.h"
 
 #include "vislib/sys/Log.h"
@@ -22,24 +24,45 @@ namespace gui {
 
 /**
  * OpenGL implementation of widget picking.
+ *
+ * Code adapted from megamol::mesh::Render3DUI
+ *
  */
 class WidgetPicking {
 public:
     WidgetPicking(void);
 
-    ~WidgetPicking(void) = default;
+    ~WidgetPicking(void);
 
-    void ProcessMouseMove(double x, double y);
+    bool ProcessMouseMove(double x, double y);
 
-    void ProcessMouseClick(megamol::core::view::MouseButton button, megamol::core::view::MouseButtonAction action,
+    bool ProcessMouseClick(megamol::core::view::MouseButton button, megamol::core::view::MouseButtonAction action,
         megamol::core::view::Modifiers mods);
 
     bool Enable(void);
 
     bool Disable(void);
 
+    static void DrawPickalbleCircle(unsigned int id);
+
 private:
     // VARIABLES --------------------------------------------------------------
+
+    double m_cursor_x, m_cursor_y;
+
+    /**
+     * Set to true if cursor on interactable object during current frame with respective obj id as second value
+     * Set to fale false if cursor on "background" during current frame with -1 as second value
+     */
+    std::pair<bool, int> m_cursor_on_interaction_obj;
+
+    /**
+     * Set to true if  with respective obj id as second value
+     * Set to fale false if cursor on "background" during current frame with -1 as second value
+     */
+    std::pair<bool, int> m_active_interaction_obj;
+
+    std::unique_ptr<glowl::FramebufferObject> m_fbo;
 };
 
 
