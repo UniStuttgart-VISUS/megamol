daf16aa1b00878050aacf1a9ef65af6b3a72a4b1
invor <michael.becher.1990@googlemail.com>
2020-09-18 18:49:35 +0200

    Fixed logical error in event storage

--- core/src/EventStorage.cpp
+++ core/src/EventStorage.cpp
@@ -33,29 +33,35 @@ bool megamol::core::EventStorage::readDataCallback(core::Call& caller) {
     auto ec = dynamic_cast<EventCallRead*>(&caller);
     if (ec == nullptr) return false;
 
+    auto current_frame_id = this->GetCoreInstance()->GetFrameID();
+
+    // the first call in a frame to either readData or writeData needs to swap the double buffer
+    if (current_frame_id > this->m_version) {
+        this->m_version = current_frame_id;
+        m_events[m_read_idx]->clear();
+        m_read_idx = m_read_idx == 0 ? 1 : 0;
+    }
+
     ec->setData(m_events[m_read_idx], this->GetCoreInstance()->GetFrameID());
     
     return true;
 }
 
 bool megamol::core::EventStorage::writeDataCallback(core::Call& caller) { 
-    auto ec = dynamic_cast<EventCallRead*>(&caller);
+    auto ec = dynamic_cast<EventCallWrite*>(&caller);
     if (ec == nullptr) return false;
 
     auto current_frame_id = this->GetCoreInstance()->GetFrameID();
 
+    // the first call in a frame to either readData or writeData needs to swap the double buffer
     if (current_frame_id > this->m_version) {
         this->m_version = current_frame_id;
-        auto write_idx = m_read_idx;
+        m_events[m_read_idx]->clear();    
         m_read_idx = m_read_idx == 0 ? 1 : 0;
-        m_events[write_idx]->clear();
-
-        ec->setData(m_events[write_idx], current_frame_id);
-        
-    } else {
-        auto write_idx = m_read_idx == 0 ? 1 : 0;
-        ec->setData(m_events[write_idx], current_frame_id);
     }
+
+    auto write_idx = m_read_idx == 0 ? 1 : 0;
+    ec->setData(m_events[write_idx], current_frame_id);
 
     return true;
 }
