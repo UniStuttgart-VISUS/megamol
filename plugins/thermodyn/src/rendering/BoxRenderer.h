05abe90c3934d0bd22e078d8850d88a30cfde429
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2020-10-07 17:20:20 +0200

    Update BoxRenderer.h

--- plugins/thermodyn/src/rendering/BoxRenderer.h
+++ plugins/thermodyn/src/rendering/BoxRenderer.h
@@ -80,12 +80,12 @@ private:
     static void prepareData(std::vector<BoxDataCall::box_entry_t> const& indata,
         std::pair<std::vector<float>, std::vector<float>>& outdata) {
 
+        pos.clear();
+        col.clear();
         auto& pos = outdata.first;
         pos.reserve(indata.size() * 72);
         auto& col = outdata.second;
         col.reserve(indata.size() * 96);
-        pos.clear();
-        col.clear();
 
         for (auto const& e : indata) {
             auto const& box = e.box_;
