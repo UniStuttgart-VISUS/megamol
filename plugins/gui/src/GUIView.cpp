2b520bfcfeae27c417141f783601c2403911f447
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2020-10-17 14:26:39 +0200

    ...

--- plugins/gui/src/GUIView.cpp
+++ plugins/gui/src/GUIView.cpp
@@ -34,16 +34,17 @@ GUIView::~GUIView() {
 
 
 bool GUIView::create() {
+
     return gui.CreateContext_GL(this->GetCoreInstance());
     /* NEW FORONTEND
-if (this->GetCoreInstance()->IsmmconsoleFrontendCompatible()) {
-    return gui.CreateContext_GL(this->GetCoreInstance());
-} else {
-    megamol::core::utility::log::Log::DefaultLog.WriteError(
-        "[GUI] GUIView module can only be used with mmconsole frontend. [%s, %s, line %d]\n", __FILE__,
-        __FUNCTION__, __LINE__);
-    return false;
-}
+    if (this->GetCoreInstance()->IsmmconsoleFrontendCompatible()) {
+        return gui.CreateContext_GL(this->GetCoreInstance());
+    } else {
+        megamol::core::utility::log::Log::DefaultLog.WriteError(
+            "[GUI] GUIView module can only be used with mmconsole frontend. [%s, %s, line %d]\n", __FILE__,
+            __FUNCTION__, __LINE__);
+        return false;
+    }
     */
 }
 
