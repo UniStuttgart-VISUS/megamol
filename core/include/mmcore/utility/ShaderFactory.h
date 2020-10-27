8f48e3ee1729a5cca5ad12b4f753dd109602b185
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2020-10-27 10:24:17 +0100

    moved add_vendor_definition to shader factory lib

--- core/include/mmcore/utility/ShaderFactory.h
+++ core/include/mmcore/utility/ShaderFactory.h
@@ -54,26 +54,6 @@ inline GLint check_and_log_program_link(GLuint program) {
 }
 
 
-inline void add_vendor_definition(megamol::shaderfactory::compiler_options& options) {
-    std::basic_string<GLubyte> const tmp(glGetString(GL_VENDOR));
-    std::string vendor_str;
-    vendor_str.resize(tmp.size());
-    std::transform(tmp.cbegin(), tmp.cend(), vendor_str.begin(), [](GLubyte chr) { return std::tolower(chr); });
-
-    if (vendor_str.find("ati") != std::string::npos || vendor_str.find("amd") != std::string::npos) {
-        options.add_definition("__AMD__");
-    }
-
-    if (vendor_str.find("nvidia") != std::string::npos) {
-        options.add_definition("__NVIDIA__");
-    }
-
-    if (vendor_str.find("intel") != std::string::npos) {
-        options.add_definition("__INTEL__");
-    }
-}
-
-
 GLuint make_shader(
     std::filesystem::path const& shader_source_path, megamol::shaderfactory::compiler_options const& options);
 
@@ -81,11 +61,9 @@ GLuint make_shader(
 template <typename... Paths>
 std::enable_if_t<(std::is_convertible_v<std::filesystem::path, Paths> && ...), GLuint> make_program(
     megamol::shaderfactory::compiler_options const& options, Paths... paths) {
-    megamol::shaderfactory::compiler_options tmp_opt = options;
-    add_vendor_definition(tmp_opt);
     constexpr std::size_t size = sizeof...(paths);
 
-    GLuint shaders[size] = {make_shader(paths, tmp_opt)...};
+    GLuint shaders[size] = {make_shader(paths, options)...};
 
     auto program = glCreateProgram();
 
