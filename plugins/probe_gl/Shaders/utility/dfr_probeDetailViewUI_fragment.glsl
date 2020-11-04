c76209dcd434ff81d3763d17969badbd8a5edf75
invor <michael.becher.1990@googlemail.com>
2020-11-04 19:23:47 +0100

    White UI elements in detail view

--- plugins/probe_gl/Shaders/utility/dfr_probeDetailViewUI_fragment.glsl
+++ plugins/probe_gl/Shaders/utility/dfr_probeDetailViewUI_fragment.glsl
@@ -3,7 +3,7 @@ layout(location = 1) out vec3 normal_out;
 layout(location = 2) out float depth_out;
 
 void main(void) {
-    albedo_out = vec4(1.0,0.0,1.0,1.0);
+    albedo_out = vec4(1.0,1.0,1.0,1.0);
     normal_out = vec3(1.0);
     depth_out = gl_FragCoord.z;
 }
