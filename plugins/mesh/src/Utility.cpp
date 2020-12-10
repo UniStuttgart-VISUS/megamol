6f9b951eb036edc566e042a55b1b3c3716dcb536
invor <michael.becher.1990@googlemail.com>
2020-12-11 00:18:29 +0100

    Fixing compile errors..

--- plugins/mesh/src/Utility.cpp
+++ plugins/mesh/src/Utility.cpp
@@ -4,7 +4,7 @@ namespace megamol {
 namespace mesh {
 namespace utility {
 
-std::tuple<VertexPositions, VertexNormals, QuadFaceIndices> tessellateFace(
+std::tuple<VertexPositions, std::vector<std::array<float, 3>>, QuadFaceIndices> tessellateFace(
     glm::vec3 v00, glm::vec3 v10, glm::vec3 v11, glm::vec3 v01, unsigned int u_subdivs, unsigned int v_subdivs) {
 
     VertexPositions vertex_positions;
@@ -17,7 +17,8 @@ std::tuple<VertexPositions, VertexNormals, QuadFaceIndices> tessellateFace(
     // TODO iterate subdivs, create faces as you go
 
 
-    return std::make_tuple < std::move(vertex_positions), std::move(vertex_normals), std::move(quad_indices));
+    return std::make_tuple<VertexPositions, VertexNormals, QuadFaceIndices>(
+        std::move(vertex_positions), std::move(vertex_normals), std::move(quad_indices));
 }
 
 } // namespace utility
--- plugins/mesh/src/Utility.h
+++ plugins/mesh/src/Utility.h
@@ -6,7 +6,9 @@
 #ifndef MESH_UTILITY_H_INCLUDED
 #define MESH_UTILITY_H_INCLUDED
 
+#include <array>
 #include <vector>
+#include <tuple>
 
 #include "glm/glm.hpp"
 
@@ -14,9 +16,9 @@ namespace megamol {
 namespace mesh {
 namespace utility {
 
-    typedef std::vector<std::array<float, 3>> VertexPositions;
-    typedef std::vector<std::array<float, 3>> VertexNormals;
-    typedef std::vector<std::array<uint32_t, 4>> QuadFaceIndices;
+    using VertexPositions = std::vector<std::array<float, 3>>;
+    using VertexNormals = std::vector<std::array<float, 3>>;
+    using QuadFaceIndices = std::vector<std::array<uint32_t, 4>>;
 
     std::tuple<VertexPositions, VertexNormals, QuadFaceIndices> tessellateFace(
         glm::vec3 v00, glm::vec3 v10, glm::vec3 v11, glm::vec3 v01, unsigned int u_subdivs, unsigned int v_subdivs);
