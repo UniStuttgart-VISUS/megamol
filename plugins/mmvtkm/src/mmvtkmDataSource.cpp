2fbe840f9ffb03df5b1636f71f9491f36452264a
sellendk <st105950>
2020-12-14 18:52:52 +0100

    added try-catch around data fetching

--- plugins/mmvtkm/src/mmvtkmDataSource.cpp
+++ plugins/mmvtkm/src/mmvtkmDataSource.cpp
@@ -132,146 +132,154 @@ bool mmvtkmDataSource::getDataCallback(core::Call& caller) {
 		/////////////////////////////
 		// get tetrahedron coordinates from csv file through adios
 		/////////////////////////////
-		std::string coord_type = rhsTopNodesCad->getData("x")->getType();
-		std::string label_type = rhsTopNodesCad->getData("node-label")->getType();
+        try {
+            std::string coordType = rhsTopNodesCad->getData("x")->getType();
+            std::string labelType = rhsTopNodesCad->getData("node-label")->getType();
 
-		// this assumes the data type from the file is float
-		// TODO do for general case
-		std::vector<float> node_labels = rhsTopNodesCad->getData("node-label")->GetAsFloat();
-		std::vector<float> x_coord = rhsTopNodesCad->getData("x")->GetAsFloat();
-		std::vector<float> y_coord = rhsTopNodesCad->getData("y")->GetAsFloat();
-		std::vector<float> z_coord = rhsTopNodesCad->getData("z")->GetAsFloat();
-		std::vector<float> s11 = rhsTopNodesCad->getData("s11")->GetAsFloat();
-		std::vector<float> s12 = rhsTopNodesCad->getData("s12")->GetAsFloat();
-		std::vector<float> s13 = rhsTopNodesCad->getData("s13")->GetAsFloat();
-		std::vector<float> s21 = s12;
-		std::vector<float> s22 = rhsTopNodesCad->getData("s22")->GetAsFloat();
-		std::vector<float> s23 = rhsTopNodesCad->getData("s23")->GetAsFloat();
-		std::vector<float> s31 = s13;
-		std::vector<float> s32 = s23;
-		std::vector<float> s33 = rhsTopNodesCad->getData("s33")->GetAsFloat();
-		std::vector<float> hs1_wert = rhsTopNodesCad->getData("hs1_wert")->GetAsFloat();
-		std::vector<float> hs1_x = rhsTopNodesCad->getData("hs1_x")->GetAsFloat();
-		std::vector<float> hs1_y = rhsTopNodesCad->getData("hs1_y")->GetAsFloat();
-		std::vector<float> hs1_z = rhsTopNodesCad->getData("hs1_z")->GetAsFloat();
-		std::vector<float> hs2_wert = rhsTopNodesCad->getData("hs2_wert")->GetAsFloat();
-		std::vector<float> hs2_x = rhsTopNodesCad->getData("hs2_x")->GetAsFloat();
-		std::vector<float> hs2_y = rhsTopNodesCad->getData("hs2_y")->GetAsFloat();
-		std::vector<float> hs2_z = rhsTopNodesCad->getData("hs2_z")->GetAsFloat();
-		std::vector<float> hs3_wert = rhsTopNodesCad->getData("hs3_wert")->GetAsFloat();
-		std::vector<float> hs3_x = rhsTopNodesCad->getData("hs3_x")->GetAsFloat();
-		std::vector<float> hs3_y = rhsTopNodesCad->getData("hs3_y")->GetAsFloat();
-		std::vector<float> hs3_z = rhsTopNodesCad->getData("hs3_z")->GetAsFloat();
+            // this assumes the data type from the file is float
+            // TODO do for general case
+            std::vector<float> nodeLabels = rhsTopNodesCad->getData("node-label")->GetAsFloat();
+            std::vector<float> xCoord = rhsTopNodesCad->getData("x")->GetAsFloat();
+            std::vector<float> yCoord = rhsTopNodesCad->getData("y")->GetAsFloat();
+            std::vector<float> zCoord = rhsTopNodesCad->getData("z")->GetAsFloat();
+            std::vector<float> s11 = rhsTopNodesCad->getData("s11")->GetAsFloat();
+            std::vector<float> s12 = rhsTopNodesCad->getData("s12")->GetAsFloat();
+            std::vector<float> s13 = rhsTopNodesCad->getData("s13")->GetAsFloat();
+            std::vector<float> s21 = s12;
+            std::vector<float> s22 = rhsTopNodesCad->getData("s22")->GetAsFloat();
+            std::vector<float> s23 = rhsTopNodesCad->getData("s23")->GetAsFloat();
+            std::vector<float> s31 = s13;
+            std::vector<float> s32 = s23;
+            std::vector<float> s33 = rhsTopNodesCad->getData("s33")->GetAsFloat();
+            std::vector<float> hs1Value = rhsTopNodesCad->getData("hs1_wert")->GetAsFloat();
+            std::vector<float> hs1X = rhsTopNodesCad->getData("hs1_x")->GetAsFloat();
+            std::vector<float> hs1Y = rhsTopNodesCad->getData("hs1_y")->GetAsFloat();
+            std::vector<float> hs1Z = rhsTopNodesCad->getData("hs1_z")->GetAsFloat();
+            std::vector<float> hs2Value = rhsTopNodesCad->getData("hs2_wert")->GetAsFloat();
+            std::vector<float> hs2X = rhsTopNodesCad->getData("hs2_x")->GetAsFloat();
+            std::vector<float> hs2Y = rhsTopNodesCad->getData("hs2_y")->GetAsFloat();
+            std::vector<float> hs2Z = rhsTopNodesCad->getData("hs2_z")->GetAsFloat();
+            std::vector<float> hs3Value = rhsTopNodesCad->getData("hs3_wert")->GetAsFloat();
+            std::vector<float> hs3X = rhsTopNodesCad->getData("hs3_x")->GetAsFloat();
+            std::vector<float> hs3Y = rhsTopNodesCad->getData("hs3_y")->GetAsFloat();
+            std::vector<float> hs3Z = rhsTopNodesCad->getData("hs3_z")->GetAsFloat();
 
-		// TODO check if size of each vector is the same (assert)
-		std::vector<float> element_label = rhsBottomLabelCad->getData("element-label")->GetAsFloat();
-		std::vector<float> nodeA = rhsBottomLabelCad->getData("nodea")->GetAsFloat();
-		std::vector<float> nodeB = rhsBottomLabelCad->getData("nodeb")->GetAsFloat();
-		std::vector<float> nodeC = rhsBottomLabelCad->getData("nodec")->GetAsFloat();
-		std::vector<float> nodeD = rhsBottomLabelCad->getData("noded")->GetAsFloat();
+            // TODO check if size of each vector is the same
+            std::vector<float> elementLabel = rhsBottomLabelCad->getData("element-label")->GetAsFloat();
+            std::vector<float> nodeA = rhsBottomLabelCad->getData("nodea")->GetAsFloat();
+            std::vector<float> nodeB = rhsBottomLabelCad->getData("nodeb")->GetAsFloat();
+            std::vector<float> nodeC = rhsBottomLabelCad->getData("nodec")->GetAsFloat();
+            std::vector<float> nodeD = rhsBottomLabelCad->getData("noded")->GetAsFloat();
+        
 
-
-		int num_elements = element_label.size();
-		int num_nodes = node_labels.size();
-		vtkm::cont::DataSetBuilderExplicit dataSetBuilder;
-		vtkm::cont::DataSetFieldAdd dataSetFieldAdd;
-
-
-		int num_skipped = 0;
-		std::vector<Vec3f> points(num_nodes);
-		std::vector<Vec3f> point_hs1(num_nodes);
-		std::vector<Vec3f> point_hs2(num_nodes);
-		std::vector<Vec3f> point_hs3(num_nodes);
-		std::vector<Vec3f> point_hs(num_nodes);
-		std::vector<vtkm::Matrix<vtkm::Float32, 3, 3>> point_tensors(num_nodes);
-		//std::vector<vtkm::Id> cell_indices(num_elements * 4);
-		std::vector<vtkm::Id> cell_indices;
-		std::vector<vtkm::IdComponent> num_indices;
-		std::vector<vtkm::UInt8> cell_shapes;
+		    int numElements = elementLabel.size();
+		    int numNodes = nodeLabels.size();
+		    vtkm::cont::DataSetBuilderExplicit dataSetBuilder;
+		    vtkm::cont::DataSetFieldAdd dataSetFieldAdd;
 
 
-		for (int i = 0; i < num_nodes; ++i) {
-		    Vec3f point = {x_coord[i], y_coord[i], z_coord[i]};
-		    Vec3f hs1 = {hs1_x[i], hs1_y[i], hs1_z[i]};
-		    Vec3f hs2 = {hs2_x[i], hs2_y[i], hs2_z[i]};
-		    Vec3f hs3 = {hs3_x[i], hs3_y[i], hs3_z[i]};
-
-			vtkm::Matrix<vtkm::Float32, 3, 3> tensor;
-		    tensor[0][0] = s11[i];
-		    tensor[0][1] = s12[i];
-		    tensor[0][2] = s13[i];
-		    tensor[1][0] = s21[i];
-		    tensor[1][1] = s22[i];
-		    tensor[1][2] = s23[i];
-		    tensor[2][0] = s31[i];
-		    tensor[2][1] = s32[i];
-		    tensor[2][2] = s33[i];
-
-			Vec3f hs1_tens_prod = hs1_wert[i] * vtkm::MatrixMultiply(tensor, hs1);
-		    Vec3f hs2_tens_prod = hs2_wert[i] * vtkm::MatrixMultiply(tensor, hs2);
-		    Vec3f hs3_tens_prod = hs3_wert[i] * vtkm::MatrixMultiply(tensor, hs3);
-
-			points[i] = point;
-		    point_tensors[i] = tensor;
-		    point_hs1[i] = hs1;
-		    point_hs2[i] = hs2;
-		    point_hs3[i] = hs3;
-		    point_hs[i] = hs1_tens_prod + hs2_tens_prod + hs3_tens_prod;
-		}
+		    int numSkipped = 0;
+		    std::vector<Vec3f> points(numNodes);
+		    std::vector<Vec3f> pointHs1(numNodes);
+		    std::vector<Vec3f> pointHs2(numNodes);
+		    std::vector<Vec3f> pointHs3(numNodes);
+		    std::vector<Vec3f> pointHs(numNodes);
+		    std::vector<vtkm::Matrix<vtkm::Float32, 3, 3>> pointTensors(numNodes);
+		    //std::vector<vtkm::Id> cell_indices(numElements * 4);
+		    std::vector<vtkm::Id> cellIndices;
+		    std::vector<vtkm::IdComponent> numIndices;
+		    std::vector<vtkm::UInt8> cellShapes;
 
 
-		for (int i = 0; i < num_elements; ++i) {
-		    std::vector<int> labels = {(int)nodeA[i], (int)nodeB[i], (int)nodeC[i], (int)nodeD[i]};
-		    std::vector<vtkm::Id> index_buffer(4);
+		    for (int i = 0; i < numNodes; ++i) {
+		        Vec3f point = {xCoord[i], yCoord[i], zCoord[i]};
+		        Vec3f hs1 = {hs1X[i], hs1Y[i], hs1Z[i]};
+		        Vec3f hs2 = {hs2X[i], hs2Y[i], hs2Z[i]};
+		        Vec3f hs3 = {hs3X[i], hs3Y[i], hs3Z[i]};
 
-		    bool not_found = false;
+		    	vtkm::Matrix<vtkm::Float32, 3, 3> tensor;
+		        tensor[0][0] = s11[i];
+		        tensor[0][1] = s12[i];
+		        tensor[0][2] = s13[i];
+		        tensor[1][0] = s21[i];
+		        tensor[1][1] = s22[i];
+		        tensor[1][2] = s23[i];
+		        tensor[2][0] = s31[i];
+		        tensor[2][1] = s32[i];
+		        tensor[2][2] = s33[i];
 
-		    // BUILD TETRAHEDRON HERE
-		    // get vertex cooridnates for current tetrahedron vertices
-		    for (int j = 0; j < 4; ++j) {
-		        auto it = std::find(node_labels.begin(), node_labels.end(), labels[j]);
-		        int node_index = std::distance(node_labels.begin(), it);
-		        if (node_index == node_labels.size()) {
-		            // core::utility::log::Log::DefaultLog.WriteInfo("(%i, %i) with %i", i, j, labels[j]);
-		            ++num_skipped;
-		            not_found = true;
-		            break;
+		    	Vec3f hs1TensProd = hs1Value[i] * vtkm::MatrixMultiply(tensor, hs1);
+		        Vec3f hs2TensProd = hs2Value[i] * vtkm::MatrixMultiply(tensor, hs2);
+		        Vec3f hs3TensProd = hs3Value[i] * vtkm::MatrixMultiply(tensor, hs3);
+
+		    	points[i] = point;
+		        pointTensors[i] = tensor;
+		        pointHs1[i] = hs1;
+		        pointHs2[i] = hs2;
+		        pointHs3[i] = hs3;
+		        pointHs[i] = hs1TensProd + hs2TensProd + hs3TensProd;
+		    }
+
+
+		    for (int i = 0; i < numElements; ++i) {
+		        std::vector<int> labels = {(int)nodeA[i], (int)nodeB[i], (int)nodeC[i], (int)nodeD[i]};
+		        std::vector<vtkm::Id> indexBuffer(4);
+
+		        bool notFound = false;
+
+		        // BUILD TETRAHEDRON HERE
+		        // get vertex cooridnates for current tetrahedron vertices
+		        for (int j = 0; j < 4; ++j) {
+		            auto it = std::find(nodeLabels.begin(), nodeLabels.end(), labels[j]);
+		            int nodeIndex = std::distance(nodeLabels.begin(), it);
+		            if (nodeIndex == nodeLabels.size()) {
+		                // core::utility::log::Log::DefaultLog.WriteInfo("(%i, %i) with %i", i, j, labels[j]);
+		                ++numSkipped;
+		                notFound = true;
+		                break;
+		            }
+
+		            indexBuffer[j] = nodeIndex;
 		        }
 
-		        index_buffer[j] = node_index;
+		        if (notFound) continue;
+
+		    	cellShapes.emplace_back(vtkm::CELL_SHAPE_TETRA);
+		        numIndices.emplace_back(4);	// tetrahedrons always have 4 vertices
+
+		        for (int j = 0; j < 4; ++j) {
+		            cellIndices.emplace_back(indexBuffer[j]);
+		        }
 		    }
 
-		    if (not_found) continue;
+		    core::utility::log::Log::DefaultLog.WriteInfo("Number of skipped tetrahedrons: %i", numSkipped);
 
-			cell_shapes.emplace_back(vtkm::CELL_SHAPE_TETRA);
-		    num_indices.emplace_back(4);	// tetrahedrons always have 4 vertices
+		    vtkmData_ = dataSetBuilder.Create(points, cellShapes, numIndices, cellIndices);
+		    dataSetFieldAdd.AddPointField(vtkmData_, "hs1", pointHs1);
+		    dataSetFieldAdd.AddPointField(vtkmData_, "hs2", pointHs2);
+		    dataSetFieldAdd.AddPointField(vtkmData_, "hs3", pointHs3);
+		    dataSetFieldAdd.AddPointField(vtkmData_, "hs", pointHs);
 
-		    for (int j = 0; j < 4; ++j) {
-		        cell_indices.emplace_back(index_buffer[j]);
-		    }
-		}
+		    // vtkm::io::writer::VTKDataSetWriter writer("tetrahedron.vtk");
+		    // writer.WriteDataSet(vtkmData_);
+		    // core::utility::log::Log::DefaultLog.WriteInfo("vtkmData_ is successfully stored in tetrahedron.vtk.");
 
-		core::utility::log::Log::DefaultLog.WriteInfo("Number of skipped tetrahedrons: %i", num_skipped);
+		    // get min max bounds from dataset
+            minMaxBounds_ = vtkmData_.GetCoordinateSystem(0).GetBounds();
 
-		vtkmData_ = dataSetBuilder.Create(points, cell_shapes, num_indices, cell_indices);
-		dataSetFieldAdd.AddPointField(vtkmData_, "hs1", point_hs1);
-		dataSetFieldAdd.AddPointField(vtkmData_, "hs2", point_hs2);
-		dataSetFieldAdd.AddPointField(vtkmData_, "hs3", point_hs3);
-		dataSetFieldAdd.AddPointField(vtkmData_, "hs", point_hs);
+		    lhsVtkmDc->UpdateDataChanges(true);
+            lhsVtkmDc->SetDataSet(&this->vtkmData_);
 
-		// vtkm::io::writer::VTKDataSetWriter writer("tetrahedron.vtk");
-		// writer.WriteDataSet(vtkmData_);
-		// core::utility::log::Log::DefaultLog.WriteInfo("vtkmData_ is successfully stored in tetrahedron.vtk.");
+            return true;
+        }
+        catch (const std::exception& e) {
+            core::utility::log::Log::DefaultLog.WriteError("In %s at line %d. \n", __FILE__, __LINE__);
+            core::utility::log::Log::DefaultLog.WriteError(e.what());
 
-		// get min max bounds from dataset
-        minMaxBounds_ = vtkmData_.GetCoordinateSystem(0).GetBounds();
-
-		lhsVtkmDc->UpdateDataChanges(true);
-        lhsVtkmDc->SetDataSet(&this->vtkmData_);
-
-        return true;
+            return false;
+        }
     }
-
+    
 
 	lhsVtkmDc->UpdateDataChanges(false);
 
