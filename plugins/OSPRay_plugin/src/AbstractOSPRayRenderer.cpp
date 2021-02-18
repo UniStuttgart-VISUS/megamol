c635a8d8ae26da37f44ce30916d36693c7671352
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2021-02-18 16:11:02 +0100

    mesh fix and fix for on delete crash

--- plugins/OSPRay_plugin/src/AbstractOSPRayRenderer.cpp
+++ plugins/OSPRay_plugin/src/AbstractOSPRayRenderer.cpp
@@ -369,11 +369,11 @@ namespace ospray {
 
     void AbstractOSPRayRenderer::fillLightArray(std::array<float,4> eyeDir) {
         // clear current lights
-        if (!_lightArray.empty()) {
+        /*if (!_lightArray.empty()) {
             for (auto& l : _lightArray) {
                 ospRelease(l.handle());
             }
-        }
+        }*/
         _lightArray.clear();
 
         // create custom ospray light
@@ -1054,11 +1054,14 @@ namespace ospray {
 
                                 unsigned long long stride = 3 * sizeof(unsigned int);
                                 auto osp_type = OSP_VEC3UI;
+                                auto divider = 3ull;
 
                                 if (mesh_type == mesh::MeshDataAccessCollection::QUADS) {
                                     stride = 4 * sizeof(unsigned int);
                                     osp_type = OSP_VEC4UI;
+                                    divider = 4ull;
                                 }
+                                count /= divider;
                                 
                                 auto indexData =
                                     ::ospray::cpp::SharedData(mesh.second.indices.data, osp_type, count, stride);
@@ -1394,9 +1397,9 @@ namespace ospray {
 
         for (auto& entry : this->_structureMap) {
 
-            if (_instances[entry.first]) {
+            /*if (_instances[entry.first]) {
                 ospRelease(_instances[entry.first].handle());
-            }
+            }*/
             _instances.erase(entry.first);
 
             auto const& element = entry.second;
