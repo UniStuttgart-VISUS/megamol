070702468c49e0f54b8d5802ad280f63d559e111
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2021-03-12 15:08:18 +0100

    minor revert

--- plugins/gui/src/GUIWindows.cpp
+++ plugins/gui/src/GUIWindows.cpp
@@ -260,11 +260,10 @@ bool GUIWindows::PreDraw(glm::vec2 framebuffer_size, glm::vec2 window_size, doub
     // Set ImGui context
     ImGui::SetCurrentContext(this->context);
     // Propagate ImGui context to core instance
-    // if ((this->core_instance != nullptr) && core_instance->IsmmconsoleFrontendCompatible()) { /// mmconsole
-    this->core_instance->SetCurrentImGuiContext(this->context);
-    //} else {
-    /// !!! TODO Move to separate GUI resource which is available in modules
-    //}
+    if ((this->core_instance != nullptr) && core_instance->IsmmconsoleFrontendCompatible()) { /// mmconsole
+        /// TODO Also for new frontend?
+        this->core_instance->SetCurrentImGuiContext(this->context);
+    }
 
     // Create new gui graph once if core instance graph is used (otherwise graph should already exist)
     if (this->state.graph_uid == GUI_INVALID_ID) {
