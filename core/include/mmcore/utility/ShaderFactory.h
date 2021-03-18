e326a6619388ca5973e3df2c487995f0cfe76034
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2021-03-18 13:19:20 +0100

    using the shadersourcelist constructor of glowl::GLSLProgram

--- core/include/mmcore/utility/ShaderFactory.h
+++ core/include/mmcore/utility/ShaderFactory.h
@@ -1,7 +1,7 @@
 /*
  * ShaderFactory.h
  *
- * Copyright (C) 2020 by Universitaet Stuttgart (VISUS). Alle Rechte vorbehalten.
+ * Copyright (C) 2020-2021 by Universitaet Stuttgart (VISUS). Alle Rechte vorbehalten.
  */
 #pragma once
 
@@ -14,6 +14,8 @@
 #include "msf/compiler_options.h"
 
 #include "mmcore/utility/log/Log.h"
+
+#include "glowl/GLSLProgram.hpp"
 
 namespace megamol::core::utility {
 
@@ -88,6 +90,17 @@ std::enable_if_t<(std::is_convertible_v<std::filesystem::path, Paths> && ...), G
     }
 
     return program;
+}
+
+
+glowl::GLSLProgram::ShaderSourceList::value_type make_glowl_shader_source(
+    std::filesystem::path const& shader_source_path, megamol::shaderfactory::compiler_options const& options);
+
+
+template<typename... Paths>
+std::enable_if_t<(std::is_convertible_v<std::filesystem::path, Paths> && ...), glowl::GLSLProgram::ShaderSourceList> make_glowl_shader_source_list(
+    megamol::shaderfactory::compiler_options const& options, Paths... paths) {
+    return {make_glowl_shader_source(paths, options)...};
 }
 
 } // end namespace megamol::core::utility
--- core/src/utility/ShaderFactory.cpp
+++ core/src/utility/ShaderFactory.cpp
@@ -1,7 +1,7 @@
 /*
  * ShaderFactory.cpp
  *
- * Copyright (C) 2020 by Universitaet Stuttgart (VISUS). Alle Rechte vorbehalten.
+ * Copyright (C) 2020-2021 by Universitaet Stuttgart (VISUS). Alle Rechte vorbehalten.
  */
 #include "stdafx.h"
 #include "mmcore/utility/ShaderFactory.h"
@@ -28,4 +28,13 @@ GLuint megamol::core::utility::make_shader(
     }
 
     return shader;
+}
+
+
+glowl::GLSLProgram::ShaderSourceList::value_type megamol::core::utility::make_glowl_shader_source(
+    std::filesystem::path const& shader_source_path, megamol::shaderfactory::compiler_options const& options) {
+    auto const shader_string = msf_cp.preprocess(shader_source_path, options);
+    auto const type = megamol::shaderfactory::get_shader_type_ogl(shader_source_path);
+
+    return std::make_pair(static_cast<glowl::GLSLProgram::ShaderType>(type), shader_string);
 }
