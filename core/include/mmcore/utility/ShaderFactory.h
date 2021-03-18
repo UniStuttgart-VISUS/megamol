f3f090348248f4a95e8eb49f74a82e7dbcba5686
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2021-03-18 17:11:21 +0100

    removed deprecated code

--- core/include/mmcore/utility/ShaderFactory.h
+++ core/include/mmcore/utility/ShaderFactory.h
@@ -9,89 +9,13 @@
 #include <filesystem>
 #include <string>
 
-#include "glad/glad.h"
-
 #include "msf/compiler_options.h"
 
-#include "mmcore/utility/log/Log.h"
+#include "glad/glad.h"
 
 #include "glowl/GLSLProgram.hpp"
 
 namespace megamol::core::utility {
-
-inline GLint check_and_log_shader_compile(GLuint shader) {
-    GLint success = 0;
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-
-    if (!success) {
-        GLint logSize = 0;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &logSize);
-        std::string shader_info;
-        shader_info.resize(logSize);
-        glGetShaderInfoLog(shader, logSize, &logSize, shader_info.data());
-
-        megamol::core::utility::log::Log::DefaultLog.WriteError("MSF Shader Compile Error:\n%s", shader_info.data());
-    }
-
-    return success;
-}
-
-
-inline GLint check_and_log_program_link(GLuint program) {
-    GLint success = 0;
-    glGetProgramiv(program, GL_LINK_STATUS, &success);
-
-    if (!success) {
-        GLint logSize = 0;
-        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &logSize);
-        std::string program_info;
-        program_info.resize(logSize);
-        glGetProgramInfoLog(program, logSize, &logSize, program_info.data());
-
-        megamol::core::utility::log::Log::DefaultLog.WriteError("MSF Program Link Error:\n%s", program_info.data());
-    }
-
-    return success;
-}
-
-
-GLuint make_shader(
-    std::filesystem::path const& shader_source_path, megamol::shaderfactory::compiler_options const& options);
-
-
-template<typename... Paths>
-std::enable_if_t<(std::is_convertible_v<std::filesystem::path, Paths> && ...), GLuint> make_program(
-    megamol::shaderfactory::compiler_options const& options, Paths... paths) {
-    constexpr std::size_t size = sizeof...(paths);
-
-    GLuint shaders[size] = {make_shader(paths, options)...};
-
-    auto program = glCreateProgram();
-
-    for (std::size_t idx = 0; idx < size; ++idx) {
-        glAttachShader(program, shaders[idx]);
-    }
-
-    glLinkProgram(program);
-
-    if (!check_and_log_program_link(program)) {
-        glDeleteProgram(program);
-
-        for (std::size_t idx = 0; idx < size; ++idx) {
-            glDeleteShader(shaders[idx]);
-        }
-
-        return 0;
-    }
-
-    for (std::size_t idx = 0; idx < size; ++idx) {
-        glDetachShader(program, shaders[idx]);
-        glDeleteShader(shaders[idx]);
-    }
-
-    return program;
-}
-
 
 glowl::GLSLProgram::ShaderSourceList::value_type make_glowl_shader_source(
     std::filesystem::path const& shader_source_path, megamol::shaderfactory::compiler_options const& options);
--- core/src/utility/ShaderFactory.cpp
+++ core/src/utility/ShaderFactory.cpp
@@ -12,25 +12,6 @@
 
 static megamol::shaderfactory::compiler msf_cp;
 
-
-GLuint megamol::core::utility::make_shader(
-    std::filesystem::path const& shader_source_path, megamol::shaderfactory::compiler_options const& options) {
-    auto shader_string = msf_cp.preprocess(shader_source_path, options);
-
-    auto shader = glCreateShader(megamol::shaderfactory::get_shader_type_ogl(shader_source_path));
-    auto const shader_cstr = shader_string.c_str();
-    GLint const shader_size = shader_string.size();
-    glShaderSource(shader, 1, &shader_cstr, nullptr);
-    glCompileShader(shader);
-    if (!check_and_log_shader_compile(shader)) {
-        glDeleteShader(shader);
-        return 0;
-    }
-
-    return shader;
-}
-
-
 glowl::GLSLProgram::ShaderSourceList::value_type megamol::core::utility::make_glowl_shader_source(
     std::filesystem::path const& shader_source_path, megamol::shaderfactory::compiler_options const& options) {
     auto const shader_string = msf_cp.preprocess(shader_source_path, options);
