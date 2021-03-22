41cb324cb01e9b74e45b6cc158a716d4200710b5
Sergej Geringer <sergej.geringer@visus.uni-stuttgart.de>
2021-03-22 12:26:07 +0100

    Screenshot Service: refactor default alpha value for screenshots

--- frontend/services/screenshot_service/Screenshot_Service.cpp
+++ frontend/services/screenshot_service/Screenshot_Service.cpp
@@ -43,6 +43,8 @@ static void log_warning(std::string const& text) {
 static megamol::core::MegaMolGraph* megamolgraph_ptr = nullptr;
 static megamol::frontend_resources::GUIState* guistate_resources_ptr = nullptr;
 
+static unsigned char default_alpha_value = 0;
+
 static void PNGAPI pngErrorFunc(png_structp pngPtr, png_const_charp msg) {
     log("PNG Error: " + std::string(msg));
 }
@@ -126,7 +128,7 @@ megamol::frontend_resources::ScreenshotImageData megamol::frontend_resources::Im
         auto r = [&]() { return byte_vector[i++]; };
         auto g = [&]() { return byte_vector[i++]; };
         auto b = [&]() { return byte_vector[i++]; };
-        auto a = [&]() { return (m_image->channels == ImageWrapper::DataChannels::RGBA8) ? byte_vector[i++] : 255; }; // alpha either from image or 1.0
+        auto a = [&]() { return (m_image->channels == ImageWrapper::DataChannels::RGBA8) ? byte_vector[i++] : default_alpha_value; }; // alpha either from image or 1.0
         ScreenshotImageData::Pixel pixel = { r(), g(), b(), a() };
         screenshot_image.image[j++] = pixel;
     }
@@ -178,7 +180,7 @@ megamol::frontend_resources::ScreenshotImageData megamol::frontend_resources::GL
     glReadPixels(0, 0, fbWidth, fbHeight, GL_RGBA, GL_UNSIGNED_BYTE, result.image.data());
 
     for (auto& pixel : result.image)
-        pixel.a = 255;
+        pixel.a = default_alpha_value;
 
     return std::move(result);
 }
