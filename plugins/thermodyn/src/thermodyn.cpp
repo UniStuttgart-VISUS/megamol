e51bacf66de2e9ef4e48170b99dac64230cd9180
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2021-03-24 13:48:21 +0100

    initial commit for timeplot adding ImPlot as external

--- plugins/thermodyn/src/thermodyn.cpp
+++ plugins/thermodyn/src/thermodyn.cpp
@@ -17,6 +17,7 @@
 #include "ParticleSurface.h"
 #include "VelocityDistribution.h"
 #include "rendering/BoxRenderer.h"
+#include "rendering/TimeLinePlot.h"
 
 #include "thermodyn/BoxDataCall.h"
 #include "thermodyn/CallStatsInfo.h"
@@ -51,6 +52,7 @@ public:
         this->module_descriptions.RegisterAutoDescription<megamol::thermodyn::ParticleSurface>();
         this->module_descriptions.RegisterAutoDescription<megamol::thermodyn::VelocityDistribution>();
         this->module_descriptions.RegisterAutoDescription<megamol::thermodyn::rendering::BoxRenderer>();
+        this->module_descriptions.RegisterAutoDescription<megamol::thermodyn::rendering::TimeLinePlot>();
         //
         // TODO: Register your plugin's modules here
         // like:
