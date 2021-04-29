ae3e93488342f48e8fc49e5fee04ec97c2f60b4a
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-04-29 17:17:40 +0200

    cmake plugin function

--- plugins/template/src/MegaMolPlugin.cpp
+++ plugins/template/src/MegaMolPlugin.cpp
@@ -1,62 +1,51 @@
-/*
- * MegaMolPlugin.cpp
- * Copyright (C) 2009-2015 by MegaMol Team
- * Alle Rechte vorbehalten.
+/**
+ * MegaMol
+ * Copyright (c) 2021, MegaMol Dev Team
+ * All rights reserved.
  */
 
-#include "stdafx.h"
-#include "MegaMolPlugin/MegaMolPlugin.h"
-
-#include "mmcore/api/MegaMolCore.std.h"
 #include "mmcore/utility/plugins/Plugin200Instance.h"
 #include "mmcore/utility/plugins/PluginRegister.h"
-#include "mmcore/versioninfo.h"
-#include "vislib/vislibversion.h"
 
 namespace megamol::MegaMolPlugin {
-    /** Implementing the instance class of this plugin */
-    class plugin_instance : public ::megamol::core::utility::plugins::Plugin200Instance {
-        REGISTERPLUGIN(plugin_instance)
-    public:
-        /** ctor */
-        plugin_instance(void)
+/** Implementing the instance class of this plugin */
+class plugin_instance : public ::megamol::core::utility::plugins::Plugin200Instance {
+    REGISTERPLUGIN(plugin_instance)
+public:
+    plugin_instance()
             : ::megamol::core::utility::plugins::Plugin200Instance(
 
-                /* machine-readable plugin assembly name */
-                "MegaMolPlugin", // TODO: Change this!
+                  // machine-readable plugin assembly name
+                  "MegaMolPlugin", // TODO: Change this!
 
-                /* human-readable plugin description */
-                "Describing MegaMolPlugin (TODO: Change this!)") {
+                  // human-readable plugin description
+                  "Describing MegaMolPlugin (TODO: Change this!)"){
 
-            // here we could perform addition initialization
-        };
-        /** Dtor */
-        virtual ~plugin_instance(void) {
-            // here we could perform addition de-initialization
-        }
-        /** Registers modules and calls */
-        virtual void registerClasses(void) {
+                  // here we could perform addition initialization
+              };
+    ~plugin_instance() override {
+        // here we could perform addition de-initialization
+    }
 
-            // register modules here:
+    // Registers modules and calls
+    void registerClasses() override {
 
-            //
-            // TODO: Register your plugin's modules here
-            // like:
-            //   this->module_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyModule1>();
-            //   this->module_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyModule2>();
-            //   ...
-            //
+        // register modules
 
-            // register calls here:
+        //
+        // TODO: Register your plugin's modules here:
+        // this->module_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyModule1>();
+        // this->module_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyModule2>();
+        // ...
+        //
 
-            //
-            // TODO: Register your plugin's calls here
-            // like:
-            //   this->call_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyCall1>();
-            //   this->call_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyCall2>();
-            //   ...
-            //
+        // register calls
 
-        }
-    };
+        // TODO: Register your plugin's calls here:
+        // this->call_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyCall1>();
+        // this->call_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyCall2>();
+        // ...
+        //
+    }
+};
 } // namespace megamol::MegaMolPlugin
