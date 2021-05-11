c6ec3c6061a68a1cf543384966a1a31129dfb9dc
Alexander Straub <alexander.straub@visus.uni-stuttgart.de>
2021-05-11 22:19:03 +0200

    Added STLDataSource (module name: MeshSTLDataSource) as derived class of STLDataSource from datatools to expose the loaded STL data to the new triangle mesh renderers

---
+++ plugins/mesh/src/STLDataSource.h
@@ -0,0 +1,72 @@
+/*
+ * STLDataSource.h
+ *
+ * Copyright (C) 2019 by Universitaet Stuttgart (VIS).
+ * Alle Rechte vorbehalten.
+ */
+#pragma once
+
+#include "mmcore/Call.h"
+
+#include "mmstd_datatools/STLDataSource.h"
+
+namespace megamol {
+namespace mesh {
+
+    class STLDataSource : public stdplugin::datatools::io::STLDataSource {
+    public:
+        /// <summary>
+        /// Answer the name of this module
+        /// </summary>
+        /// <returns>Module name</returns>
+        static const char* ClassName() {
+            return "MeshSTLDataSource";
+        }
+
+        /// <summary>
+        /// Answer a human-readable description
+        /// </summary>
+        /// <returns>Description</returns>
+        static const char* Description() {
+            return "Reader for triangle data in STL files";
+        }
+
+        /// <summary>
+        /// Answer the availability of this module
+        /// </summary>
+        /// <returns>Availability</returns>
+        static bool IsAvailable() {
+            return true;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        STLDataSource();
+
+        /// <summary>
+        /// Destructor
+        /// </summary>
+        ~STLDataSource();
+
+    protected:
+        /// <summary>
+        /// Callback function for requesting information
+        /// </summary>
+        /// <param name="caller">Call for this request</param>
+        /// <returns>True on success; false otherwise</returns>
+        virtual bool get_extent_callback(core::Call& caller) override;
+
+        /// <summary>
+        /// Callback function for requesting data
+        /// </summary>
+        /// <param name="caller">Call for this request</param>
+        /// <returns>True on success; false otherwise</returns>
+        virtual bool get_mesh_data_callback(core::Call& caller) override;
+
+        /// Output
+        core::CalleeSlot ngmesh_output_slot;
+    };
+
+} // namespace flowvis
+} // namespace megamol
