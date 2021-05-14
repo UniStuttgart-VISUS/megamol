e76525602ffd94631ea3ceccb7895f374def8ac9
Alexander Straub <alexander.straub@visus.uni-stuttgart.de>
2021-05-14 15:55:45 +0200

    Empty FlowVis plugin

---
+++ plugins/flowvis/src/flowvis.cpp
@@ -0,0 +1,48 @@
+/*
+ * flowvis.cpp
+ * Copyright (C) 2018-2019 by MegaMol Team
+ * Alle Rechte vorbehalten.
+ */
+
+#include "stdafx.h"
+
+#include "mmcore/api/MegaMolCore.std.h"
+#include "mmcore/utility/plugins/Plugin200Instance.h"
+#include "mmcore/utility/plugins/PluginRegister.h"
+#include "mmcore/versioninfo.h"
+#include "vislib/vislibversion.h"
+
+// INCLUDES GO HERE
+
+namespace megamol::flowvis {
+/** Implementing the instance class of this plugin */
+class plugin_instance : public ::megamol::core::utility::plugins::Plugin200Instance {
+    REGISTERPLUGIN(plugin_instance)
+public:
+    /** ctor */
+    plugin_instance(void)
+        : ::megamol::core::utility::plugins::Plugin200Instance(
+
+              /* machine-readable plugin assembly name */
+              "flowvis",
+
+              /* human-readable plugin description */
+              "Plugin for flow visualization."){
+
+              // here we could perform addition initialization
+          };
+    /** Dtor */
+    virtual ~plugin_instance(void) {
+        // here we could perform addition de-initialization
+    }
+    /** Registers modules and calls */
+    virtual void registerClasses(void) {
+
+        // register modules here:
+        
+
+        // register calls here:
+        
+    }
+};
+} // namespace megamol::flowvis
