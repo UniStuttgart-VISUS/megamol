bcbbfb701de64d78903176db6632f5005a8f1c7a
sellendk <dominik.sellenthin@web.de>
2021-05-24 09:21:00 +0200

    took out the mmvtkmRenderer module as it only produces linker error under ubuntu and is most probably not used anyway

--- plugins/mmvtkm/src/mmvtkmRenderer.cpp
+++ plugins/mmvtkm/src/mmvtkmRenderer.cpp
@@ -27,10 +27,10 @@ mmvtkmDataRenderer::mmvtkmDataRenderer(void)
     , psColorTables_("colorTable", "Colortables specify the color range of the data")
     , vtkmDataSet_()
     , vtkmMetaData_()
-    , scene_()
-    , mapper_()
-    , canvas_()
-    , vtkmCamera_()
+    // , scene_()
+    // , mapper_()
+    // , canvas_()
+    // , vtkmCamera_()
     , colorArray_(nullptr)
     , canvasWidth_(0.f)
     , canvasHeight_(0.f)
@@ -85,93 +85,93 @@ void mmvtkmDataRenderer::release() {
 bool mmvtkmDataRenderer::Render(core::view::CallRender3DGL& call) {
     mmvtkmDataCall* rhsVtkmDc = this->vtkmDataCallerSlot_.CallAs<mmvtkmDataCall>();
 
-    if (rhsVtkmDc == nullptr) {
-        core::utility::log::Log::DefaultLog.WriteError("In %s at line %d. rhsVtkmDc is nullptr.", __FILE__, __LINE__);
-        return false;
-    }
+    // if (rhsVtkmDc == nullptr) {
+    //     core::utility::log::Log::DefaultLog.WriteError("In %s at line %d. rhsVtkmDc is nullptr.", __FILE__, __LINE__);
+    //     return false;
+    // }
 
 
-    if (!(*rhsVtkmDc)(0)) { // getData
-        return false;
-    }
+    // if (!(*rhsVtkmDc)(0)) { // getData
+    //     return false;
+    // }
 
 
-    if (rhsVtkmDc->hasUpdate() || localUpdate_) {
-        vtkmDataSet_ = rhsVtkmDc->getData();
-        vtkmMetaData_ = rhsVtkmDc->getMetaData();
+    // if (rhsVtkmDc->hasUpdate() || localUpdate_) {
+    //     vtkmDataSet_ = rhsVtkmDc->getData();
+    //     vtkmMetaData_ = rhsVtkmDc->getMetaData();
         
-        vtkm::cont::ColorTable colorTable(psColorTables_.Param<core::param::EnumParam>()->ValueString().PeekBuffer());
-        // default cellname = "cells"
-        // default coordinatesystem name = "coordinates"
-        // default fieldname = "pointvar"
-        vtkm::rendering::Actor actor(vtkmDataSet_->data.GetCellSet(0), vtkmDataSet_->data.GetCoordinateSystem(0),
-            vtkmDataSet_->data.GetPointField("pointvar"),
-            colorTable); // depending on dataset change to getCellField with according FrameID
-        scene_ = vtkm::rendering::Scene();
-        scene_.AddActor(actor); // makescene(...)
+    //     vtkm::cont::ColorTable colorTable(psColorTables_.Param<core::param::EnumParam>()->ValueString().PeekBuffer());
+    //     // default cellname = "cells"
+    //     // default coordinatesystem name = "coordinates"
+    //     // default fieldname = "pointvar"
+    //     vtkm::rendering::Actor actor(vtkmDataSet_->data.GetCellSet(0), vtkmDataSet_->data.GetCoordinateSystem(0),
+    //         vtkmDataSet_->data.GetPointField("pointvar"),
+    //         colorTable); // depending on dataset change to getCellField with according FrameID
+    //     scene_ = vtkm::rendering::Scene();
+    //     scene_.AddActor(actor); // makescene(...)
 
-        vtkmCamera_.ResetToBounds(vtkmMetaData_.minMaxBounds);
+    //     vtkmCamera_.ResetToBounds(vtkmMetaData_.minMaxBounds);
 
-        // TOOD set fieldnames of vtk file as dropdown as parameter
-        localUpdate_ = false;
-    }
+    //     // TOOD set fieldnames of vtk file as dropdown as parameter
+    //     localUpdate_ = false;
+    // }
 
 
-    // Camera setup and complete snapshot generation
-    core::view::Camera_2 cam;
-    call.GetCamera(cam);
-    cam_type::snapshot_type snapshot;
-    cam_type::matrix_type viewTemp, projTemp;
-    cam.calc_matrices(snapshot, viewTemp, projTemp, core::thecam::snapshot_content::all);
-    glm::vec4 camView = snapshot.view_vector;
-    glm::vec4 camUp = snapshot.up_vector;
-    glm::vec4 camPos = snapshot.position;
+    // // Camera setup and complete snapshot generation
+    // core::view::Camera_2 cam;
+    // call.GetCamera(cam);
+    // cam_type::snapshot_type snapshot;
+    // cam_type::matrix_type viewTemp, projTemp;
+    // cam.calc_matrices(snapshot, viewTemp, projTemp, core::thecam::snapshot_content::all);
+    // glm::vec4 camView = snapshot.view_vector;
+    // glm::vec4 camUp = snapshot.up_vector;
+    // glm::vec4 camPos = snapshot.position;
 
 
     // camera settings for vtkm
-    canvasWidth_ = cam.resolution_gate().width();
-    canvasHeight_ = cam.resolution_gate().height();
-    canvas_ = vtkm::rendering::CanvasRayTracer(canvasWidth_, canvasHeight_);
-    vtkm::Vec<vtkm::Float32, 3> lookAt(camView.x, camView.y, camView.z);
-    vtkm::Vec<vtkm::Float32, 3> up(camUp.x, camUp.y, camUp.z);
-    vtkm::Vec<vtkm::Float32, 3> position(camPos.x, camPos.y, camPos.z);
-    vtkm::Float32 nearPlane = snapshot.frustum_near;
-    vtkm::Float32 farPlane = snapshot.frustum_far;
-    vtkm::Float32 fovY = cam.aperture_angle();
+    // canvasWidth_ = cam.resolution_gate().width();
+    // canvasHeight_ = cam.resolution_gate().height();
+    // canvas_ = vtkm::rendering::CanvasRayTracer(canvasWidth_, canvasHeight_);
+    // vtkm::Vec<vtkm::Float32, 3> lookAt(camView.x, camView.y, camView.z);
+    // vtkm::Vec<vtkm::Float32, 3> up(camUp.x, camUp.y, camUp.z);
+    // vtkm::Vec<vtkm::Float32, 3> position(camPos.x, camPos.y, camPos.z);
+    // vtkm::Float32 nearPlane = snapshot.frustum_near;
+    // vtkm::Float32 farPlane = snapshot.frustum_far;
+    // vtkm::Float32 fovY = cam.aperture_angle();
 
 
-    try {
-        // setup a camera and point it to towards the center of the input data
-        vtkmCamera_.SetLookAt(lookAt);
-        vtkmCamera_.SetViewUp(up);
-        vtkmCamera_.SetClippingRange(nearPlane, farPlane);
-        vtkmCamera_.SetFieldOfView(fovY);
-        vtkmCamera_.SetPosition(position);
+    // try {
+    //     // setup a camera and point it to towards the center of the input data
+    //     vtkmCamera_.SetLookAt(lookAt);
+    //     vtkmCamera_.SetViewUp(up);
+    //     vtkmCamera_.SetClippingRange(nearPlane, farPlane);
+    //     vtkmCamera_.SetFieldOfView(fovY);
+    //     vtkmCamera_.SetPosition(position);
 
 
-        // update actor, acutally just the field, each frame
-        // store dynamiccellset and coordinatesystem after reading them in GetExtents
-        vtkm::rendering::View3D view(scene_, mapper_, canvas_, vtkmCamera_);
-        // required
-        view.Initialize();
-        view.Paint();
-        // the canvas holds the buffer of the offscreen rendered image
-        vtkm::cont::ArrayHandle<vtkm::Vec<vtkm::Float32, 4>> colorBuffer = view.GetCanvas().GetColorBuffer();
+    //     // update actor, acutally just the field, each frame
+    //     // store dynamiccellset and coordinatesystem after reading them in GetExtents
+    //     vtkm::rendering::View3D view(scene_, mapper_, canvas_, vtkmCamera_);
+    //     // required
+    //     view.Initialize();
+    //     view.Paint();
+    //     // the canvas holds the buffer of the offscreen rendered image
+    //     vtkm::cont::ArrayHandle<vtkm::Vec<vtkm::Float32, 4>> colorBuffer = view.GetCanvas().GetColorBuffer();
 
 
-        // pulling out the c array from the buffer
-        // which can just be rendered
-        colorArray_ = colorBuffer.GetStorage().GetBasePointer();
+    //     // pulling out the c array from the buffer
+    //     // which can just be rendered
+    //     colorArray_ = colorBuffer.GetStorage().GetBasePointer();
 
-    } catch (const std::exception& e) {
-        core::utility::log::Log::DefaultLog.WriteError("In % s at line %d. \n", __FILE__, __LINE__);
-        core::utility::log::Log::DefaultLog.WriteError(e.what());
-        return false;
-    }
+    // } catch (const std::exception& e) {
+    //     core::utility::log::Log::DefaultLog.WriteError("In % s at line %d. \n", __FILE__, __LINE__);
+    //     core::utility::log::Log::DefaultLog.WriteError(e.what());
+    //     return false;
+    // }
 
 
-    // Write the C array to an OpenGL buffer
-    glDrawPixels((GLint)canvasWidth_, (GLint)canvasHeight_, GL_RGBA, GL_FLOAT, colorArray_);
+    // // Write the C array to an OpenGL buffer
+    // glDrawPixels((GLint)canvasWidth_, (GLint)canvasHeight_, GL_RGBA, GL_FLOAT, colorArray_);
 
     // SwapBuffers();
 
