dae886b5fa648cf863b0f04c5414646a3fb90489
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2021-06-30 13:48:35 +0200

    fixed depth compositing in ContextToGL

--- plugins/OSPRay_plugin/src/OSPRayToGL.h
+++ plugins/OSPRay_plugin/src/OSPRayToGL.h
@@ -11,64 +11,41 @@ inline constexpr char ospraytogl_desc[] = "Merges content to the input GL buffer
 inline constexpr auto ospray_to_gl_init_func = [](std::shared_ptr<vislib::graphics::gl::FramebufferObject>& lhs_fbo,
                                                    std::shared_ptr<core::view::CPUFramebuffer>& fbo, int width,
                                                    int height) -> void {
+    if (fbo != nullptr) {
+        glDeleteTextures(1, &fbo->data.col_tex);
+        glDeleteTextures(1, &fbo->data.depth_tex);
+    }
     fbo = std::make_shared<core::view::CPUFramebuffer>();
-};
 
-inline constexpr auto ospray_to_gl_ren_func = [](std::shared_ptr<vislib::graphics::gl::FramebufferObject>& lhs_fbo,
-                                                  std::shared_ptr<core::view::CPUFramebuffer>& fbo,
-                                                  core::view::RenderUtils& utils, int width,
-                                                  int height) -> void { // module own fbo
-    auto new_fbo = vislib::graphics::gl::FramebufferObject();
-    new_fbo.Create(width, height, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE,
-        vislib::graphics::gl::FramebufferObject::ATTACHMENT_TEXTURE, GL_DEPTH_COMPONENT);
-    new_fbo.Enable();
-
-    new_fbo.BindColourTexture();
-    glClear(GL_COLOR_BUFFER_BIT);
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, fbo->colorBuffer.data());
+    glGenTextures(1, (GLuint*) &fbo->data.col_tex);
+    glBindTexture(GL_TEXTURE_2D, fbo->data.col_tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, nullptr);
     glBindTexture(GL_TEXTURE_2D, 0);
 
-    if (fbo->depthBufferActive) {
-        new_fbo.BindDepthTexture();
-        glClear(GL_DEPTH_BUFFER_BIT);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT,
-            fbo->depthBuffer.data());
-        glBindTexture(GL_TEXTURE_2D, 0);
-    }
-    float right = (width + static_cast<float>(width)) / 2.0f;
-    float left = (width - static_cast<float>(width)) / 2.0f;
-    float bottom = (height + static_cast<float>(height)) / 2.0f;
-    float up = (height - static_cast<float>(height)) / 2.0f;
-    glm::vec3 pos_bottom_left = {left, bottom, 0.0f};
-    glm::vec3 pos_upper_left = {left, up, 0.0f};
-    glm::vec3 pos_upper_right = {right, up, 0.0f};
-    glm::vec3 pos_bottom_right = {right, bottom, 0.0f};
-    utils.Push2DColorTexture(
-        new_fbo.GetColourTextureID(), pos_bottom_left, pos_upper_left, pos_upper_right, pos_bottom_right, true);
-    if (fbo->depthBufferActive) {
-        utils.Push2DDepthTexture(
-            new_fbo.GetDepthTextureID(), pos_bottom_left, pos_upper_left, pos_upper_right, pos_bottom_right, true);
-    }
+    glGenTextures(1, (GLuint*) &fbo->data.depth_tex);
+    glBindTexture(GL_TEXTURE_2D, fbo->data.depth_tex);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, width, height, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
+    glBindTexture(GL_TEXTURE_2D, 0);
+};
 
-    new_fbo.Disable();
+inline constexpr auto ospray_to_gl_ren_func =
+    [](std::shared_ptr<glowl::GLSLProgram>& shader, std::shared_ptr<vislib::graphics::gl::FramebufferObject>& lhs_fbo,
+        std::shared_ptr<core::view::CPUFramebuffer>& fbo, int width, int height) -> void {
+    glBindTexture(GL_TEXTURE_2D, fbo->data.col_tex);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, fbo->colorBuffer.data());
+    glBindTexture(GL_TEXTURE_2D, fbo->data.depth_tex);
+    glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, width, height, GL_DEPTH_COMPONENT, GL_FLOAT, fbo->depthBuffer.data());
+    glBindTexture(GL_TEXTURE_2D, 0);
 
-    // draw into lhs fbo
-    if ((lhs_fbo->GetWidth() != width) || (lhs_fbo->GetHeight() != height)) {
-        lhs_fbo->Release();
-        lhs_fbo->Create(width, height, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE,
-            vislib::graphics::gl::FramebufferObject::ATTACHMENT_TEXTURE, GL_DEPTH_COMPONENT);
-    }
-    if (lhs_fbo->IsValid() && !lhs_fbo->IsEnabled()) {
-        lhs_fbo->Enable();
-    }
-
-    glm::mat4 ortho = glm::ortho(0.0f, static_cast<float>(width), 0.0f, static_cast<float>(height), -1.0f, 1.0f);
-
-    utils.DrawTextures(ortho, glm::vec2(width, height));
-
-    if (lhs_fbo->IsValid()) {
-        lhs_fbo->Disable();
-    }
+    core::view::renderToFBO(shader, lhs_fbo, fbo->data.col_tex, fbo->data.depth_tex, width, height);
 };
 
 using OSPRayToGL = core::view::ContextToGL<core::view::CallRender3D, ospray_to_gl_init_func, ospray_to_gl_ren_func,
--- plugins/optix_hpg/src/CUDAToGL.h
+++ plugins/optix_hpg/src/CUDAToGL.h
@@ -35,8 +35,8 @@ inline constexpr auto cuda_to_gl_init_func = [](std::shared_ptr<vislib::graphics
     glBindTexture(GL_TEXTURE_2D, fbo->data.col_tex);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, width, height, 0, GL_RGBA, GL_FLOAT, nullptr);
     glBindTexture(GL_TEXTURE_2D, 0);
 
@@ -59,8 +59,8 @@ inline constexpr auto cuda_to_gl_init_func = [](std::shared_ptr<vislib::graphics
     glBindTexture(GL_TEXTURE_2D, fbo->data.depth_tex);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, width, height, 0, GL_RED, GL_FLOAT, nullptr);
     glBindTexture(GL_TEXTURE_2D, 0);
 
@@ -69,7 +69,6 @@ inline constexpr auto cuda_to_gl_init_func = [](std::shared_ptr<vislib::graphics
 
     CUDA_CHECK_ERROR(cuGraphicsMapResources(1, &fbo->data.depth_tex_ref, fbo->data.exec_stream));
 
-    cuarr;
     CUDA_CHECK_ERROR(cuGraphicsSubResourceGetMappedArray(&cuarr, fbo->data.depth_tex_ref, 0, 0));
 
     surf_desc.resType = CU_RESOURCE_TYPE_ARRAY;
@@ -78,55 +77,20 @@ inline constexpr auto cuda_to_gl_init_func = [](std::shared_ptr<vislib::graphics
 
     CUDA_CHECK_ERROR(cuSurfObjectCreate(&fbo->depthBuffer, &surf_desc));
 
-    // if (lhs_fbo != nullptr) {
-    //    auto color_image = lhs_fbo->GetColourTextureID();
-    //    auto color_att_state = lhs_fbo->GetColorAttachmentState();
-    //    GLenum color_target = GL_TEXTURE_2D;
-    //    if (color_att_state.has_value()) {
-    //        if (color_att_state.value() ==
-    //            vislib::graphics::gl::FramebufferObject::AttachmentState::ATTACHMENT_RENDERBUFFER) {
-    //            color_target = GL_RENDERBUFFER;
-    //        }
-    //    }
-    //    CUDA_CHECK_ERROR(cuGraphicsGLRegisterImage(
-    //        &fbo->data.col_att_ref, color_image, color_target, CU_GRAPHICS_REGISTER_FLAGS_NONE));
-
-    //    /*auto depth_image = lhs_fbo->GetDepthTextureID();
-    //    auto depth_att_state = lhs_fbo->GetDepthAttachmentState();
-    //    GLenum depth_target = GL_TEXTURE_2D;
-    //    if (depth_att_state.has_value()) {
-    //        if (depth_att_state.value() ==
-    //            vislib::graphics::gl::FramebufferObject::AttachmentState::ATTACHMENT_RENDERBUFFER) {
-    //            depth_target = GL_RENDERBUFFER;
-    //        }
-    //    }
-    //    cuGraphicsGLRegisterImage(
-    //        &fbo->depthBuffer, depth_image, depth_target, CU_GRAPHICS_REGISTER_FLAGS_WRITE_DISCARD);*/
-    //}
-
     fbo->width = width;
     fbo->height = height;
 };
 
-inline constexpr auto cuda_to_gl_ren_func = [](std::shared_ptr<vislib::graphics::gl::FramebufferObject>& lhs_fbo,
-                                                std::shared_ptr<CUDAFramebuffer>& fbo, core::view::RenderUtils& utils,
-                                                int width, int height) -> void {
+inline constexpr auto cuda_to_gl_ren_func = [](std::shared_ptr<glowl::GLSLProgram>& shader,
+                                                std::shared_ptr<vislib::graphics::gl::FramebufferObject>& lhs_fbo,
+                                                std::shared_ptr<CUDAFramebuffer>& fbo, int width, int height) -> void {
     CUDA_CHECK_ERROR(cuGraphicsUnmapResources(1, &fbo->data.col_tex_ref, fbo->data.exec_stream));
-    float right = (width + static_cast<float>(width)) / 2.0f;
-    float left = (width - static_cast<float>(width)) / 2.0f;
-    float bottom = (height + static_cast<float>(height)) / 2.0f;
-    float up = (height - static_cast<float>(height)) / 2.0f;
-    glm::vec3 pos_bottom_left = {left, bottom, 0.0f};
-    glm::vec3 pos_upper_left = {left, up, 0.0f};
-    glm::vec3 pos_upper_right = {right, up, 0.0f};
-    glm::vec3 pos_bottom_right = {right, bottom, 0.0f};
-    utils.Push2DColorTexture(
-        fbo->data.col_tex, pos_bottom_left, pos_upper_left, pos_upper_right, pos_bottom_right, true);
-    utils.Push2DDepthTexture(
-        fbo->data.depth_tex, pos_bottom_left, pos_upper_left, pos_upper_right, pos_bottom_right, true);
-    glm::mat4 ortho = glm::ortho(0.0f, static_cast<float>(width), 0.0f, static_cast<float>(height), -1.0f, 1.0f);
-    utils.DrawTextures(ortho, glm::vec2(width, height));
+    CUDA_CHECK_ERROR(cuGraphicsUnmapResources(1, &fbo->data.depth_tex_ref, fbo->data.exec_stream));
+
+    core::view::renderToFBO(shader, lhs_fbo, fbo->data.col_tex, fbo->data.depth_tex, width, height);
+
     CUDA_CHECK_ERROR(cuGraphicsMapResources(1, &fbo->data.col_tex_ref, fbo->data.exec_stream));
+    CUDA_CHECK_ERROR(cuGraphicsMapResources(1, &fbo->data.depth_tex_ref, fbo->data.exec_stream));
 };
 
 using CUDAToGL =
