f46fa333ded42f8cee3f2ca9dd356f30863f7cdb
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2021-07-14 23:43:57 +0200

    clang format

--- plugins/gui/src/widgets/ParameterGroupViewCubeWidget.h
+++ plugins/gui/src/widgets/ParameterGroupViewCubeWidget.h
@@ -51,7 +51,7 @@ namespace gui {
         ~PickableTexture() = default;
 
         bool Draw(unsigned int picking_id, int selected_face_id, int& out_orientation_change, int& out_hovered_arrow_id,
-                  const megamol::core::utility::ManipVector_t& pending_manipulations);
+            const megamol::core::utility::ManipVector_t& pending_manipulations);
 
         megamol::core::utility::InteractVector_t GetInteractions(unsigned int id) const;
 
