71e5dd6b3ac79ec724f535209190ed986cf83665
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2021-07-22 19:22:29 +0200

    much easier this way actually

--- test/runtests.py
+++ test/runtests.py
@@ -4,7 +4,7 @@ import os.path
 import glob
 from PIL import Image
 import subprocess
-import time
+import re
 
 parser = argparse.ArgumentParser(usage="%(prog)s <DIRECTORY>", description="execute test scripts in DIRECTORY")
 parser.add_argument('directories', nargs="*")
@@ -13,6 +13,7 @@ parser.add_argument('--generate-neutral-test', action='count', help='generate a 
 args = parser.parse_args()
 
 resultname = 'result.png'
+istest = re.compile('.*\.test\.\d+\.lua')
 
 if not args.directories:
     print("need at least one input directory")
@@ -21,30 +22,26 @@ for dir in args.directories:
     for subdir, dirs, files in os.walk(dir, topdown=True):
         for file in files:
             entry = os.path.join(subdir, file)
-            if (entry.endswith(".lua")):
+            if (not istest.match(entry)):
                 #print(f'found {entry}')
                 testname, _ = os.path.splitext(entry)
                 #print(f'using {testname}')
                 if args.generate_neutral_test:
-                    tfname = f'{testname}.test.1'
+                    tfname = f'{testname}.test.1.lua'
                     if not os.path.isfile(tfname):
                         with open(tfname, "w") as outfile:
                             print(f'generating neutral test {tfname}')
-                            outfile.write('mmScreenshot("result.png")\n')
+                            outfile.write('mmRenderNextFrame()\nmmScreenshot("result.png")\nmmQuit()\n')
                     continue
 
-                tests = list(glob.iglob(f'{testname}.test.*'))
+                tests = list(glob.iglob(f'{testname}.test.*.lua'))
                 if len(tests) > 0:
                     print(f'found tests for {file}')
-                    with subprocess.Popen(f'megamol.exe {entry}') as proc:
-                        for testfile in tests:
-                            print(f"running test {testfile}")
-                            # TODO wait till up, cleverly
-                            time.sleep(5)
-                            refname = testfile + ".png"
-                            subprocess.run(f'remoteconsole.exe --open tcp://localhost:33333 --exec {testfile}')
-                            time.sleep(1)
-                            subprocess.run(f'remoteconsole.exe --open tcp://localhost:33333 --exec {testfile}')
+                    for testfile in tests:
+                        print(f"running test {testfile}")
+                        refname = testfile + ".png"
+                        with subprocess.Popen(f'megamol.exe {entry} {testfile}') as proc:
+                            proc.wait()
                             if args.generate_reference:
                                 try:
                                     os.rename(resultname, refname)
@@ -57,5 +54,3 @@ for dir in args.directories:
                                 except:
                                     print(f'could not open reference image {testname + ".png"}')
 
-                        print("closing MegaMol...")
-                        proc.kill()
