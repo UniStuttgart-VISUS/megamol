35ccaaccc14fffe5a42e74c790b7886067affebd
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2021-07-23 09:28:48 +0200

    added SSIM and some checks

--- test/runtests.py
+++ test/runtests.py
@@ -3,6 +3,7 @@ import os
 import os.path
 import glob
 from PIL import Image
+from SSIM_PIL import compare_ssim
 import subprocess
 import re
 
@@ -14,6 +15,13 @@ args = parser.parse_args()
 
 resultname = 'result.png'
 istest = re.compile(r'.*\.test\.\d+\.lua')
+testresults = []
+
+ssim_threshold = 0.95
+class TestResult:
+    testfile: str
+    passed: bool
+    result: str
 
 if not args.directories:
     print("need at least one input directory")
@@ -38,19 +46,56 @@ for dir in args.directories:
                 if len(tests) > 0:
                     print(f'found tests for {file}')
                     for testfile in tests:
-                        print(f"running test {testfile}")
+                        stdoutfile = f'{testfile}.stdout'
+                        stderrfile = f'{testfile}.stderr'
+                        if os.path.isfile(resultname):
+                            os.remove(resultname)
+                        if os.path.isfile(stdoutfile):
+                            os.remove(stdoutfile)
+                        # if os.path.isfile(stderrfile):
+                        #     os.remove(stderrfile)
+                        print(f"running test {testfile}... ", end='')
+                        tr = TestResult()
+                        tr.testfile=testfile
+                        tr.passed=True
                         refname = testfile + ".png"
-                        with subprocess.Popen(f'megamol.exe --nogui {entry} {testfile}') as proc:
-                            proc.wait()
-                            if args.generate_reference:
-                                try:
-                                    os.rename(resultname, refname)
-                                except:
-                                    print(f'could not move {resultname} to {refname}')
-                            else:
-                                try:
-                                    reference_image = Image.open(refname)
-                                    print(f'testing {entry}')
-                                except:
-                                    print(f'could not open reference image {testname + ".png"}')
+                        compl = subprocess.run(f'megamol.exe --nogui {entry} {testfile}', capture_output=True)
+                        if args.generate_reference:
+                            try:
+                                os.rename(resultname, refname)
+                            except:
+                                print(f'could not move {resultname} to {refname}')
+                        else:
+                            if not os.path.isfile(resultname):
+                                print(f'failed')
+                                tr.passed = False
+                                tr.result = "no output generated"
+                                testresults.append(tr)
+                                continue
+                            if not os.path.isfile(refname):
+                                print(f'failed')
+                                tr.passed = False
+                                tr.result = "missing reference image"
+                                testresults.append(tr)
+                                continue
+                            try:
+                                reference_image = Image.open(refname)
+                                result_image = Image.open(resultname)
+                                ssim = compare_ssim(reference_image, result_image, GPU=False)
+                                if ssim > ssim_threshold:
+                                    print(f'passed ({ssim})')
+                                else:
+                                    print(f'failed ({ssim})')
+                                    tr.passed = False
+                                    with open(stdoutfile, "wb") as outfile:
+                                        outfile.write(compl.stdout)
+                                    # with open(stderrfile, "wb") as outfile:
+                                    #     outfile.write(compl.stderr)
 
+                                tr.result = f'SSIM = {ssim}'
+                                testresults.append(tr)
+                            except Exception as e:
+                                print(f'unexpected exception: {e}')
+
+for tr in testresults:
+    print(f'{tr.testfile}: {"passed" if tr.passed else "failed"} {tr.result}')
