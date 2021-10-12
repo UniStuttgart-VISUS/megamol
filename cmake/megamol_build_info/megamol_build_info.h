bf1eeab1e183d834f28016c91951eb7bd627a47b
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-10-12 15:56:57 +0200

    typo

--- cmake/megamol_build_info/megamol_build_info.h
+++ cmake/megamol_build_info/megamol_build_info.h
@@ -20,7 +20,7 @@ namespace megamol::build_info {
     extern char const* const MEGAMOL_GIT_HASH;
     extern char const* const MEGAMOL_GIT_BRANCH_NAME;
     extern char const* const MEGAMOL_GIT_BRANCH_NAME_FULL;
-    extern char const* const MEGAMOL_GIT_ORIGIN_NAME;
+    extern char const* const MEGAMOL_GIT_REMOTE_NAME;
     extern char const* const MEGAMOL_GIT_REMOTE_URL;
     extern char const* const MEGAMOL_GIT_DIFF;
     extern int const MEGAMOL_GIT_IS_DIRTY;
