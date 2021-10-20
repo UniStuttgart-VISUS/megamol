66298f3083e08605b5359efa1ebc116244b621c8
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-10-20 20:04:17 +0200

    delete vislib

--- vislib/src/sys/CriticalSection.cpp
+++
@@ -1,9 +0,0 @@
-/*
- * CriticalSection.cpp
- *
- * Copyright (C) 2006 by Universitaet Stuttgart (VIS). Alle Rechte vorbehalten.
- * Copyright (C) 2005 by Christoph Mueller (christoph.mueller@vis.uni-stuttgart.de). Alle Rechte vorbehalten.
- * Copyright 2019 MegaMol Dev Team
- */
-
-#include "vislib/sys/CriticalSection.h"
--- vislib/src/sys/ReaderWriterMutexWrapper.cpp
+++
@@ -1,6 +0,0 @@
-/*
- * ReaderWriterMutexWrapper.h
- *
- * Copyright 2019 MegaMol Dev Team
- */
-#include "vislib/sys/ReaderWriterMutexWrapper.h"
