d59957c4d206d41e0217551cf0b90374d79453be
rauts <tobias.rau@visus.uni-stuttgart.de>
2021-10-28 18:34:30 +0200

    separated thermodyn

---
+++ plugins/thermodyn_gl/src/thermodyn_gl.cpp
@@ -0,0 +1,32 @@
+/**
+ * MegaMol
+ * Copyright (c) 2009-2021, MegaMol Dev Team
+ * All rights reserved.
+ */
+
+#include "mmcore/utility/plugins/AbstractPluginInstance.h"
+#include "mmcore/utility/plugins/PluginRegister.h"
+
+#include "rendering/BoxRenderer.h"
+
+namespace megamol::thermodyn {
+class ThermodynGLPluginInstance : public megamol::core::utility::plugins::AbstractPluginInstance {
+    REGISTERPLUGIN(ThermodynGLPluginInstance)
+
+public:
+    ThermodynGLPluginInstance()
+            : megamol::core::utility::plugins::AbstractPluginInstance("thermodyn_gl", "The thermodyn plugin."){};
+
+    ~ThermodynGLPluginInstance() override = default;
+
+    // Registers modules and calls
+    void registerClasses() override {
+
+        // register modules
+        this->module_descriptions.RegisterAutoDescription<megamol::thermodyn::rendering::BoxRenderer>();
+
+        // register calls
+
+    }
+};
+} // namespace megamol::thermodyn
