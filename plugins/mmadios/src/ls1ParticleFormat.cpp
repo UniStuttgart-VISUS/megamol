1a2568d1c51291d05ad9e02dfde4c1cfee3040ed
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2021-11-03 19:45:38 +0100

    check plugin structure in ci

--- plugins/mmadios/src/ls1ParticleFormat.cpp
+++ plugins/mmadios/src/ls1ParticleFormat.cpp
@@ -7,7 +7,7 @@
 
 #include "stdafx.h"
 #include "ls1ParticleFormat.h"
-#include "adios_plugin/CallADIOSData.h"
+#include "mmadios/CallADIOSData.h"
 #include "geometry_calls/MultiParticleDataCall.h"
 #include "mmcore/utility/log/Log.h"
 #include "mmcore/param/EnumParam.h"
