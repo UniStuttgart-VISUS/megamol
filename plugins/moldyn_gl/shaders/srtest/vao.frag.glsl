5a66f11d4ca5d8f13242534ffb45225a7cc63922
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2021-11-15 11:38:11 +0100

    different intersection test and cleanup

--- plugins/moldyn_gl/shaders/srtest/vao.frag.glsl
+++ plugins/moldyn_gl/shaders/srtest/vao.frag.glsl
@@ -4,6 +4,8 @@ flat in vec3 objPos;
 flat in float rad;
 flat in float sqrRad;
 flat in vec4 pointColor;
+flat in vec3 oc_pos;
+flat in float dot_oc_pos;
 
 #include "srtest_ubo.glsl"
 
@@ -13,35 +15,31 @@ layout(depth_greater) out float gl_FragDepth;
 #include "lightdirectional.glsl"
 
 void main(void) {
-    vec3 pos_w = gl_FragCoord.xyz;
-
-    vec3 pos_ndc = vec3(2.0 * (pos_w.xy / viewAttr.zw) - 1.0, (2.0 * pos_w.z) / (far - near) - 1.0);
-    vec4 pos_clip = (MVPinv * vec4(pos_ndc, 1.0));
+    vec4 pos_ndc = vec4(2.0f * (gl_FragCoord.xy / viewAttr.zw) - 1.0f, (2.0f * gl_FragCoord.z) / (far - near) - 1.0f, 1.0f);
+    vec4 pos_clip = MVPinv * pos_ndc;
     vec3 pos_obj = pos_clip.xyz / pos_clip.w;
 
     vec3 ray = normalize(pos_obj - camPos);
 
-    vec3 oc = camPos - objPos;
-    float b = dot(ray, oc);
-    float c = dot(oc, oc) - sqrRad;
+    float b = dot(-oc_pos, ray);
+    vec3 temp = oc_pos + b * ray;
+    float delta = sqrRad - dot(temp, temp);
 
-    float d = b * b - c;
-
-    if (d >= 0.0) {
-        outColor = pointColor;
-        float ta = -b - sqrt(d);
-        float tb = -b + sqrt(d);
-        float t = min(ta, tb);
-        vec4 new_pos = vec4(camPos + t * ray, 1.0);
-
-        vec3 normal = (new_pos.xyz - objPos) / rad;
-        outColor = vec4(LocalLighting(ray, normal, lightDir, outColor.rgb), outColor.a);
-
-        float depth = dot(MVPtransp[2], new_pos);
-        float depthW = dot(MVPtransp[3], new_pos);
-        gl_FragDepth = ((depth / depthW) + 1.0) * 0.5;
-    } else {
-        // outColor = vec4(1, 0, 0, 1);
+    if (delta < 0.0f)
         discard;
-    }
+
+    float c = dot_oc_pos - sqrRad;
+    float sign = b >= 0.0f ? 1.0f : -1.0f;
+    float q = b + sign * sqrt(delta);
+
+    float t = min(c / q, q);
+
+    vec4 new_pos = vec4(camPos + t * ray, 1.0f);
+
+    vec3 normal = (new_pos.xyz - objPos) / rad;
+    outColor = vec4(LocalLighting(ray, normal, lightDir, pointColor.rgb), pointColor.a);
+
+    float depth = dot(MVPtransp[2], new_pos);
+    float depthW = dot(MVPtransp[3], new_pos);
+    gl_FragDepth = ((depth / depthW) + 1.0f) * 0.5f;
 }
--- plugins/moldyn_gl/shaders/srtest/vao.vert.glsl
+++ plugins/moldyn_gl/shaders/srtest/vao.vert.glsl
@@ -10,6 +10,8 @@ flat out vec3 objPos;
 flat out float rad;
 flat out float sqrRad;
 flat out vec4 pointColor;
+flat out vec3 oc_pos;
+flat out float dot_oc_pos;
 
 #include "srtest_ubo.glsl"
 
@@ -31,6 +33,9 @@ void main(void) {
 
     objPos = inPosition.xyz;
 
+    oc_pos = camPos - objPos;
+    dot_oc_pos = dot(oc_pos, oc_pos);
+
     vec2 mins, maxs;
 
     vec3 di = objPos - camPos;
@@ -40,15 +45,15 @@ void main(void) {
 
     float s = (sqrRad) / (dd);
 
-    float v = rad / sqrt(1 - s);
+    float v = rad / sqrt(1.0f - s);
 
     vec3 vr = normalize(cross(di, camUp)) * v;
     vec3 vu = normalize(cross(di, vr)) * v;
 
-    vec4 v1 = MVP * vec4(objPos + vr, 1.0);
-    vec4 v2 = MVP * vec4(objPos - vr, 1.0);
-    vec4 v3 = MVP * vec4(objPos + vu, 1.0);
-    vec4 v4 = MVP * vec4(objPos - vu, 1.0);
+    vec4 v1 = MVP * vec4(objPos + vr, 1.0f);
+    vec4 v2 = MVP * vec4(objPos - vr, 1.0f);
+    vec4 v3 = MVP * vec4(objPos + vu, 1.0f);
+    vec4 v4 = MVP * vec4(objPos - vu, 1.0f);
 
     v1 /= v1.w;
     v2 /= v2.w;
@@ -64,18 +69,18 @@ void main(void) {
     mins = min(mins, v4.xy);
     maxs = max(maxs, v4.xy);
 
-    v1.xy = 0.5 * v1.xy * viewAttr.zw + 0.5 * viewAttr.zw;
-    v2.xy = 0.5 * v2.xy * viewAttr.zw + 0.5 * viewAttr.zw;
-    v3.xy = 0.5 * v3.xy * viewAttr.zw + 0.5 * viewAttr.zw;
-    v4.xy = 0.5 * v4.xy * viewAttr.zw + 0.5 * viewAttr.zw;
+    v1.xy = 0.5f * v1.xy * viewAttr.zw + 0.5f * viewAttr.zw;
+    v2.xy = 0.5f * v2.xy * viewAttr.zw + 0.5f * viewAttr.zw;
+    v3.xy = 0.5f * v3.xy * viewAttr.zw + 0.5f * viewAttr.zw;
+    v4.xy = 0.5f * v4.xy * viewAttr.zw + 0.5f * viewAttr.zw;
 
     vec2 vw = (v1 - v2).xy;
     vec2 vh = (v3 - v4).xy;
 
-    vec4 projPos = MVP * vec4(objPos + rad * (camDir), 1.0);
+    vec4 projPos = MVP * vec4(objPos + rad * (camDir), 1.0f);
     projPos = projPos / projPos.w;
 
     gl_PointSize = max(length(vw), length(vh));
 
-    gl_Position = vec4((mins + maxs) * 0.5, projPos.z, 1.0);
+    gl_Position = vec4((mins + maxs) * 0.5f, projPos.z, 1.0f);
 }
