affaa24ac0b3edd9b413a090d2d6e47c4d8c06a5
Karsten Schatz <karsten.schatz@visus.uni-stuttgart.de>
2021-12-01 16:54:28 +0100

    clang format on ballifier

--- plugins/protein/src/MoleculeBallifier.cpp
+++ plugins/protein/src/MoleculeBallifier.cpp
@@ -5,10 +5,10 @@
  * All rights reserved.
  */
 
-#include "stdafx.h"
 #include "MoleculeBallifier.h"
-#include "protein_calls/MolecularDataCall.h"
 #include "geometry_calls/MultiParticleDataCall.h"
+#include "protein_calls/MolecularDataCall.h"
+#include "stdafx.h"
 
 
 using namespace megamol;
@@ -16,13 +16,16 @@ using namespace megamol::protein;
 using namespace megamol::geocalls;
 using namespace megamol::protein_calls;
 
-/*
- * 
- */
-MoleculeBallifier::MoleculeBallifier(void) : core::Module(), 
-        outDataSlot("outData", "Sends MultiParticleDataCall data out into the world"),
-        inDataSlot("inData", "Fetches MolecularDataCall data"),
-        inHash(0), outHash(0), data(), colMin(0.0f), colMax(1.0f), frameOld(-1) {
+MoleculeBallifier::MoleculeBallifier(void)
+        : core::Module()
+        , outDataSlot("outData", "Sends MultiParticleDataCall data out into the world")
+        , inDataSlot("inData", "Fetches MolecularDataCall data")
+        , inHash(0)
+        , outHash(0)
+        , data()
+        , colMin(0.0f)
+        , colMax(1.0f)
+        , frameOld(-1) {
 
     this->inDataSlot.SetCompatibleCall<MolecularDataCallDescription>();
     this->MakeSlotAvailable(&this->inDataSlot);
@@ -32,65 +35,50 @@ MoleculeBallifier::MoleculeBallifier(void) : core::Module(),
     this->outDataSlot.SetCallback(geocalls::MultiParticleDataCall::ClassName(),
         geocalls::MultiParticleDataCall::FunctionName(1), &MoleculeBallifier::getExt);
     this->MakeSlotAvailable(&this->outDataSlot);
-
 }
 
-
-/*
- * 
- */
 MoleculeBallifier::~MoleculeBallifier(void) {
     this->Release();
 }
 
-
-/*
- * 
- */
 bool MoleculeBallifier::create(void) {
     // intentionally empty
     return true;
 }
 
-
-/*
- * 
- */
 void MoleculeBallifier::release(void) {
     // intentionally empty
 }
 
-
-/*
- * 
- */
 bool MoleculeBallifier::getData(core::Call& c) {
     using geocalls::MultiParticleDataCall;
-    MultiParticleDataCall *ic = dynamic_cast<MultiParticleDataCall*>(&c);
-    if (ic == NULL) return false;
+    MultiParticleDataCall* ic = dynamic_cast<MultiParticleDataCall*>(&c);
+    if (ic == NULL)
+        return false;
 
-    MolecularDataCall *oc = this->inDataSlot.CallAs<MolecularDataCall>();
-    if (oc == NULL) return false;
+    MolecularDataCall* oc = this->inDataSlot.CallAs<MolecularDataCall>();
+    if (oc == NULL)
+        return false;
 
     // Transfer frame ID plus force flag
     oc->SetFrameID(ic->FrameID(), ic->IsFrameForced());
 
     if ((*oc)(0)) {
         // Rewrite data if the frame number OR the datahash has changed
-        if ((this->inHash != oc->DataHash())||(this->frameOld = static_cast<int>(oc->FrameID()))) {
+        if ((this->inHash != oc->DataHash()) || (this->frameOld = static_cast<int>(oc->FrameID()))) {
             this->inHash = oc->DataHash();
             this->frameOld = static_cast<int>(oc->FrameID());
             this->outHash++;
 
-/*
-da kannst Du die atom-position und den B-Faktor auslesen
-AtomCount, AtomPositions, AtomBFactors
-ueber den Atom-Type kannst Du auf das Type-Array zugreifen, das den Radius drin hat
-*/
+            /*
+            da kannst Du die atom-position und den B-Faktor auslesen
+            AtomCount, AtomPositions, AtomBFactors
+            ueber den Atom-Type kannst Du auf das Type-Array zugreifen, das den Radius drin hat
+            */
 
             unsigned int cnt = oc->AtomCount();
             this->data.AssertSize(sizeof(float) * 5 * cnt);
-            float *fData = this->data.As<float>();
+            float* fData = this->data.As<float>();
 
             for (unsigned int i = 0; i < cnt; i++, fData += 5) {
                 fData[0] = oc->AtomPositions()[i * 3 + 0];
@@ -100,10 +88,11 @@ ueber den Atom-Type kannst Du auf das Type-Array zugreifen, das den Radius drin 
                 fData[3] = oc->AtomTypes()[oc->AtomTypeIndices()[i]].Radius();
 
                 fData[4] = oc->AtomBFactors()[i];
-                if ((i == 0) || (this->colMin > fData[4])) this->colMin = fData[4];
-                if ((i == 0) || (this->colMax < fData[4])) this->colMax = fData[4];
+                if ((i == 0) || (this->colMin > fData[4]))
+                    this->colMin = fData[4];
+                if ((i == 0) || (this->colMax < fData[4]))
+                    this->colMax = fData[4];
             }
-
         }
 
         ic->SetDataHash(this->outHash);
@@ -112,26 +101,24 @@ ueber den Atom-Type kannst Du auf das Type-Array zugreifen, das den Radius drin 
         p.SetCount(this->data.GetSize() / (sizeof(float) * 5));
         if (p.GetCount() > 0) {
             p.SetVertexData(MultiParticleDataCall::Particles::VERTDATA_FLOAT_XYZR, this->data.At(0), sizeof(float) * 5);
-            p.SetColourData(MultiParticleDataCall::Particles::COLDATA_FLOAT_I, this->data.At(sizeof(float) * 4), sizeof(float) * 5);
+            p.SetColourData(
+                MultiParticleDataCall::Particles::COLDATA_FLOAT_I, this->data.At(sizeof(float) * 4), sizeof(float) * 5);
             p.SetColourMapIndexValues(this->colMin, this->colMax);
         }
-
     }
 
     return true;
 }
 
-
-/*
- * 
- */
 bool MoleculeBallifier::getExt(core::Call& c) {
     using geocalls::MultiParticleDataCall;
-    MultiParticleDataCall *ic = dynamic_cast<MultiParticleDataCall*>(&c);
-    if (ic == NULL) return false;
+    MultiParticleDataCall* ic = dynamic_cast<MultiParticleDataCall*>(&c);
+    if (ic == NULL)
+        return false;
 
-    MolecularDataCall *oc = this->inDataSlot.CallAs<MolecularDataCall>();
-    if (oc == NULL) return false;
+    MolecularDataCall* oc = this->inDataSlot.CallAs<MolecularDataCall>();
+    if (oc == NULL)
+        return false;
 
     if ((*oc)(1)) {
         ic->SetFrameCount(oc->FrameCount());
