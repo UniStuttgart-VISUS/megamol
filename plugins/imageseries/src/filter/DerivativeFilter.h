60574a28690ff88717f0f21fda78747c16be0d9f
Marukyu <434348-Marukyu@users.noreply.gitlab.com>
2022-02-28 12:36:55 +0100

    Add image derivative filter

---
+++ plugins/imageseries/src/filter/DerivativeFilter.h
@@ -0,0 +1,32 @@
+#ifndef SRC_IMAGESERIES_FILTER_DERIVATIVEFILTER_HPP_
+#define SRC_IMAGESERIES_FILTER_DERIVATIVEFILTER_HPP_
+
+#include "imageseries/AsyncImageData2D.h"
+
+#include <memory>
+
+namespace megamol::ImageSeries::filter {
+
+class DerivativeFilter {
+public:
+    using AsyncImagePtr = std::shared_ptr<const AsyncImageData2D>;
+    using ImagePtr = std::shared_ptr<const AsyncImageData2D::BitmapImage>;
+
+    struct Input {
+        AsyncImagePtr image;
+    };
+
+    DerivativeFilter(Input input);
+    DerivativeFilter(AsyncImagePtr image);
+    ImagePtr operator()();
+
+    std::size_t getByteSize() const;
+
+private:
+    Input input;
+};
+
+} // namespace megamol::ImageSeries::filter
+
+
+#endif
