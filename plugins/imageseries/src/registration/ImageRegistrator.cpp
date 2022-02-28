20aa437e19f12dfa7ca78e18f705340e5298b516
Marukyu <434348-Marukyu@users.noreply.gitlab.com>
2022-02-28 12:39:57 +0100

    Add 'Gradient Descent'-based image registrator

---
+++ plugins/imageseries/src/registration/ImageRegistrator.cpp
@@ -0,0 +1,104 @@
+#include "ImageRegistrator.h"
+
+#include "../filter/AsyncFilterRunner.h"
+#include "../filter/Convolution2DFilter.h"
+#include "../filter/DerivativeFilter.h"
+#include "../util/ImageUtils.h"
+
+namespace megamol::ImageSeries::registration {
+
+ImageRegistrator::ImageRegistrator() : transform(1, 0, 0, 1, 0, 0) {}
+
+void ImageRegistrator::setInputImage(AsyncImagePtr image) {
+    if (this->inputImage != image) {
+        this->inputImage = image;
+
+        // Blue input image
+        filter::Convolution2DFilter::Input blurInput;
+        blurInput.image = this->inputImage;
+        blurInput.kernelX = filter::Convolution2DFilter::makeGaussianKernel(3, 5);
+        blurInput.kernelY = blurInput.kernelX;
+        auto blurredImage = std::make_shared<AsyncImageData2D>(filter::Convolution2DFilter(blurInput)());
+
+        this->inputDerivative = filter::DerivativeFilter(blurredImage)();
+    }
+}
+
+ImageRegistrator::AsyncImagePtr ImageRegistrator::getInputImage() const {
+    return inputImage;
+}
+
+void ImageRegistrator::setReferenceImage(AsyncImagePtr image) {
+    this->referenceImage = image;
+}
+
+ImageRegistrator::AsyncImagePtr ImageRegistrator::getReferenceImage() const {
+    return referenceImage;
+}
+
+void ImageRegistrator::setTransform(glm::mat3x2 transform) {
+    this->transform = transform;
+}
+
+const glm::mat3x2& ImageRegistrator::getTransform() const {
+    return transform;
+}
+
+void ImageRegistrator::setConvergenceRate(float rate) {
+    this->convergenceRate = rate;
+}
+
+float ImageRegistrator::getConvergenceRate() const {
+    return convergenceRate;
+}
+
+float ImageRegistrator::getMeanSquareError() const {
+    return meanSquareError;
+}
+
+void ImageRegistrator::step() {
+    auto referenceData = referenceImage ? referenceImage->getImageData() : nullptr;
+    auto inputData = inputImage ? inputImage->getImageData() : nullptr;
+
+    if (referenceData == nullptr || inputData == nullptr || inputDerivative == nullptr) {
+        return;
+    }
+
+    util::ImageSampler<uint8_t, 1> reference(*referenceData);
+    util::ImageSampler<uint8_t, 1> input(*inputData);
+    util::ImageSampler<uint8_t, 2> derivative(*inputDerivative);
+
+    glm::mat3x2 sum(0.f);
+    std::size_t sampleCount = 0;
+    double squareErrorSum = 0;
+
+    for (std::size_t y = 0; y < reference.getHeight(); ++y) {
+        for (std::size_t x = 0; x < reference.getWidth(); ++x) {
+            auto point = transform * glm::vec3(x, y, 1.f);
+
+            if (point.x < 0 || point.y < 0 || point.x > reference.getWidth() - 1 ||
+                point.y > reference.getHeight() - 1) {
+                continue;
+            }
+
+            auto referenceSample = reference.get(x, y) / 255.f;
+            auto inputSample = input.lerp(point) / 255.f;
+            auto derivativeSample = (derivative.lerp(point) - glm::vec2(127.f, 127.f)) / 255.f;
+
+            auto difference = referenceSample - inputSample;
+            squareErrorSum += difference.x * difference.x;
+
+            auto offset = difference * 2.f * (transform * glm::vec3(derivativeSample, 0.f));
+            sum += glm::mat3x2(x * offset.x, x * offset.y, y * offset.x, y * offset.y, offset.x, offset.y);
+            sampleCount++;
+        }
+    }
+
+    if (sampleCount != 0) {
+        transform += (convergenceRate / sampleCount) * sum;
+        meanSquareError = squareErrorSum / sampleCount;
+    }
+}
+
+
+} // namespace megamol::ImageSeries::registration
---
+++ plugins/imageseries/src/registration/ImageRegistrator.h
@@ -0,0 +1,50 @@
+#ifndef SRC_IMAGESERIES_REGISTRATION_IMAGEREGISTRATOR_HPP_
+#define SRC_IMAGESERIES_REGISTRATION_IMAGEREGISTRATOR_HPP_
+
+#include "imageseries/AsyncImageData2D.h"
+
+#include "glm/mat3x2.hpp"
+
+#include <memory>
+
+namespace megamol::ImageSeries::registration {
+
+class ImageRegistrator {
+public:
+    using AsyncImagePtr = std::shared_ptr<const AsyncImageData2D>;
+    using ImagePtr = std::shared_ptr<const AsyncImageData2D::BitmapImage>;
+
+    ImageRegistrator();
+
+    ImageRegistrator(ImageRegistrator& registrator) = default;
+    ImageRegistrator& operator=(ImageRegistrator& registrator) = default;
+
+    void setInputImage(AsyncImagePtr image);
+    AsyncImagePtr getInputImage() const;
+
+    void setReferenceImage(AsyncImagePtr image);
+    AsyncImagePtr getReferenceImage() const;
+
+    void setTransform(glm::mat3x2 transform);
+    const glm::mat3x2& getTransform() const;
+
+    void setConvergenceRate(float rate);
+    float getConvergenceRate() const;
+
+    float getMeanSquareError() const;
+
+    void step();
+
+private:
+    AsyncImagePtr inputImage;
+    ImagePtr inputDerivative;
+    AsyncImagePtr referenceImage;
+    glm::mat3x2 transform;
+    float convergenceRate = 0.02f;
+    float meanSquareError = 0.f;
+};
+
+} // namespace megamol::ImageSeries::registration
+
+
+#endif
