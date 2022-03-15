4c2a2df478c7b2bdfd3dafbbeb9f7d6eccd2d255
schatzkn <fliegendesv@t-online.de>
2022-03-15 12:39:10 +0100

    added new protein color class to replace the others. Moved utilities to the helpers file.

--- plugins/protein_gl/src/MSMSCavityFinder.h
+++ plugins/protein_gl/src/MSMSCavityFinder.h
@@ -9,7 +9,7 @@
 #pragma once
 #endif /* (defined(_MSC_VER) && (_MSC_VER > 1000)) */
 
-#include "protein/ProteinHelpers.h"
+#include "protein_calls/ProteinHelpers.h"
 
 #include "mmcore/Call.h"
 #include "mmcore/CalleeSlot.h"
