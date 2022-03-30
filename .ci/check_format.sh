96c7f466b89bec9b0be470dc95c05ea4be7630ed
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2022-03-30 16:39:53 +0200

    fix format check

--- .ci/check_format.sh
+++ .ci/check_format.sh
@@ -54,13 +54,15 @@ while read -r file; do
     if [[ $1 == "fix" ]]; then
       clang-format-12 -i "$file"
     else
-      output="$(clang-format-12 --dry-run --Werror "$file" 2>&1)"
-      if [[ $? -ne 0 ]]; then
+      # Workaround "set -e" and store exit code
+      format_exit_code=0
+      output="$(clang-format-12 --dry-run --Werror "$file" 2>&1)" || format_exit_code=$?
+      if [[ $format_exit_code -ne 0 ]]; then
         EXIT_CODE=1
         echo "::error::ClangFormat found issues in: $file"
         #echo "$output"
         # Show detailed diff. Requires ClangFormat to run again, but should mostly affect only a few files.
-        clang-format-12 "$file" | diff --color=always -u "$file" -
+        clang-format-12 "$file" | diff --color=always -u "$file" - || true
       fi
     fi
   fi
