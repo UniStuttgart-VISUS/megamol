7c499584d0aa8d1705ffa22fc18e789e73eaafe5
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2022-04-04 20:00:48 +0200

    optimize

--- plugins/infovis_gl/shaders/infovis_gl/pc/common/quad_vertices.inc.glsl
+++ plugins/infovis_gl/shaders/infovis_gl/pc/common/quad_vertices.inc.glsl
@@ -1,19 +1,17 @@
 /**
  * Returns position of a vertex based on gl_VertexID assuming the following draw call:
  * glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+ *
+ * gl_VertexID:
  *   2---3
  *   | \ |
  *   0---1
+ *
+ * Result:
+ *   (0,1)---(1,1)
+ *     |   \   |
+ *   (0,0)---(1,0)
  */
 vec2 quadVertexPosition() {
-    if (gl_VertexID == 0) {
-        return vec2(0.0f, 0.0f);
-    } else if (gl_VertexID == 1) {
-        return vec2(1.0f, 0.0f);
-    } else if (gl_VertexID == 2) {
-        return vec2(0.0f, 1.0f);
-    } else if (gl_VertexID == 3) {
-        return vec2(1.0f, 1.0f);
-    }
-    return vec2(0.0f, 0.0f);
+    return vec2(float(gl_VertexID % 2), float((gl_VertexID % 4) / 2));
 }
