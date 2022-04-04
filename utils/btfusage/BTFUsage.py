a9966985366e7b34ab61ea3dc1a560ae5cc0bdc2
schatzkn <fliegendesv@t-online.de>
2022-04-04 21:16:49 +0200

    first prototype for shader usage script

--- utils/btfusage/BTFUsage.py
+++ utils/btfusage/BTFUsage.py
@@ -10,7 +10,10 @@ def dir_path(string):
         raise NotADirectoryError(string)
 
 def parseArguments():
-    parser = argparse.ArgumentParser()
+    descr = """This script creates a human-readable report on the usage of shaders residing in 
+    BTF-Files. It only needs the path to to the plugin as input. It should also work with the
+    core itself. The report is printed to the console and should ideally be piped into a file."""
+    parser = argparse.ArgumentParser(description=descr)
     parser.add_argument("path", type=dir_path, help="Path to the plugin folder")
     args = parser.parse_args()
     return args.path
@@ -52,15 +55,58 @@ def listBtfFiles(path):
     return result
 
 def parsebtfs(btflist):
+    res = []
     for file in btflist:
-        res = btfparser.parseBTF(file)
-        break # intentional right now for testing
+        res = res + btfparser.parseBTF(file)
+    return res
+
+def parsesources(sourcelist):
+    res = []
+    for file in sourcelist:
+        res.append(cppparser.parseCPP(file))
+    #create dictionary that maps shader names to files
+    dictionary = {}
+    for e in res:
+        if len(e) > 0:
+            for sname in e[1]: #access all shader names
+                if sname in dictionary:
+                    dictionary[sname] = dictionary[sname] + e[0]
+                else:
+                    dictionary[sname] = [e[0]]
+    return dictionary
+
+def createReport(path, btfresult, srcresult):
+    divisor = "------------------------------------------------------------------"
+    divisorsmall = "-----"
+    print("MegaMol BTF Shader Usage Report for \"" + path + "\":")
+    name = ""
+    for shader in btfresult:
+        if name != shader[0]: # new file
+            print(divisor)
+            name = shader[0]
+            print("SHADER FILE = \"" + name + "\"")
+            print(divisorsmall)
+        print("NAME = \"" + shader[1] + "\", LINES: " + str(shader[2]) + " to " + str(shader[3]))
+        print("    used in")
+        dictres = []
+        if shader[1] in srcresult:
+            dictres = srcresult[shader[1]]
+        if len(dictres) == 0:
+            print("        NOWHERE")
+        else:
+            for e in dictres:
+                print("        " + e)
+
+    print(divisor)
+    print("WARNING: There is NO GUARANTEE that alle usages are listed, as shader names can be constructed programmatically!")
 
 def main():
     path = parseArguments()
     btflist = listBtfFiles(path)
     srclist = listSourceFiles(path)
-    parsebtfs(btflist)
+    btfres = parsebtfs(btflist)
+    srcres = parsesources(srclist)
+    createReport(path, btfres, srcres)
 
 if __name__ == "__main__":
     main()
