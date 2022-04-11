bd2fdb36302ab6cc601379b86aefdf47ad6193f9
Moritz Heinemann <mo@blog-srv.net>
2022-04-12 01:22:20 +0200

    use common shader quad util

--- plugins/infovis_gl/shaders/infovis_gl/amort/amort_quad.vert.glsl
+++ plugins/infovis_gl/shaders/infovis_gl/amort/amort_quad.vert.glsl
@@ -1,16 +1,12 @@
 #version 450
 
+#include "../common/quad_vertices.inc.glsl"
+
 out vec2 uvCoords;
 
 void main() {
-    const vec2 coords[4] = vec2[4](
-        vec2(0.0f, 0.0f),
-        vec2(1.0f, 0.0f),
-        vec2(0.0f, 1.0f),
-        vec2(1.0f, 1.0f));
+    vec2 coord = quadVertexPosition();
 
-    const vec2 coord = coords[gl_VertexID];
-
-    gl_Position =  vec4(2.0f * coord - 1.0f, 0.0f, 1.0f);
+    gl_Position = vec4(2.0f * coord - 1.0f, 0.0f, 1.0f);
     uvCoords = coord;
 }
