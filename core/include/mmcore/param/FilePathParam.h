38aa5c30b9e23751585b877a4d70223dbe136e80
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2022-04-11 15:11:27 +0200

    fix for filepathparam any_flag

--- core/include/mmcore/param/FilePathParam.h
+++ core/include/mmcore/param/FilePathParam.h
@@ -29,13 +29,12 @@ namespace param {
 class MEGAMOLCORE_API FilePathParam : public AbstractParam {
 public:
     enum FilePathFlags_ : uint32_t {
-        Flag_Any = 1 << 0,
-        Flag_File = 1 << 1,
-        Flag_Directory = 1 << 2,
-        Flag_NoExistenceCheck = 1 << 3,
-        Flag_RestrictExtension = 1 << 4,
+        Flag_File = 1 << 0,
+        Flag_Directory = 1 << 1,
+        Flag_NoExistenceCheck = 1 << 2,
+        Flag_RestrictExtension = 1 << 3,
         /// Convenience flags:
-        Flag_Any_RestrictExtension = Flag_Any | Flag_RestrictExtension,
+        Flag_Any = Flag_File | Flag_Directory,
         Flag_Any_ToBeCreated = Flag_Any | Flag_NoExistenceCheck,
         Flag_File_RestrictExtension = Flag_File | Flag_RestrictExtension,
         Flag_File_ToBeCreated = Flag_File | Flag_NoExistenceCheck,
