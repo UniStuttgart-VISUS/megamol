7f1db3b00d7d1960fe8f4619ff2cf998d6d5912f
Alexander Straub <alexander.straub@visus.uni-stuttgart.de>
2022-05-24 14:26:56 +0200

    Better template type naming, const return to prevent modification of copy, "disable" pointer access

--- core/include/mmcore/utility/ConvertingIterator.h
+++ core/include/mmcore/utility/ConvertingIterator.h
@@ -5,104 +5,109 @@
 namespace megamol::core::utility {
 
 // Alex' tactical nuke, i.e., iterator for mass-casting a whole iterable and get rid of tons of warnings.
-template<typename U, typename T>
+template<typename To, typename From>
 struct ConvertingIterator {
-    using value_type = U;
-    using reference = value_type;
-    using pointer = value_type*;
+    using value_type = To;
+    using reference = const value_type;
+    using pointer = typename std::conditional<std::is_same<To, From>::value, value_type*, const void*>::type;
     using iterator_category = std::random_access_iterator_tag;
     using difference_type = std::ptrdiff_t;
 
     // Construction
-    ConvertingIterator(const T* current) : _current(current) {}
+    ConvertingIterator(const From* current) : _current(current) {}
 
     // Access
     constexpr reference operator*() const noexcept {
-        return static_cast<U>(*_current);
+        return static_cast<To>(*_current);
     }
 
-    constexpr pointer operator->() const noexcept {
+    template<typename U_ = To, typename T_ = From>
+    constexpr typename std::enable_if<std::is_same<U_, T_>::value, pointer>::type operator->() const noexcept {
         return _current;
     }
 
+    template<typename U_ = To, typename T_ = From>
+    constexpr typename std::enable_if<!std::is_same<U_, T_>::value>::type operator->() const noexcept {
+        static_assert(std::is_same<U_, T_>::value, "Pointer access not possible.");
+    }
+
     // Comparison
-    constexpr bool operator==(const ConvertingIterator<U, T>& other) const noexcept {
+    constexpr bool operator==(const ConvertingIterator<To, From>& other) const noexcept {
         return other._current == _current;
     }
 
-    constexpr bool operator!=(const ConvertingIterator<U, T>& other) const noexcept {
+    constexpr bool operator!=(const ConvertingIterator<To, From>& other) const noexcept {
         return other._current != _current;
     }
 
-    constexpr bool operator<(const ConvertingIterator<U, T>& other) const noexcept {
+    constexpr bool operator<(const ConvertingIterator<To, From>& other) const noexcept {
         return _current < other._current;
     }
 
-    constexpr bool operator>(const ConvertingIterator<U, T>& other) const noexcept {
+    constexpr bool operator>(const ConvertingIterator<To, From>& other) const noexcept {
         return _current > other._current;
     }
 
-    constexpr bool operator<=(const ConvertingIterator<U, T>& other) const noexcept {
+    constexpr bool operator<=(const ConvertingIterator<To, From>& other) const noexcept {
         return _current <= other._current;
     }
 
-    constexpr bool operator>=(const ConvertingIterator<U, T>& other) const noexcept {
+    constexpr bool operator>=(const ConvertingIterator<To, From>& other) const noexcept {
         return _current >= other._current;
     }
 
     // Increment/Decrement
-    ConvertingIterator<U, T> operator++(int) const {
-        auto temp = ConvertingIterator<U, T>(_current);
+    ConvertingIterator<To, From> operator++(int) const {
+        auto temp = ConvertingIterator<To, From>(_current);
         ++(*this);
         return temp;
     }
 
-    ConvertingIterator<U, T>& operator++() {
+    ConvertingIterator<To, From>& operator++() {
         ++_current;
         return *this;
     }
 
-    ConvertingIterator<U, T> operator--(int) const {
-        auto temp = ConvertingIterator<U, T>(_current);
+    ConvertingIterator<To, From> operator--(int) const {
+        auto temp = ConvertingIterator<To, From>(_current);
         --(*this);
         return temp;
     }
 
-    ConvertingIterator<U, T>& operator--() {
+    ConvertingIterator<To, From>& operator--() {
         --_current;
         return *this;
     }
 
     // Add/Subtract
-    constexpr ConvertingIterator<U, T>& operator+=(const difference_type other) noexcept {
+    constexpr ConvertingIterator<To, From>& operator+=(const difference_type other) noexcept {
         _current += other;
         return *this;
     }
 
-    constexpr ConvertingIterator<U, T> operator+(const difference_type other) const noexcept {
-        return ConvertingIterator<U, T>(_current + other);
+    constexpr ConvertingIterator<To, From> operator+(const difference_type other) const noexcept {
+        return ConvertingIterator<To, From>(_current + other);
     }
 
-    constexpr ConvertingIterator<U, T>& operator-=(const difference_type other) noexcept {
+    constexpr ConvertingIterator<To, From>& operator-=(const difference_type other) noexcept {
         _current -= other;
         return *this;
     }
 
-    constexpr ConvertingIterator<U, T> operator-(const difference_type other) const noexcept {
-        return ConvertingIterator<U, T>(_current - other);
+    constexpr ConvertingIterator<To, From> operator-(const difference_type other) const noexcept {
+        return ConvertingIterator<To, From>(_current - other);
     }
 
-    // Algebra
-    constexpr difference_type operator-(const ConvertingIterator<U, T>& other) const noexcept {
+    constexpr difference_type operator-(const ConvertingIterator<To, From>& other) const noexcept {
         return std::distance(other._current, _current);
     }
 
     // Random access
     constexpr reference operator[](std::size_t index) const {
-        return static_cast<U>(_current[index]);
+        return static_cast<To>(_current[index]);
     }
 
 private:
-    const T* _current;
+    const From* _current;
 };
 } // namespace megamol::core::utility
