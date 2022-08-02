4fab0e65e51e377788d81beb552ad410842e3fd3
Moritz Heinemann <mo@blog-srv.net>
2022-08-02 19:46:25 +0200

    bhtsne export symbols

--- cmake/vcpkg_ports/bhtsne/portfile.cmake
+++ cmake/vcpkg_ports/bhtsne/portfile.cmake
@@ -3,6 +3,7 @@ vcpkg_from_github(
     REPO lvdmaaten/bhtsne
     REF cd619e6c186b909a2d8ed26fbf0b1afec770f43d
     SHA512 de599c34083af328cd0a014e02ee8b61c839361c2b020386230a3f4ff0b3e3980e3ef93d12d52b639c53b39964c571de079b8a31fe8cf0402fdd734c568fb282
+    PATCHES tsne_export.patch
 )
 
 file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
