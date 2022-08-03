377730a3a3692d31cd6997b2c9eb61ed55e5e20f
Alex Heller <st162467@stud.uni-stuttgart.de>
2022-08-03 13:18:53 +0200

    Remove resolved TODO comment

--- plugins/moldyn_gl/src/rendering/SphereRenderer.cpp
+++ plugins/moldyn_gl/src/rendering/SphereRenderer.cpp
@@ -2045,7 +2045,6 @@ bool SphereRenderer::enableTransferFunctionTexture(std::shared_ptr<glowl::GLSLPr
 
     mmstd_gl::CallGetTransferFunctionGL* cgtf = this->get_tf_slot_.CallAs<mmstd_gl::CallGetTransferFunctionGL>();
     if ((cgtf != nullptr) && (*cgtf)(0)) {
-        // TODO: how to do with unique?
         cgtf->BindConvenience(*prgm, GL_TEXTURE0, 0);
     } else {
         glEnable(GL_TEXTURE_1D);
