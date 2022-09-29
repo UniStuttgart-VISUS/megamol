b4a58bce3396b047cc72d300d6e897d1d08d9c31
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2022-09-29 20:24:10 +0200

    move 3rd party code to extra files

--- plugins/mmstd_gl/include/mmstd_gl/special/TextureInspector.h
+++ plugins/mmstd_gl/include/mmstd_gl/special/TextureInspector.h
@@ -79,45 +79,6 @@ public:
 
 private:
     /*
-     * SceneColorFilters
-     * An example showing controls to filter red, green and blue channels
-     * independently. These controls are provided by the DrawColorChannelSelector
-     * funtions.  The example also uses the DrawGridEditor function to allow the
-     * user to control grid appearance.
-     *
-     * The Draw_* functions are provided for convenience, it is of course possible
-     * to control these aspects programmatically.
-     */
-    void SceneColorFilters();
-
-    /*
-     * SceneColorMatrix
-     * An example showing usage of the ColorMatrix.  See comments at the
-     * declaration of CurrentInspector_SetColorMatrix for details.  This example
-     * shows how to set the matrix directly, as well as how to use the
-     * DrawColorMatrixEditor convenience function to draw ImGui controls to
-     * manipulate it.
-     */
-    void SceneColorMatrix();
-
-    void SceneTextureAnnotations();
-
-    /*
-     * SceneAlphaMode
-     * Very simple example that calls DrawAlphaModeSelector to draw controls to
-     * allow user to select alpha mode for the inpsector. See InspectorAlphaMode
-     * enum for details on what the different modes are.
-     */
-    void SceneAlphaMode();
-
-    /**
-     * SceneWrapAndFilter
-     * Scene showing the effect of the InspectorFlags_ShowWrap & InspectorFlags_NoForceFilterNearest flags.
-     * See InspectorFlags_ enum for details on these flags.
-     */
-    void SceneWrapAndFilter();
-
-    /*
      * Init
      * Initializes opengl and creates a context.
      */
@@ -128,8 +89,6 @@ private:
 
     /** Slot to select a specific texture */
     core::param::ParamSlot select_texture_;
-
-    void (TextureInspector::*draw)();
 
     Texture tex_;
     // ImGuiTexInspect::InspectorFlags flags_;
--- plugins/mmstd_gl/src/special/TextureInspector.cpp
+++ plugins/mmstd_gl/src/special/TextureInspector.cpp
@@ -1,28 +1,4 @@
 /**
- * The MIT License (MIT)
- *
- * Copyright (c) 2014-2021 Omar Cornut
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-/**
  * MegaMol
  * Copyright (c) 2022, MegaMol Dev Team
  * All rights reserved.
@@ -30,6 +6,7 @@
 
 #include "mmstd_gl/special/TextureInspector.h"
 
+#include "ImGuiTexInspect/DemoSnippets.h"
 #include "imgui_tex_inspect_internal.h"
 #include "tex_inspect_opengl.h"
 
@@ -38,7 +15,6 @@ using namespace megamol::mmstd_gl::special;
 TextureInspector::TextureInspector(const std::vector<std::string>& textures)
         : show_inspector_("Show", "Turn the texture inspector on or off.")
         , select_texture_("Texture", "Select which texture to be shown.")
-        , draw(nullptr)
         , tex_({nullptr, 0.f, 0.f})
         , flags_(0)
         , flip_x_(false)
@@ -57,7 +33,6 @@ TextureInspector::TextureInspector(const std::vector<std::string>& textures)
 TextureInspector::TextureInspector()
         : show_inspector_("Show", "Turn the texture inspector on or off.")
         , select_texture_("Texture", "Select which texture to be shown.")
-        , draw(nullptr)
         , tex_({nullptr, 0.f, 0.f})
         , flags_(0)
         , flip_x_(false)
@@ -72,271 +47,6 @@ TextureInspector::TextureInspector()
 }
 
 TextureInspector::~TextureInspector() {}
-
-
-//-------------------------------------------------------------------------
-// [SECTION] EXAMPLE USAGES
-//-------------------------------------------------------------------------
-
-/* Each of the following Scene* functions is a standalone demo showing an example
- * usage of ImGuiTexInspect.  You'll notice the structure is essentially the same in
- * all the examples, i.e. a call to BeginInspectorPanel & a call to
- * EndInspectorPanel, possibly with some annotation functions in between,
- * followed by some controls to manipulate the inspector.
- *
- * Each Scene* function corresponds to one of the large buttons at the top of
- * the scene window.
- */
-
-/**
- * void TextureInspector::SceneColorFilters
- */
-void TextureInspector::SceneColorFilters() {
-    /* BeginInspectorPanel & EndInspectorPanel is all you need to draw an
-     * inspector (assuming you are already in between an ImGui::Begin and
-     * ImGui::End pair)
-     * */
-
-    if (ImGuiTexInspect::BeginInspectorPanel(
-            "##ColorFilters", this->tex_.texture, ImVec2(this->tex_.x, this->tex_.y), flags_)) {
-        // Draw some text showing color value of each texel (you must be zoomed in to see this)
-        ImGuiTexInspect::DrawAnnotations(ImGuiTexInspect::ValueText(ImGuiTexInspect::ValueText::BytesDec));
-    }
-    ImGuiTexInspect::EndInspectorPanel();
-
-    // Now some ordinary ImGui elements to provide some explanation
-    ImGui::BeginChild("Controls", ImVec2(600, 100));
-    ImGui::TextWrapped("Basics:");
-    ImGui::BulletText("Use mouse wheel to zoom in and out.  Click and drag to pan.");
-    ImGui::BulletText("Use the scene select buttons at the top of the window to explore");
-    ImGui::BulletText("Use the controls below to change basic color filtering options");
-    ImGui::EndChild();
-
-
-    /* DrawColorChannelSelector & DrawGridEditor are convenience functions that
-     * draw ImGui controls to manipulate config of the most recently drawn
-     * texture inspector
-     **/
-    ImGuiTexInspect::DrawColorChannelSelector();
-    ImGui::SameLine(200);
-    ImGuiTexInspect::DrawGridEditor();
-}
-
-//-------------------------------------------------------------------------
-
-/**
- * void TextureInspector::SceneColorMatrix
- */
-void TextureInspector::SceneColorMatrix() {
-    if (ImGuiTexInspect::BeginInspectorPanel(
-            "##ColorMatrix", this->tex_.texture, ImVec2(this->tex_.x, this->tex_.y), flags_)) {
-        // Draw some text showing color value of each texel (you must be zoomed in to see this)
-        ImGuiTexInspect::DrawAnnotations(ImGuiTexInspect::ValueText(ImGuiTexInspect::ValueText::BytesDec));
-    }
-    ImGuiTexInspect::EndInspectorPanel();
-
-    ImGui::BeginGroup();
-    ImGui::Text("Colour Matrix Editor:");
-    // Draw Matrix editor to allow user to manipulate the ColorMatrix
-    ImGuiTexInspect::DrawColorMatrixEditor();
-    ImGui::EndGroup();
-
-    ImGui::SameLine();
-
-    // Provide some presets that can be used to set the ColorMatrix for example purposes
-    ImGui::BeginGroup();
-    ImGui::PushItemWidth(200);
-    ImGui::Indent(50);
-    const ImVec2 button_size = ImVec2(160, 0);
-    ImGui::Text("Example Presets:");
-    // clang-format off
-    if (ImGui::Button("Negative", button_size))
-    {
-        // Matrix which inverts each of the red, green, blue channels and leaves Alpha untouched
-        float matrix[] = {-1.000f,  0.000f,  0.000f,  0.000f, 
-                           0.000f, -1.000f,  0.000f,  0.000f,
-                           0.000f,  0.000f, -1.000f,  0.000f, 
-                           0.000f,  0.000f,  0.000f,  1.000f};
-
-        float color_offset[] = {1, 1, 1, 0};
-        ImGuiTexInspect::CurrentInspector_SetColorMatrix(matrix, color_offset);
-    }
-    if (ImGui::Button("Swap Red & Blue", button_size))
-    {
-        // Matrix which swaps red and blue channels but leaves green and alpha untouched
-        float matrix[] = { 0.000f,  0.000f,  1.000f,  0.000f, 
-                           0.000f,  1.000f,  0.000f,  0.000f,
-                           1.000f,  0.000f,  0.000f,  0.000f, 
-                           0.000f,  0.000f,  0.000f,  1.000f};
-        float color_offset[] = {0, 0, 0, 0};
-        ImGuiTexInspect::CurrentInspector_SetColorMatrix(matrix, color_offset);
-    }
-    if (ImGui::Button("Alpha", button_size))
-    {
-        // Red, green and blue channels are set based on alpha value so that alpha = 1 shows as white. 
-        // output alpha is set to 1
-        float highlight_transparency_matrix[] = {0.000f, 0.000f, 0.000f, 0.000f,
-                                               0.000f, 0.000f, 0.000f, 0.000f,
-                                               0.000f, 0.000f, 0.000f, 0.000f, 
-                                               1.000,  1.000,  1.000,  1.000f};
-        float highlight_transparency_offset[] = {0, 0, 0, 1};
-        ImGuiTexInspect::CurrentInspector_SetColorMatrix(highlight_transparency_matrix, highlight_transparency_offset);
-    }
-    if (ImGui::Button("Transparency", button_size))
-    {
-        // Red, green and blue channels are scaled by 0.1f. Low alpha values are shown as magenta
-        float highlight_transparency_matrix[] = {0.100f,  0.100f,  0.100f,  0.000f, 
-                                               0.100f,  0.100f,  0.100f,  0.000f,
-                                               0.100f,  0.100f,  0.100f,  0.000f, 
-                                              -1.000f,  0.000f, -1.000f,  0.000f};
-        float highlight_transparency_offset[] = {1, 0, 1, 1};
-        ImGuiTexInspect::CurrentInspector_SetColorMatrix(highlight_transparency_matrix, highlight_transparency_offset);
-    }
-    if (ImGui::Button("Default", button_size))
-    {
-        // Default "identity" matrix that doesn't modify colors at all
-        float matrix[] = {1.000f, 0.000f, 0.000f, 0.000f, 
-                          0.000f, 1.000f, 0.000f, 0.000f,
-                          0.000f, 0.000f, 1.000f, 0.000f, 
-                          0.000f, 0.000f, 0.000f, 1.000f};
-
-        float color_offset[] = {0, 0, 0, 0};
-        ImGuiTexInspect::CurrentInspector_SetColorMatrix(matrix, color_offset);
-    }
-    // clang-format on
-    ImGui::PopItemWidth();
-    ImGui::EndGroup();
-}
-
-/**
- * void TextureInspector::SceneAlphaMode
- */
-void TextureInspector::SceneAlphaMode() {
-    if (ImGuiTexInspect::BeginInspectorPanel(
-            "##AlphaMode", this->tex_.texture, ImVec2(this->tex_.x, this->tex_.y), flags_)) {
-        // Add annotations here
-    }
-    ImGuiTexInspect::EndInspectorPanel();
-    ImGuiTexInspect::DrawAlphaModeSelector();
-}
-
-/**
- * void TextureInspector::SceneWrapAndFilter
- */
-void TextureInspector::SceneWrapAndFilter() {
-    static bool show_wrap = false;
-    static bool force_nearest_texel = true;
-
-    if (ImGuiTexInspect::BeginInspectorPanel(
-            "##WrapAndFilter", this->tex_.texture, ImVec2(this->tex_.x, this->tex_.y), flags_)) {}
-
-    ImGuiTexInspect::InspectorFlags flags = 0;
-
-    if (show_wrap)
-        flags |= ImGuiTexInspect::InspectorFlags_ShowWrap;
-    if (!force_nearest_texel)
-        flags |= ImGuiTexInspect::InspectorFlags_NoForceFilterNearest;
-
-    ImGuiTexInspect::CurrentInspector_SetFlags(flags, ~flags);
-    ImGuiTexInspect::EndInspectorPanel();
-
-    ImGui::BeginChild("Controls", ImVec2(600, 0));
-    ImGui::TextWrapped(
-        "The following option can be enabled to render texture outside of the [0,1] UV range, what you actually "
-        "see outside of this range will depend on the mode of the texture. For example you may see the texture repeat, "
-        "or "
-        "it might be clamped to the colour of the edge pixels.\nIn this scene the texture is set to wrap.");
-    ImGui::Checkbox("Show Wrapping Mode", &show_wrap);
-
-    ImGui::TextWrapped("The following option is enabled by default and forces a nearest texel filter, implemented at "
-                       "the shader level. "
-                       "By disabling this you can the currently set mode for this texture.");
-    ImGui::Checkbox("Force Nearest Texel", &force_nearest_texel);
-    ImGui::EndChild();
-}
-
-// This class is used in SceneTextureAnnotations to show the process of creating a new texture annotation.
-class CustomAnnotationExample {
-public:
-    void DrawAnnotation(ImDrawList* drawList, ImVec2 texel, ImGuiTexInspect::Transform2D texelsToPixels, ImVec4 value) {
-        /* A silly example to show the process of creating a new annotation
-         * We'll see which primary colour is the dominant colour in the texel
-         * then draw a different shape for each primary colour.  The radius
-         * will be based on the overall brightness.
-         */
-        int num_segments;
-
-        if (value.x > value.y && value.x > value.z) {
-            // Red pixel - draw a triangle!
-            num_segments = 3;
-        } else {
-            if (value.y > value.z) {
-                // Green pixel - draw a diamond!
-                num_segments = 4;
-            } else {
-                // Blue pixel - draw a hexagon!
-                num_segments = 6;
-            }
-        }
-
-        // Don't go larger than whole texel
-        const float max_radius = texelsToPixels.Scale.x * 0.5f;
-
-        // Scale radius based on texel brightness
-        const float radius = max_radius * (value.x + value.y + value.z) / 3;
-        drawList->AddNgon(texelsToPixels * texel, radius, 0xFFFFFFFF, num_segments);
-    }
-};
-
-/**
- * void TextureInspector::SceneTextureAnnotations
- */
-void TextureInspector::SceneTextureAnnotations() {
-    static bool annotation_enabled_arrow = true;
-    static bool annotation_enabled_value_text = false;
-    static bool annotation_enabled_custom_example = false;
-
-    static ImGuiTexInspect::ValueText::Format text_format = ImGuiTexInspect::ValueText::BytesHex;
-
-    const int max_annotated_texels = 1000;
-
-    if (ImGuiTexInspect::BeginInspectorPanel(
-            "##TextureAnnotations", this->tex_.texture, ImVec2(this->tex_.x, this->tex_.y), flags_)) {
-        // Draw the currently enabled annotations...
-        if (annotation_enabled_arrow) {
-            ImGuiTexInspect::DrawAnnotations(
-                ImGuiTexInspect::Arrow().UsePreset(ImGuiTexInspect::Arrow::NormalMap), max_annotated_texels);
-        }
-
-        if (annotation_enabled_value_text) {
-            ImGuiTexInspect::DrawAnnotations(ImGuiTexInspect::ValueText(text_format), max_annotated_texels);
-        }
-
-        if (annotation_enabled_custom_example) {
-            ImGuiTexInspect::DrawAnnotations(CustomAnnotationExample(), max_annotated_texels);
-        }
-    }
-    ImGuiTexInspect::EndInspectorPanel();
-
-    // Checkboxes to toggle each type of annotation on and off
-    ImGui::BeginChild("Controls", ImVec2(600, 0));
-    ImGui::Checkbox("Arrow (Hint: zoom in on the normal-map part of the texture)", &annotation_enabled_arrow);
-    ImGui::Checkbox("Value Text", &annotation_enabled_value_text);
-    ImGui::Checkbox("Custom Annotation Example", &annotation_enabled_custom_example);
-    ImGui::EndChild();
-
-    if (annotation_enabled_value_text) {
-        // Show a combo to select the text formatting mode
-        ImGui::SameLine();
-        ImGui::BeginGroup();
-        const char* text_options[] = {"Hex String", "Bytes in Hex", "Bytes in Decimal", "Floats"};
-        ImGui::SetNextItemWidth(200);
-        int text_format_int = (int)(text_format);
-        ImGui::Combo("Text Mode", &text_format_int, text_options, IM_ARRAYSIZE(text_options));
-        text_format = (ImGuiTexInspect::ValueText::Format)text_format_int;
-        ImGui::EndGroup();
-    }
-}
 
 //-------------------------------------------------------------------------
 // [SECTION] MAIN SCENE WINDOW FUNCTION
@@ -354,16 +64,17 @@ void TextureInspector::ShowWindow() {
     ImGui::SetNextWindowSize(ImVec2(1000, 1000), ImGuiCond_FirstUseEver);
 
     struct SceneConfig {
-        const char* button_name;             // Button text to display to user for a scene
-        void (TextureInspector::*draw_fn)(); // Function which implements the scene
+        const char* button_name; // Button text to display to user for a scene
+        void (*draw_fn)(const ImGuiTexInspect::Texture& testTex,
+            ImGuiTexInspect::InspectorFlags inFlags); // Function which implements the scene
     };
 
     const SceneConfig scenes[] = {
-        {"Basics", &TextureInspector::SceneColorFilters},
-        {"Color Matrix", &TextureInspector::SceneColorMatrix},
-        {"Annotations", &TextureInspector::SceneTextureAnnotations},
-        {"Alpha Mode", &TextureInspector::SceneAlphaMode},
-        {"Wrap & Filter", &TextureInspector::SceneWrapAndFilter},
+        {"Basics", &ImGuiTexInspect::Demo_ColorFilters},
+        {"Color Matrix", &ImGuiTexInspect::Demo_ColorMatrix},
+        {"Annotations", &ImGuiTexInspect::Demo_TextureAnnotations},
+        {"Alpha Mode", &ImGuiTexInspect::Demo_AlphaMode},
+        {"Wrap & Filter", &ImGuiTexInspect::Demo_WrapAndFilter},
     };
 
     if (ImGui::Begin("ImGuiTexInspect")) {
@@ -398,8 +109,7 @@ void TextureInspector::ShowWindow() {
             SetFlag(flags_, ImGuiTexInspect::InspectorFlags_FlipY);
 
         // Call function to render currently example scene
-        this->draw = scenes[selected_scene].draw_fn;
-        (this->*draw)();
+        (*(scenes[selected_scene].draw_fn))({tex_.texture, ImVec2{tex_.x, tex_.y}}, flags_);
 
         ImGui::Separator();
 
