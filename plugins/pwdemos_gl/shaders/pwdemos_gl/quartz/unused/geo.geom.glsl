93729d7988f2a1a34477a4409bbb137d61f5634e
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2022-10-04 19:20:34 +0200

    save unused quartz btf shader

---
+++ plugins/pwdemos_gl/shaders/pwdemos_gl/quartz/unused/geo.geom.glsl
@@ -0,0 +1,8 @@
+#version 430
+
+#include "pwdemos_gl/quartz/snippets/geo_geom_1.glsl"
+#include "pwdemos_gl/quartz/snippets/geo_geom_2.glsl"
+#include "pwdemos_gl/quartz/snippets/common_directLight.glsl"
+#include "pwdemos_gl/quartz/snippets/geo_geom_3.glsl"
+#include "pwdemos_gl/quartz/snippets/geo_geom_geometry.glsl"
+#include "pwdemos_gl/quartz/snippets/geo_geom_4.glsl"
---
+++ plugins/pwdemos_gl/shaders/pwdemos_gl/quartz/unused/geo.vert.glsl
@@ -0,0 +1,4 @@
+#version 430
+
+#include "pwdemos_gl/quartz/snippets/geo_vert_1.glsl"
+#include "pwdemos_gl/quartz/snippets/geo_vert_2.glsl"
