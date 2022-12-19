eb8ef6e1c09559990c6b09bad38cedbd5717672d
Guido Reina <guido.reina@informatik.uni-stuttgart.de>
2022-12-19 18:11:06 +0100

    better color

--- plugins/mmstd_gl/shaders/mmstd_gl/animation/campath.vert.glsl
+++ plugins/mmstd_gl/shaders/mmstd_gl/animation/campath.vert.glsl
@@ -7,6 +7,6 @@ uniform int line_len;
 out vec4 vertColor;
 
 void main(void) {
-    vertColor = vec4(gl_VertexID / float(line_len), 1.0, 1.0, 1.0);
+    vertColor = vec4(gl_VertexID / float(line_len) * 0.5 + 0.5, 0.5, 0.5, 1.0);
     gl_Position = mvp * vec4(in_pos, 1.0);
 }
