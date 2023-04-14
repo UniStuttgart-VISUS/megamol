5daaa5ba75c52e7e09def724b1121d676c45c43b
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2023-04-14 14:12:27 +0200

    output formatting

--- .ci/check_shaders.sh
+++ .ci/check_shaders.sh
@@ -26,6 +26,8 @@ do
   INCLUDE_PATHS+=("-I$(pwd)/${sdir}")
 done
 
+echo "::add-matcher::.ci/check-shaders-problem-matchers.json"
+
 # Iterate over all shaders
 for sdir in "${SHADER_DIRS[@]}"
 do
@@ -44,12 +46,17 @@ do
     LOCAL_INCLUDE_PATHS=("-I$(pwd)/$(dirname "$sfile")")
 
     glslang_exit_code=0
-    glslangValidator -l "${INCLUDE_PATHS[@]}" --p "#extension GL_GOOGLE_include_directive : require" "${LOCAL_INCLUDE_PATHS[@]}" $sfile || glslang_exit_code=$?
+    output="$(glslangValidator -l "${INCLUDE_PATHS[@]}" --p "#extension GL_GOOGLE_include_directive : require" "${LOCAL_INCLUDE_PATHS[@]}" $sfile)" || glslang_exit_code=$?
     if [[ $glslang_exit_code -ne 0 ]]; then
+      echo ""
+      echo "::error::########## glslangValidator found issues in $sfile ##########"
+      echo "$output"
       EXIT_CODE=1
     fi
 
   done <<< "$shader_files"
 done
+
+echo "::remove-matcher owner=glslang-check::"
 
 exit $EXIT_CODE
