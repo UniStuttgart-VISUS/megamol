e9b0688a4fc3a36e9f262d83202f0f90275eb31d
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2023-05-09 17:17:13 +0200

    fix imgui define

--- cmake/imgui/megamol_imgui_config.h
+++ cmake/imgui/megamol_imgui_config.h
@@ -2,4 +2,5 @@
 
 #define IMGUI_DEFINE_MATH_OPERATORS
 //#define IMGUI_DISABLE_OBSOLETE_FUNCTIONS // TODO implot currently requires deprecated functions, can be enabled when implot is updated.
-#define IMGUI_DISABLE_OBSOLETE_KEYIO
+
+// Do not use IMGUI_DISABLE_OBSOLETE_KEYIO! This breaks ImGui ABI, because imgui port is build without this define.
