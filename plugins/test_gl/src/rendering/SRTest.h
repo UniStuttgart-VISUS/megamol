2703f76ff2ff995f6e3776cf5afeb1cd2c493796
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2023-06-11 15:30:12 +0200

    fixed some dependencies

--- plugins/test_gl/src/rendering/SRTest.h
+++ plugins/test_gl/src/rendering/SRTest.h
@@ -149,7 +149,9 @@ class SRTest : public mmstd_gl::Renderer3DModuleGL {
 public:
     static void requested_lifetime_resources(frontend_resources::ResourceRequest& req) {
         /*std::vector<std::string> resources = */ ModuleGL::requested_lifetime_resources(req);
+#ifdef MEGAMOL_USE_PROFILING
         req.require<frontend_resources::PerformanceManager>();
+#endif
         //resources.emplace_back(frontend_resources::PerformanceManager_Req_Name);
         //return resources;
     }
--- plugins/test_gl/src/test_gl.cpp
+++ plugins/test_gl/src/test_gl.cpp
@@ -7,6 +7,8 @@
 #include "mmcore/factories/AbstractPluginInstance.h"
 #include "mmcore/factories/PluginRegister.h"
 
+#include "rendering/SRTest.h"
+
 namespace megamol::MegaMolPlugin {
 class PluginInstance : public megamol::core::factories::AbstractPluginInstance {
     REGISTERPLUGIN(PluginInstance)
@@ -32,6 +34,7 @@ public:
         // this->module_descriptions.RegisterAutoDescription<megamol::MegaMolPlugin::MyModule2>();
         // ...
         //
+        this->module_descriptions.RegisterAutoDescription<megamol::test_gl::rendering::SRTest>();
 
         // register calls
 
