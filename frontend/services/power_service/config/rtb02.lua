7863db2af6b9ab1642698846f888ef5567f4a30e
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2023-08-01 18:36:03 +0200

    sol osci config scripts

---
+++ frontend/services/power_service/config/rtb02.lua
@@ -0,0 +1,12 @@
+acq = oscilloscope_single_acquisition:new():points(points):count(count):segmented(true)
+trigger = oscilloscope_edge_trigger:new("EXT")
+trigger:level(5, oscilloscope_quantity:new(2000.0, "mV")):slope(oscilloscope_trigger_slope.rising):mode(oscilloscope_trigger_mode.normal)
+quant = oscilloscope_quantity:new(range, "ms")
+rtb02 = rtx_instrument_configuration:new(quant, acq, trigger, timeout);
+chan_1 = oscilloscope_channel:new(1)
+chan_1:state(true):attenuation(oscilloscope_quantity:new(10, "A")):range(oscilloscope_quantity:new(40, "A")):label(oscilloscope_label:new("current#1", true))
+chan_2 = oscilloscope_channel:new(2)
+chan_2:state(true):attenuation(oscilloscope_quantity:new(10, "A")):range(oscilloscope_quantity:new(40, "A")):label(oscilloscope_label:new("current#2", true))
+rtb02:channel(chan_1)
+rtb02:channel(chan_2)
+rtb02 = as_slave(rtb02)
