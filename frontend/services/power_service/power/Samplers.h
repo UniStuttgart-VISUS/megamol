75b2d4d5ec1cfd2eaed9014283e79db4c5e34bdf
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2023-10-19 17:38:58 +0200

    timestamp fix

--- frontend/services/power_service/power/Samplers.h
+++ frontend/services/power_service/power/Samplers.h
@@ -31,13 +31,13 @@ using buffers_t = std::vector<SampleBuffer>;
 using context_t = std::tuple<std::string*, SampleBuffer*, bool const&, int64_t const&>;
 
 inline int64_t convert_timestamp_ns(int64_t const& offset_100ns, int64_t const& ts_100ns) {
-    return (ts_100ns + offset_100ns) * 100;
+    return ts_100ns + offset_100ns;
 }
 
 inline int64_t convert_walltime_ns(int64_t const& ts_100ns) {
     constexpr int64_t offset =
         std::chrono::duration<std::int64_t, std::ratio<1, 10000000>>(116444736000000000LL).count();
-    return (ts_100ns - offset) * 100;
+    return ts_100ns - offset;
 }
 
 inline void tracy_sample(
