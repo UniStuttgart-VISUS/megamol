4b6cdf95e6c6243b89ec3d500f7dd96e7cc5efc4
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2023-11-06 18:11:22 +0100

    comments

--- frontend/services/power_service/power/Trigger.h
+++ frontend/services/power_service/power/Trigger.h
@@ -16,20 +16,48 @@
 #endif
 
 namespace megamol::power {
-class Trigger {
+/// <summary>
+/// Class containing the trigger base functionality to trigger an oscilloscope over the parallel port
+/// and synchronize the measurement with other sensors.
+/// </summary>
+class Trigger final {
 public:
+    /// <summary>
+    /// Ctr setting the address of the parallel port to use.
+    /// Expected format: "lpt1" without prefix "\\.\"
+    /// </summary>
+    /// <param name="address">LPT address</param>
     Trigger(std::string const& address) {
         set_lpt(address);
     }
 
+    /// <summary>
+    /// Arms the trigger sequence.
+    /// </summary>
     void ArmTrigger() {
         armed_ = true;
     }
 
+    /// <summary>
+    /// Disarms the trigger sequence.
+    /// Trigger sequence will exit after full cycle.
+    /// </summary>
     void DisarmTrigger() {
         armed_ = false;
     }
 
+    /// <summary>
+    /// Runs the trigger sequence as long as <c>armed_</c> is <c>true</c>.
+    /// The sequence fires a pre trigger first, for instance, to start recording non-osci sensors, since an osci records the time window of the past.
+    /// Then the main trigger fires that will start the measurement at the oscilloscope.
+    /// Afterwards, a post trigger is fired , for instance, to stop recording of non-osci sensors.
+    /// After exit of this trigger sequence a final trigger is fired, for instance, to write the recorded buffers of the measurements.
+    /// This function locks access to the parallel port handle.
+    /// </summary>
+    /// <param name="prefix"></param>
+    /// <param name="postfix"></param>
+    /// <param name="wait"></param>
+    /// <returns>The timestamp of the last main trigger in filetime.</returns>
     filetime_dur_t StartTriggerSequence(std::chrono::milliseconds const& prefix,
         std::chrono::milliseconds const& postfix, std::chrono::milliseconds const& wait) {
         filetime_dur_t trg_tp;
@@ -54,18 +82,37 @@ public:
         sub_trigger_[name] = trigger;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="pre_trigger"></param>
     void RegisterPreTrigger(std::string const& name, std::function<void()> const& pre_trigger) {
         pre_trigger_[name] = pre_trigger;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="post_trigger"></param>
     void RegisterPostTrigger(std::string const& name, std::function<void()> const& post_trigger) {
         post_trigger_[name] = post_trigger;
     }
 
+    /// <summary>
+    /// Register a final trigger that is fired after the trigger sequence.
+    /// </summary>
+    /// <param name="name">Name of the trigger. Should be unique.</param>
+    /// <param name="fin_trigger">The instance of the function to register.</param>
     void RegisterFinTrigger(std::string const& name, std::function<void()> const& fin_trigger) {
         fin_trigger_[name] = fin_trigger;
     }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="address"></param>
     void SetLPTAddress(std::string const& address) {
         set_lpt(address);
     }
