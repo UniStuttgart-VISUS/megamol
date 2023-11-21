5f5589e599367ac082a10cae448a4a28336c8ca8
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2023-11-21 16:01:28 +0100

    fix imgui-tex-inspect

--- cmake/vcpkg_ports/imgui-tex-inspect/portfile.cmake
+++ cmake/vcpkg_ports/imgui-tex-inspect/portfile.cmake
@@ -6,6 +6,8 @@ vcpkg_from_github(
     REF ccff03b844cc9845cc3e3c6ef69026fe7051d330 # https://github.com/andyborrell/imgui_tex_inspect/pull/3
     SHA512 16e73a68bb8c4473bb8b3b1ddbab62453b36f4e4d0deb5f62c209c7b05bbe12173420dee8441e3d1ab9cec25f6dd77f8f4039c1a6f91fe972c88e5c21cfa634f
     HEAD_REF main
+    PATCHES
+        imgui-changes.patch
 )
 
 vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
