802d7d83ca21717fceff0089c419bc596284352a
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2024-06-28 20:32:43 +0200

    Cleanup and comments. Dataverse writer should be Windows only currently.

--- frontend/services/power_service/power/DataverseWriter.cpp
+++ frontend/services/power_service/power/DataverseWriter.cpp
@@ -2,16 +2,15 @@
 
 #ifdef MEGAMOL_USE_POWER
 
-#include <atomic>
-#include <chrono>
 #include <iostream>
-#include <thread>
+#include <string>
 
 #include <dataverse/dataverse_connection.h>
 
 namespace megamol::power {
 void DataverseWriter(std::string const& dataverse_path, std::string const& doi, std::string const& filepath,
     char const* key, char& signal) {
+#if WIN32
     using namespace visus::dataverse;
     signal = true;
     try {
@@ -36,35 +35,7 @@ void DataverseWriter(std::string const& dataverse_path, std::string const& doi, 
     } catch (...) {
         signal = false;
     }
-    //std::thread t(
-    //    [](std::string const& dataverse_path, std::string const& doi, std::string const& filepath, char const* key) {
-    //        std::atomic_bool running(true);
-    //        dataverse_connection dataverse;
-    //        dataverse.base_path(make_const_narrow_string(dataverse_path, CP_OEMCP))
-    //            .api_key(make_const_narrow_string(key, CP_OEMCP))
-    //            .upload(
-    //                make_const_narrow_string(doi, CP_OEMCP), make_const_narrow_string(filepath, CP_OEMCP),
-    //                /*make_const_narrow_string("Test Data", CP_OEMCP), make_const_narrow_string("", CP_OEMCP), nullptr, 0,
-    //                false,*/
-    //                [](const blob& result, void* context) {
-    //                    std::string output(result.as<char>(), result.size());
-    //                    std::cout << convert<char>(convert<wchar_t>(output, CP_UTF8), CP_OEMCP) << std::endl;
-    //                    std::cout << std::endl;
-    //                    (*static_cast<decltype(running)*>(context)).store(false);
-    //                },
-    //                [](const int error, const char* msg, const char* cat, narrow_string::code_page_type cp,
-    //                    void* context) {
-    //                    std::cerr << msg << std::endl << std::endl;
-    //                    (*static_cast<decltype(running)*>(context)).store(false);
-    //                },
-    //                &running);
-
-    //        while (running.load()) {
-    //            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    //        }
-    //    },
-    //    dataverse_path, doi, filepath, key);
-    //t.detach();
+#endif
 }
 } // namespace megamol::power
 
--- frontend/services/power_service/power/DataverseWriter.h
+++ frontend/services/power_service/power/DataverseWriter.h
@@ -5,6 +5,14 @@
 #include <string>
 
 namespace megamol::power {
+/**
+ * @brief Writes the specified file into the Dataverse.
+ * @param dataverse_path API path to the Dataverse.
+ * @param doi DOI of the dataset to write into in the Dataverse.
+ * @param filepath The path to the file to be written into the Dataverse.
+ * @param key API token of the Dataverse.
+ * @param signal True while the file is being written.
+ */
 void DataverseWriter(std::string const& dataverse_path, std::string const& doi, std::string const& filepath,
     char const* key, char& signal);
 }
