e2f0bf627c3f8589c1ac2c19498158d0c05cef18
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2024-07-06 11:03:04 +0200

    updated findoptix, add lineinfo to ptx debug compile, cleanup

--- plugins/optix_hpg/cmake/configure_optix.cmake
+++ plugins/optix_hpg/cmake/configure_optix.cmake
@@ -1,35 +1,4 @@
-# ======================================================================== #
-# Copyright 2018-2020 Ingo Wald                                            #
-#                                                                          #
-# Licensed under the Apache License, Version 2.0 (the "License");          #
-# you may not use this file except in compliance with the License.         #
-# You may obtain a copy of the License at                                  #
-#                                                                          #
-#     http://www.apache.org/licenses/LICENSE-2.0                           #
-#                                                                          #
-# Unless required by applicable law or agreed to in writing, software      #
-# distributed under the License is distributed on an "AS IS" BASIS,        #
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
-# See the License for the specific language governing permissions and      #
-# limitations under the License.                                           #
-# ======================================================================== #
-
-#set(CMAKE_MODULE_PATH
-#  "${CMAKE_CURRENT_SOURCE_DIR}/../cmake"
-#  ${CMAKE_MODULE_PATH}
-#  )
-
-#include(configure_cuda)
-#find_package(CUDA REQUIRED)
 find_package(OptiX REQUIRED)
-
-#include_directories(${CUDA_TOOLKIT_INCLUDE})
-#include_directories(${OptiX_INCLUDE})
-
-#if (WIN32)
-#  add_definitions(-DNOMINMAX)
-#endif()
-
   
 find_program(BIN2C bin2c
   DOC "Path to the cuda-sdk bin2c executable.")
@@ -56,6 +25,7 @@ function(embed_ptx)
   set_property(TARGET ${PTX_TARGET} PROPERTY CUDA_PTX_COMPILATION ON)
   set_property(TARGET ${PTX_TARGET} PROPERTY CUDA_ARCHITECTURES OFF)
   set_property(TARGET ${PTX_TARGET} PROPERTY CXX_STANDARD 17)
+  target_compile_options(${PTX_TARGET} PRIVATE $<$<CONFIG:Debug>:-lineinfo> -diag-suppress 20012) # warning suppressed due to GLM
 
   set(EMBED_PTX_C_FILE ${CMAKE_CURRENT_BINARY_DIR}/${EMBED_PTX_OUTPUT_TARGET}.c)
   get_filename_component(OUTPUT_FILE_NAME ${EMBED_PTX_C_FILE} NAME)
@@ -70,38 +40,3 @@ function(embed_ptx)
   add_library(${EMBED_PTX_OUTPUT_TARGET} OBJECT)
   target_sources(${EMBED_PTX_OUTPUT_TARGET} PRIVATE ${EMBED_PTX_C_FILE})
 endfunction()
-
-# this macro defines cmake rules that execute the following four steps:
-# 1) compile the given cuda file ${cuda_file} to an intermediary PTX file
-# 2) use the 'bin2c' tool (that comes with CUDA) to
-#    create a second intermediary (.c-)file which defines a const string variable
-#    (named '${c_var_name}') whose (constant) value is the PTX output
-#    from the previous step.
-# 3) compile the given .c file to an intermediary object file (why thus has
-#    that PTX string 'embedded' as a global constant.
-# 4) assign the name of the intermediary .o file to the cmake variable
-#    'output_var', which can then be added to cmake targets.
-#macro(cuda_compile_and_embed output_var cuda_file)
-#  set(c_var_name ${output_var})
-#  if(${CMAKE_BUILD_TYPE} MATCHES "Release")
-#    cuda_compile_ptx(ptx_files
-#      ${cuda_file}
-#      OPTIONS -O3 -DNDEBUG=1 --use_fast_math -arch=compute_${CMAKE_CUDA_ARCHITECTURES}
-#      )
-#  else()
-#    cuda_compile_ptx(ptx_files
-#      ${cuda_file}
-#      OPTIONS -arch=compute_${CMAKE_CUDA_ARCHITECTURES}
-#      )
-#  endif()
-#  list(GET ptx_files 0 ptx_file)
-#  set(embedded_file ${ptx_file}_embedded.c)
-##  message("adding rule to compile and embed ${cuda_file} to \"const char ${var_name}[];\"")
-#  add_custom_command(
-#    OUTPUT ${embedded_file}
-#    COMMAND ${BIN2C} -c --padd 0 --type char --name ${c_var_name} ${ptx_file} > ${embedded_file}
-#    DEPENDS ${ptx_file}
-#    COMMENT "compiling (and embedding ptx from) ${cuda_file}"
-#    )
-#  set(${output_var} ${embedded_file})
-#endmacro()
