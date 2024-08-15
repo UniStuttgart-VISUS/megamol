d646098fe9a476675d83f7c07e7ac84a27fb1156
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2024-08-15 20:45:36 +0200

    split dim in two coords

--- plugins/optix_owl/cuda_resources/particle.h
+++ plugins/optix_owl/cuda_resources/particle.h
@@ -11,11 +11,19 @@ namespace device {
 using namespace owl::common;
 struct Particle {
     vec3f pos;
-    void set_dim(int dim) {
-        pos.x = (pos.x & ~3) | (dim & 3);
+    void set_dim(int const dim) {
+        auto x_val = reinterpret_cast<uint32_t*>(&pos.x);
+        *x_val = (*x_val & ~1u) | (dim & 1u);
+        auto y_val = reinterpret_cast<uint32_t*>(&pos.y);
+        *y_val = (*y_val & ~1u) | ((dim >> 1) & 1u);
     }
     CU_CALLABLE int get_dim() const {
-        return pos.x & 3;
+        int dim = 0;
+        auto x_val = reinterpret_cast<uint32_t const*>(&pos.x);
+        dim = (dim & ~1u) | (*x_val & 1u);
+        auto y_val = reinterpret_cast<uint32_t const*>(&pos.y);
+        dim = (dim & ~2u) | ((*y_val & 1u) << 1);
+        return dim;
     }
 };
 
